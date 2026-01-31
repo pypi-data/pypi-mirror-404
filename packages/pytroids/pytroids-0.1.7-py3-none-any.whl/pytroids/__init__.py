f0332489992aa80ceafd585c0d746c843a892fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:05:11 +0000

    Add package pytroids/0.1.7/pytroids-0.1.7-py3-none-any.whl

---
+++ packages/pytroids/pytroids-0.1.7-py3-none-any.whl/pytroids/__init__.py
@@ -0,0 +1,7 @@
+from .numbers.fibbonaccis import fibbonacci
+from .numbers.number_type_verify import number_type_verification
+from .numbers.numberS_type_verification import numberS_type_verification_LIST
+from .numbers.numericals import numerical_operations
+from .strings.lists import list_functions
+from .cyber.important_names import imp_names
+from .cyber.windows_file_attrib_setters import WINDOWS_file_attribs
