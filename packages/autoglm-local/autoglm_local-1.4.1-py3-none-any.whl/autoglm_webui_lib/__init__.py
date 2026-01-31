a6b5dc176dee3583c2d9243b2e669153de9a8c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:48 +0000

    Add package autoglm-local/1.4.1/autoglm_local-1.4.1-py3-none-any.whl

---
+++ packages/autoglm-local/autoglm_local-1.4.1-py3-none-any.whl/autoglm_webui_lib/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AutoGLM WebUI Library
+AI-APPUI自动化测试平台 - 服务端和本地端分离架构
+
+作者: chenwenkun
+"""
+
+__version__ = "1.0.0"
+__author__ = "chenwenkun"
