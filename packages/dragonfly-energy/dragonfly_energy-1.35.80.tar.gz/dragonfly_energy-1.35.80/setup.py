8cd84bad9b05fc0de41f15778a6672c85e5df3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:43:48 +0000

    Add package dragonfly-energy/1.35.80/dragonfly_energy-1.35.80.tar.gz

---
+++ packages/dragonfly-energy/dragonfly_energy-1.35.80.tar.gz/dragonfly_energy-1.35.80/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('standards-requirements.txt') as f:
+    standards_requirements = f.read().splitlines()
+
+with open('openstudio-requirements.txt') as f:
+    openstudio_requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="dragonfly-energy",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Dragonfly extension for energy simulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/dragonfly-energy",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        'standards': standards_requirements,
+        'openstudio': openstudio_requirements
+    },
+    entry_points={
+        "console_scripts": ["dragonfly-energy = dragonfly_energy.cli:energy"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: IronPython",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
