5f09f1d59011987cca43ac8cdc80020675d8524d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:06:00 +0000

    Add package statsig-python-core/0.15.1b2601310133/statsig_python_core-0.15.1b2601310133-cp37-abi3-win_amd64.whl

---
+++ packages/statsig-python-core/statsig_python_core-0.15.1b2601310133-cp37-abi3-win_amd64.whl/statsig_python_core/observability_client.py
@@ -0,0 +1,35 @@
+from statsig_python_core import ObservabilityClientBase
+from typing import Optional, Dict
+
+
+class ObservabilityClient(ObservabilityClientBase):
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.init_fn = self.init
+        self.increment_fn = self.increment
+        self.gauge_fn = self.gauge
+        self.dist_fn = self.dist
+        self.error_fn = self.error
+        self.should_enable_high_cardinality_for_this_tag_fn = self.should_enable_high_cardinality_for_this_tag
+
+    def init(self):
+        pass
+
+    def increment(self, metric_name: str, value: float, tags: Optional[Dict[str, str]] = None):
+        pass
+
+    def gauge(self, metric_name: str, value: float, tags: Optional[Dict[str, str]] = None):
+        pass
+
+    def dist(self, metric_name: str, value: float, tags: Optional[Dict[str, str]] = None):
+        pass
+
+    def error(self, tag: str, error: str):
+        pass
+
+    def should_enable_high_cardinality_for_this_tag(self, tag: str):
+        pass
+    
