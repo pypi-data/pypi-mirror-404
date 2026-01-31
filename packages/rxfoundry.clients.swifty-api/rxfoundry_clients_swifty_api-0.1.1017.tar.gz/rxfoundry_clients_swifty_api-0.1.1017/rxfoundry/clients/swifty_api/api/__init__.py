c745b692ab6af91db4c451205196083cc0a7f3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:30:57 +0000

    Add package rxfoundry.clients.swifty-api/0.1.1017/rxfoundry_clients_swifty_api-0.1.1017.tar.gz

---
+++ packages/rxfoundry.clients.swifty-api/rxfoundry_clients_swifty_api-0.1.1017.tar.gz/rxfoundry_clients_swifty_api-0.1.1017/rxfoundry/clients/swifty_api/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from rxfoundry.clients.swifty_api.api.async_api import AsyncApi
+from rxfoundry.clients.swifty_api.api.code_api import CodeApi
+from rxfoundry.clients.swifty_api.api.fax_api import FaxApi
+from rxfoundry.clients.swifty_api.api.medication_api import MedicationApi
+from rxfoundry.clients.swifty_api.api.patient_api import PatientApi
+from rxfoundry.clients.swifty_api.api.pharmacist_api import PharmacistApi
+from rxfoundry.clients.swifty_api.api.pharmacy_api import PharmacyApi
+from rxfoundry.clients.swifty_api.api.prescriber_api import PrescriberApi
+from rxfoundry.clients.swifty_api.api.prescription_api import PrescriptionApi
+from rxfoundry.clients.swifty_api.api.task_api import TaskApi
+from rxfoundry.clients.swifty_api.api.token_api import TokenApi
+from rxfoundry.clients.swifty_api.api.validation_api import ValidationApi
+from rxfoundry.clients.swifty_api.api.version_api import VersionApi
+from rxfoundry.clients.swifty_api.api.virtual_pharmacy_api import VirtualPharmacyApi
+from rxfoundry.clients.swifty_api.api.workflow_api import WorkflowApi
+
