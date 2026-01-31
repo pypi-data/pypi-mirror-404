98b049f830791997a1afa716745dc4f13db07189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:28 +0000

    Add package openviking/0.1.6/openviking-0.1.6-cp311-cp311-manylinux_2_39_x86_64.whl

---
+++ packages/openviking/openviking-0.1.6-cp311-cp311-manylinux_2_39_x86_64.whl/openviking/utils/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2026 Beijing Volcano Engine Technology Co., Ltd.
+# SPDX-License-Identifier: Apache-2.0
+"""Utility functions and helpers."""
+
+from openviking.utils.llm import StructuredLLM, parse_json_from_response, parse_json_to_model
+from openviking.utils.logger import default_logger, get_logger
+from openviking.utils.uri import VikingURI
+
+__all__ = [
+    "VikingURI",
+    "get_logger",
+    "default_logger",
+    "StructuredLLM",
+    "parse_json_from_response",
+    "parse_json_to_model",
+]
