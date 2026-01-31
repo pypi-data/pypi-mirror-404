9d2ad10b5c2fdc75d0513977013996ffe5e2b3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:13 +0000

    Add package easycoder/260130.1/easycoder-260130.1.tar.gz

---
+++ packages/easycoder/easycoder-260130.1.tar.gz/easycoder-260130.1/test_mqtt_standalone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Standalone test for MQTT Hub/Spoke without EasyCoder.
+Run this to verify the MQTT implementation works correctly.
+"""
+
+import sys
+sys.path.insert(0, '/home/graham/dev/easycoder/easycoder-py')
+
+# Import the module directly to run the test
+from easycoder import ec_mqtt
+
+# The test code will run from the __main__ block in ec_mqtt.py
