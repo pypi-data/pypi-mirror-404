9d3a1ae293caad17cde00ccaea44b585ca49c7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:30:53 +0000

    Add package coreason-maco/0.3.2/coreason_maco-0.3.2.tar.gz

---
+++ packages/coreason-maco/coreason_maco-0.3.2.tar.gz/coreason_maco-0.3.2/src/coreason_maco/utils/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2025 CoReason, Inc.
+#
+# This software is proprietary and dual-licensed.
+# Licensed under the Prosperity Public License 3.0 (the "License").
+# A copy of the license is available at https://prosperitylicense.com/versions/3.0.0
+# For details, see the LICENSE file.
+# Commercial use beyond a 30-day trial requires a separate license.
+#
+# Source Code: https://github.com/CoReason-AI/coreason_maco
+
+"""
+Utils module.
+"""
+
+from .context import request_id_var
+
+__all__ = ["request_id_var"]
