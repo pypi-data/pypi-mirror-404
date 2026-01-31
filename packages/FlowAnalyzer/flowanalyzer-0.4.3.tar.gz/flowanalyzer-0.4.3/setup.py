b1227a5075aeb88d46e0dd6be2b0b0e3b3d46538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:10 +0000

    Add package FlowAnalyzer/0.4.3/flowanalyzer-0.4.3.tar.gz

---
+++ packages/FlowAnalyzer/flowanalyzer-0.4.3.tar.gz/flowanalyzer-0.4.3/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="FlowAnalyzer",
+    version="0.4.3",
+    description="FlowAnalyzer是一个流量分析器，用于解析和处理tshark导出的JSON数据文件",
+    author="Byxs20",
+    author_email="97766819@qq.com",
+    packages=find_packages(exclude=["tests", "*.egg-info"]),
+    package_data={
+        '': ['LICENSE', 'README.md', 'setup.py'],
+    },
+    install_requires=[
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Byxs20/FlowAnalyzer",
+)
