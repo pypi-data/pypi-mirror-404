7653c4ecbb54eb1ae5f8c254618284e2a21b9201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:40 +0000

    Add package kryptoon/2026.1.31/kryptoon-2026.1.31-cp312-cp312-win_amd64.whl

---
+++ packages/kryptoon/kryptoon-2026.1.31-cp312-cp312-win_amd64.whl/kryptoon/classic/phf/__init__.pyi
@@ -0,0 +1,13 @@
+# IMPORT
+from kryptoon.classic.phf.__internal__ import (
+    Algorithm,
+    Argon2,
+    Bcrypt
+)
+
+# MAIN
+__all__: list[str] = [
+    "Algorithm",
+    "Argon2",
+    "Bcrypt"
+]
