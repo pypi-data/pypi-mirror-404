72ed5be9cf1301f2a9400b4a8be1b92466a042ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:47 +0000

    Add package badldap/0.7.5/badldap-0.7.5-py3-none-any.whl

---
+++ packages/badldap/badldap-0.7.5-py3-none-any.whl/badldap/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.7.5"
+__banner__ = \
+"""
+# badldap %s 
+# Original Author: Tamas Jos (@skelsec)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
---
+++ packages/badldap/badldap-0.7.5-py3-none-any.whl/badldap/examples/msldapbloodhound.py
@@ -0,0 +1,20 @@
+import asyncio
+from badldap.bloodhound import MSLDAPDump2Bloodhound
+
+async def amain(url, follow_trusts):
+	badldap = MSLDAPDump2Bloodhound(url, follow_trusts=follow_trusts)
+	await badldap.run()
+
+def main():
+	import argparse
+	parser = argparse.ArgumentParser(description='Bloodhound collector for badldap')
+	parser.add_argument('url', help='LDAP connection URL, or ADEXPLORER dat file path in the form adexplorer://<path>')
+	parser.add_argument('--follow-trusts', action='store_true', help='Follow trusts')
+	print("""
+WARNING: This script is still in development. It is not guaranteed to provide the same results as the original Bloodhound collector.
+""")
+	args = parser.parse_args()
+	asyncio.run(amain(args.url, args.follow_trusts))
+
+if __name__ == '__main__':
+	main()
