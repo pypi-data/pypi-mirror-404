0da4f590f937a586162ab5a96b92e5b61fd39f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:41 +0000

    Add package sapiens-model/1.0.8/sapiens_model-1.0.8.tar.gz

---
+++ packages/sapiens-model/sapiens_model-1.0.8.tar.gz/sapiens_model-1.0.8/setup.py
@@ -0,0 +1,43 @@
+"""
+This algorithm was designed, programmed, and developed by Sapiens Technology®️ to enable the construction, training, tuning, and inference of language models using the main architectural frameworks of Sapiens Technology®️.
+The code includes a main class with the base architecture and several other internal sub-architectures that can be configured through the model's parameters.
+
+Any changes to this code, reverse engineering, disclosure, or public commentary involving the technical aspects of the technology contained herein are strictly prohibited, and the authors will be duly prosecuted by our legal team.
+
+WE DO NOT authorize the commercial use of this code without prior permission from Sapiens Technology®️.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'sapiens_model'
+version = '1.0.8'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=[
+        'semantic-comparison-network==1.0.8',
+        'scnetwork==1.1.3',
+        'scn==3.0.1',
+        'hur==1.1.4',
+        'hur-multimodal==1.0.9',
+        'sapiens-cpu==4.0.1',
+        'mgt==2.0.2',
+        'sapiens-dataset==4.0.4',
+        'utilities-nlp==7.0.3',
+        'tqdm==4.67.1',
+        'certifi==2026.1.4',
+        'INFINITE-CONTEXT-WINDOW==3.0.2'
+    ],
+    url='https://github.com/sapiens-technology/sapiens_architecture_with_sapiens_model',
+    license='Proprietary Software'
+)
+"""
+This algorithm was designed, programmed, and developed by Sapiens Technology®️ to enable the construction, training, tuning, and inference of language models using the main architectural frameworks of Sapiens Technology®️.
+The code includes a main class with the base architecture and several other internal sub-architectures that can be configured through the model's parameters.
+
+Any changes to this code, reverse engineering, disclosure, or public commentary involving the technical aspects of the technology contained herein are strictly prohibited, and the authors will be duly prosecuted by our legal team.
+
+WE DO NOT authorize the commercial use of this code without prior permission from Sapiens Technology®️.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
