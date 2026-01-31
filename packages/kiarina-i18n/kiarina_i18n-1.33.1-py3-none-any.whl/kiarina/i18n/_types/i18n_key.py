76c18cc50f4fabffc3bec8b0eff202bf4ad3c4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:50 +0000

    Add package kiarina-i18n/1.33.1/kiarina_i18n-1.33.1-py3-none-any.whl

---
+++ packages/kiarina-i18n/kiarina_i18n-1.33.1-py3-none-any.whl/kiarina/i18n/_types/i18n_key.py
@@ -0,0 +1,8 @@
+from typing import TypeAlias
+
+I18nKey: TypeAlias = str
+"""
+A key for internationalization (i18n) translations within a specific scope.
+
+For example, a key could be "hello" for a greeting message.
+"""
