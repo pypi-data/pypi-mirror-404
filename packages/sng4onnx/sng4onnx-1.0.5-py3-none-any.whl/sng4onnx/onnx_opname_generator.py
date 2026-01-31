eaecaf47f49c3f90a996eb16d4a61e087d7fc535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:52 +0000

    Add package sng4onnx/1.0.5/sng4onnx-1.0.5-py3-none-any.whl

---
+++ packages/sng4onnx/sng4onnx-1.0.5-py3-none-any.whl/sng4onnx/onnx_opname_generator.py
@@ -0,0 +1,201 @@
+#! /usr/bin/env python
+
+import sys
+from argparse import ArgumentParser
+import onnx
+from onnx.external_data_helper import uses_external_data
+import onnx_graphsurgeon as gs
+from typing import Optional
+
+class Color:
+    BLACK          = '\033[30m'
+    RED            = '\033[31m'
+    GREEN          = '\033[32m'
+    YELLOW         = '\033[33m'
+    BLUE           = '\033[34m'
+    MAGENTA        = '\033[35m'
+    CYAN           = '\033[36m'
+    WHITE          = '\033[37m'
+    COLOR_DEFAULT  = '\033[39m'
+    BOLD           = '\033[1m'
+    UNDERLINE      = '\033[4m'
+    INVISIBLE      = '\033[08m'
+    REVERCE        = '\033[07m'
+    BG_BLACK       = '\033[40m'
+    BG_RED         = '\033[41m'
+    BG_GREEN       = '\033[42m'
+    BG_YELLOW      = '\033[43m'
+    BG_BLUE        = '\033[44m'
+    BG_MAGENTA     = '\033[45m'
+    BG_CYAN        = '\033[46m'
+    BG_WHITE       = '\033[47m'
+    BG_DEFAULT     = '\033[49m'
+    RESET          = '\033[0m'
+
+def model_uses_external_data(input_onnx_file_path: str) -> bool:
+    model = onnx.load(input_onnx_file_path, load_external_data=False)
+    def iter_tensors_in_graph(g):
+        for t in g.initializer:
+            yield t
+        for t in g.sparse_initializer:
+            yield t
+        for n in g.node:
+            for a in n.attribute:
+                if a.type == onnx.AttributeProto.TENSOR:
+                    yield a.t
+                elif a.type == onnx.AttributeProto.TENSORS:
+                    for t in a.tensors:
+                        yield t
+                elif a.type == onnx.AttributeProto.GRAPH:
+                    yield from iter_tensors_in_graph(a.g)
+                elif a.type == onnx.AttributeProto.GRAPHS:
+                    for sg in a.graphs:
+                        yield from iter_tensors_in_graph(sg)
+    return any(uses_external_data(t) for t in iter_tensors_in_graph(model.graph))
+
+def generate(
+    input_onnx_file_path: Optional[str] = '',
+    onnx_graph: Optional[onnx.ModelProto] = None,
+    output_onnx_file_path: Optional[str] = '',
+    has_external_data: Optional[bool] = False,
+    non_verbose: Optional[bool] = False,
+) -> onnx.ModelProto:
+    """
+    Parameters
+    ----------
+    input_onnx_file_path: Optional[str]
+        Input onnx file path.\n\
+        Either input_onnx_file_path or onnx_graph must be specified.\n\
+        Default: ''
+
+    onnx_graph: Optional[onnx.ModelProto]
+        onnx.ModelProto.\n\
+        Either input_onnx_file_path or onnx_graph must be specified.\n\
+        onnx_graph If specified, ignore input_onnx_file_path and process onnx_graph.
+
+    output_onnx_file_path: Optional[str]
+        Output onnx file path. If not specified, no ONNX file is output.\n\
+        Default: ''
+
+    has_external_data: Optional[bool]
+        Default: False
+
+    non_verbose: Optional[bool]
+        Do not show all information logs. Only error logs are displayed.\n\
+        Default: False
+
+    Returns
+    -------
+    renamed_graph: onnx.ModelProto
+        Renamed onnx ModelProto.
+    """
+
+    # Unspecified check for input_onnx_file_path and onnx_graph
+    if not input_onnx_file_path and not onnx_graph:
+        print(
+            f'{Color.RED}ERROR:{Color.RESET} '+
+            f'One of input_onnx_file_path or onnx_graph must be specified.'
+        )
+        sys.exit(1)
+
+    # Loading Graphs
+    # onnx_graph If specified, onnx_graph is processed first
+    if not onnx_graph:
+        onnx_graph = onnx.load(input_onnx_file_path)
+    domain: str = onnx_graph.domain
+    ir_version: int = onnx_graph.ir_version
+    meta_data = {'domain': domain, 'ir_version': ir_version}
+    metadata_props = None
+    if hasattr(onnx_graph, 'metadata_props'):
+        metadata_props = onnx_graph.metadata_props
+    graph = gs.import_onnx(onnx_graph)
+    graph.cleanup().toposort()
+
+    # opname auto supplementation
+    # OPs in old ONNX files may be missing opname,
+    # so opname is automatically completed when opname is empty.
+    supplementation_idx = 0
+    for graph_node in graph.nodes:
+        if graph_node.name == '':
+            graph_node.name = f'sng_{graph_node.op}_{supplementation_idx}'
+            supplementation_idx += 1
+
+    graph.cleanup().toposort()
+
+    # Shape Estimation
+    renamed_graph = None
+    try:
+        exported_onnx_graph = gs.export_onnx(graph, do_type_check=False, **meta_data)
+        if metadata_props is not None:
+            exported_onnx_graph.metadata_props.extend(metadata_props)
+        if not has_external_data:
+            renamed_graph = onnx.shape_inference.infer_shapes(exported_onnx_graph)
+        else:
+            renamed_graph = exported_onnx_graph
+    except Exception as ex:
+        renamed_graph = gs.export_onnx(graph, do_type_check=False, **meta_data)
+        if metadata_props is not None:
+            exported_onnx_graph.metadata_props.extend(metadata_props)
+        if not non_verbose:
+            print(
+                f'{Color.YELLOW}WARNING:{Color.RESET} '+
+                'The input shape of the next OP does not match the output shape. '+
+                'Be sure to open the .onnx file to verify the certainty of the geometry.'
+            )
+
+    # Save
+    if output_onnx_file_path:
+        onnx.save(renamed_graph, output_onnx_file_path)
+
+    if not non_verbose:
+        print(f'{Color.GREEN}INFO:{Color.RESET} Finish!')
+
+    # Return
+    return renamed_graph
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-if',
+        '--input_onnx_file_path',
+        type=str,
+        required=True,
+        help='Input onnx file path.'
+    )
+    parser.add_argument(
+        '-of',
+        '--output_onnx_file_path',
+        type=str,
+        required=True,
+        help='Output onnx file path.'
+    )
+    parser.add_argument(
+        '-n',
+        '--non_verbose',
+        action='store_true',
+        help='Do not show all information logs. Only error logs are displayed.'
+    )
+    args = parser.parse_args()
+
+    input_onnx_file_path = args.input_onnx_file_path
+    output_onnx_file_path = args.output_onnx_file_path
+    non_verbose = args.non_verbose
+
+    # Load
+    onnx_graph = onnx.load(input_onnx_file_path)
+    # External Data Check
+    has_external_data = model_uses_external_data(input_onnx_file_path)
+
+    # OP add
+    renamed_graph = generate(
+        input_onnx_file_path=None,
+        onnx_graph=onnx_graph,
+        output_onnx_file_path=output_onnx_file_path,
+        has_external_data=has_external_data,
+        non_verbose=non_verbose,
+    )
+
+
+if __name__ == '__main__':
+    main()
