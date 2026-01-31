2a3d8378bd3c41c97acc11b83e374e369794036a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:49 +0000

    Add package pyAgrum-nightly/2.3.2.9.dev202601311769423251/pyagrum_nightly-2.3.2.9.dev202601311769423251-cp310-abi3-macosx_11_0_arm64.whl

---
+++ packages/pyAgrum-nightly/pyagrum_nightly-2.3.2.9.dev202601311769423251-cp310-abi3-macosx_11_0_arm64.whl/pyagrum/causal/__init__.py
@@ -0,0 +1,81 @@
+############################################################################
+#   This file is part of the aGrUM/pyAgrum library.                        #
+#                                                                          #
+#   Copyright (c) 2005-2026 by                                             #
+#       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 #
+#       - Christophe GONZALES(_at_AMU)                                     #
+#                                                                          #
+#   The aGrUM/pyAgrum library is free software; you can redistribute it    #
+#   and/or modify it under the terms of either :                           #
+#                                                                          #
+#    - the GNU Lesser General Public License as published by               #
+#      the Free Software Foundation, either version 3 of the License,      #
+#      or (at your option) any later version,                              #
+#    - the MIT license (MIT),                                              #
+#    - or both in dual license, as here.                                   #
+#                                                                          #
+#   (see https://agrum.gitlab.io/articles/dual-licenses-lgplv3mit.html)    #
+#                                                                          #
+#   This aGrUM/pyAgrum library is distributed in the hope that it will be  #
+#   useful, but WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,          #
+#   INCLUDING BUT NOT LIMITED TO THE WARRANTIES MERCHANTABILITY or FITNESS #
+#   FOR A PARTICULAR PURPOSE  AND NONINFRINGEMENT. IN NO EVENT SHALL THE   #
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER #
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,        #
+#   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR  #
+#   OTHER DEALINGS IN THE SOFTWARE.                                        #
+#                                                                          #
+#   See LICENCES for more details.                                         #
+#                                                                          #
+#   SPDX-FileCopyrightText: Copyright 2005-2026                            #
+#       - Pierre-Henri WUILLEMIN(_at_LIP6)                                 #
+#       - Christophe GONZALES(_at_AMU)                                     #
+#   SPDX-License-Identifier: LGPL-3.0-or-later OR MIT                      #
+#                                                                          #
+#   Contact  : info_at_agrum_dot_org                                       #
+#   homepage : http://agrum.gitlab.io                                      #
+#   gitlab   : https://gitlab.com/agrumery/agrum                           #
+#                                                                          #
+############################################################################
+
+"""
+Causality in pyAgrum mainly consists in the ability to build a causal model, i.e. a (observational) Bayesian network
+and a set of latent variables and their relation with observation variables and in the ability to compute using
+do-calculus the causal impact in such a model.
+"""
+
+__author__ = "Pierre-Henri Wuillemin, Paul Alam, Ibrahim Merad"
+__copyright__ = "(c) 2019-2024 PARIS"
+
+from ._causalImpact import causalImpact, counterfactualModel, counterfactual
+from ._doorCriteria import backdoor_generator, frontdoor_generator
+from ._doAST import ASTtree, ASTjointProba, ASTmult, ASTdiv, ASTsum, ASTposteriorProba, ASTminus, ASTplus, ASTBinaryOp
+from ._doCalculus import doCalculusWithObservation, identifyingIntervention
+from ._CausalFormula import CausalFormula
+from ._CausalModel import CausalModel
+from ._exceptions import HedgeException, UnidentifiableException
+from .causalEffectEstimation import CausalEffectEstimation
+import sys
+
+__all__ = [
+  "CausalModel",
+  "CausalFormula",
+  "CausalEffectEstimation",
+  "causalImpact",
+  "counterfactualModel",
+  "counterfactual",
+  "doCalculusWithObservation",
+  "identifyingIntervention",
+  "backdoor_generator",
+  "HedgeException",
+  "UnidentifiableException",
+  "ASTtree",
+  "ASTjointProba",
+  "ASTmult",
+  "ASTdiv",
+  "ASTsum",
+  "ASTposteriorProba",
+  "ASTminus",
+  "ASTplus",
+  "ASTBinaryOp",
+]
