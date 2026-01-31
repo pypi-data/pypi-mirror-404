8be2be7f3c08cabf532864664dc2b018d55e7313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:30:53 +0000

    Add package pyart-mch/2.3.1/pyart_mch-2.3.1-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pyart-mch/pyart_mch-2.3.1-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/pyart/__init__.py
@@ -0,0 +1,57 @@
+"""
+Py-ART: The Python ARM Radar Toolkit
+=====================================
+
+"""
+
+import importlib.metadata as _importlib_metadata
+from os import environ as _environ
+
+# import subpackages
+# print out helpful message if build fails or importing from source tree
+from . import (
+    __check_build,  # noqa
+    aux_io,  # noqa
+    bridge,  # noqa
+    config,  # noqa
+    core,  # noqa
+    correct,  # noqa
+    filters,  # noqa
+    graph,  # noqa
+    io,  # noqa
+    map,  # noqa
+    retrieve,  # noqa
+    testing,  # noqa
+    util,  # noqa
+    xradar,  # noqa
+)
+from ._debug_info import _debug_info  # noqa
+
+# root level functions
+from .config import load_config  # noqa
+
+# print information on citing Py-ART, this message can be suppressed by
+# setting the PYART_QUIET environment variable
+_citation_text = """
+## You are using the Python ARM Radar Toolkit (Py-ART), an open source
+## library for working with weather radar data. Py-ART is partly
+## supported by the U.S. Department of Energy as part of the Atmospheric
+## Radiation Measurement (ARM) Climate Research Facility, an Office of
+## Science user facility.
+##
+## If you use this software to prepare a publication, please cite:
+##
+##     JJ Helmus and SM Collis, JORS 2016, doi: 10.5334/jors.119
+"""
+
+if "PYART_QUIET" not in _environ:
+    print(_citation_text)
+
+__ismchfork__ = True  # used to recognized whether ARM or MCH version
+
+# Get the version
+try:
+    __version__ = _importlib_metadata.version("pyart_mch")
+except _importlib_metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = "0.0.0"
---
+++ packages/pyart-mch/pyart_mch-2.3.1-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/pyart/core/__init__.py
@@ -0,0 +1,54 @@
+"""
+========================
+Core (:mod:`pyart.core`)
+========================
+
+.. currentmodule:: pyart.core
+
+Core Py-ART classes and function for interacting with weather radar data.
+
+Core classes
+============
+
+.. autosummary::
+    :toctree: generated/
+
+    Radar
+    Grid
+    HorizontalWindProfile
+    RadarSpectra
+
+Coordinate transformations
+==========================
+
+.. autosummary::
+    :toctree: generated/
+
+    antenna_to_cartesian
+    antenna_vectors_to_cartesian
+    cartesian_to_geographic
+    cartesian_vectors_to_geographic
+    cartesian_to_geographic_aeqd
+    cartesian_to_antenna
+    geographic_to_cartesian
+    geographic_to_cartesian_aeqd
+    wgs84_to_swissCH1903
+
+"""
+
+from .radar import Radar  # noqa
+from .grid import Grid  # noqa
+from .wind_profile import HorizontalWindProfile  # noqa
+from .radar_spectra import RadarSpectra  # noqa
+
+from .transforms import antenna_to_cartesian  # noqa
+from .transforms import antenna_vectors_to_cartesian  # noqa
+from .transforms import cartesian_to_geographic  # noqa
+from .transforms import cartesian_vectors_to_geographic  # noqa
+from .transforms import cartesian_to_geographic_aeqd  # noqa
+from .transforms import cartesian_to_antenna  # noqa
+from .transforms import geographic_to_cartesian  # noqa
+from .transforms import geographic_to_cartesian_aeqd  # noqa
+from .transforms import wgs84_to_swissCH1903  # noqa
+
+__all__ = [s for s in dir() if not s.startswith("_")]
