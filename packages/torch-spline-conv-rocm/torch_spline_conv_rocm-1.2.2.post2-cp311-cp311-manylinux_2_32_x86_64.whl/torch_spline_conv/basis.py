084505569c1ce2dbfc03a375702d3004b1364808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:05:12 +0000

    Add package torch-spline-conv-rocm/1.2.2.post2/torch_spline_conv_rocm-1.2.2.post2-cp311-cp311-manylinux_2_32_x86_64.whl

---
+++ packages/torch-spline-conv-rocm/torch_spline_conv_rocm-1.2.2.post2-cp311-cp311-manylinux_2_32_x86_64.whl/torch_spline_conv/basis.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+import torch
+
+
+def spline_basis(
+    pseudo: torch.Tensor,
+    kernel_size: torch.Tensor,
+    is_open_spline: torch.Tensor,
+    degree: int,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    return torch.ops.torch_spline_conv.spline_basis(pseudo, kernel_size,
+                                                    is_open_spline, degree)
