52ae38f3ad6b0ae5968d7fba687cddd503be3b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:24 +0000

    Add package pytorch-ignite/0.6.0.dev20260131/pytorch_ignite-0.6.0.dev20260131-py3-none-any.whl

---
+++ packages/pytorch-ignite/pytorch_ignite-0.6.0.dev20260131-py3-none-any.whl/ignite/contrib/handlers/mlflow_logger.py
@@ -0,0 +1,22 @@
+""" ``ignite.contrib.handlers.mlflow_logger`` was moved to ``ignite.handlers.mlflow_logger``.
+Note:
+    ``ignite.contrib.handlers.mlflow_logger`` was moved to ``ignite.handlers.mlflow_logger``.
+    Please refer to :mod:`~ignite.handlers.mlflow_logger`.
+"""
+
+import warnings
+
+removed_in = "0.6.0"
+deprecation_warning = (
+    f"{__file__} has been moved to /ignite/handlers/mlflow_logger.py"
+    + f" and will be removed in version {removed_in}"
+    + ".\n Please refer to the documentation for more details."
+)
+warnings.warn(deprecation_warning, DeprecationWarning, stacklevel=2)
+from ignite.handlers.mlflow_logger import MLflowLogger, OptimizerParamsHandler, OutputHandler
+from ignite.handlers.utils import global_step_from_engine  # noqa
+
+__all__ = ["MLflowLogger", "OutputHandler", "OptimizerParamsHandler"]
+MLflowLogger = MLflowLogger
+OutputHandler = OutputHandler
+OptimizerParamsHandler = OptimizerParamsHandler
---
+++ packages/pytorch-ignite/pytorch_ignite-0.6.0.dev20260131-py3-none-any.whl/ignite/contrib/handlers/polyaxon_logger.py
@@ -0,0 +1,22 @@
+""" ``ignite.contrib.handlers.polyaxon_logger`` was moved to ``ignite.handlers.polyaxon_logger``.
+Note:
+    ``ignite.contrib.handlers.polyaxon_logger`` was moved to ``ignite.handlers.polyaxon_logger``.
+    Please refer to :mod:`~ignite.handlers.polyaxon_logger`.
+"""
+
+import warnings
+
+removed_in = "0.6.0"
+deprecation_warning = (
+    f"{__file__} has been moved to /ignite/handlers/polyaxon_logger.py"
+    + f" and will be removed in version {removed_in}"
+    + ".\n Please refer to the documentation for more details."
+)
+warnings.warn(deprecation_warning, DeprecationWarning, stacklevel=2)
+from ignite.handlers.polyaxon_logger import OptimizerParamsHandler, OutputHandler, PolyaxonLogger
+from ignite.handlers.utils import global_step_from_engine  # noqa
+
+__all__ = ["PolyaxonLogger", "OutputHandler", "OptimizerParamsHandler"]
+PolyaxonLogger = PolyaxonLogger
+OutputHandler = OutputHandler
+OptimizerParamsHandler = OptimizerParamsHandler
