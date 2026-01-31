dea428f1ba6f9ec7d44cd192bfaa6006f1e857a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:43:52 +0000

    Add package dissect.target/3.25.dev42/dissect_target-3.25.dev42.tar.gz

---
+++ packages/dissect.target/dissect_target-3.25.dev42.tar.gz/dissect_target-3.25.dev42/tests/plugins/os/unix/bsd/citrix/test_history.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import datetime
+import gzip
+import textwrap
+from io import BytesIO
+from typing import TYPE_CHECKING
+
+import pytest
+
+from dissect.target.plugins.os.unix.bsd.citrix.history import CitrixCommandHistoryPlugin
+
+if TYPE_CHECKING:
+    from dissect.target.filesystem import VirtualFilesystem
+    from dissect.target.target import Target
+
+
+COMMANDHISTORY_DATA = """\
+Aug  9 12:56:00 <local7.notice> ns bash[23841]: root on /dev/pts/0 shell_command="find . -name '*ci.php*'"
+Aug 10 11:57:39 <local7.notice> ns bash[56440]: (null) on /dev/pts/1 shell_command="debug "hello world""
+"""
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/var/log/bash.log",
+        "/var/log/sh.log",
+    ],
+)
+def test_netscaler_bash_history_absolute_path_glob(target_citrix: Target, fs_bsd: VirtualFilesystem, path: str) -> None:
+    fs_bsd.map_file_fh(path, BytesIO(textwrap.dedent(COMMANDHISTORY_DATA).encode()))
+    target_citrix.add_plugin(CitrixCommandHistoryPlugin)
+
+    results = list(target_citrix.commandhistory())
+    assert len(results) == 2
+    # Due to the usage of year_rollover_helper, the results are returned back-to-front. Moreover, it takes the
+    # year from the file's mtime, which in this mocked version is epoch 0 (thus the year 1970)
+    assert results[1].ts == datetime.datetime(1970, 8, 9, 12, 56, 0, tzinfo=datetime.timezone.utc)
+    assert results[1].command == "find . -name '*ci.php*'"
+    assert results[1].order == -1
+    assert results[1].shell == "citrix-netscaler-bash"
+    assert results[1].source.as_posix() == path
+
+    assert results[0].ts == datetime.datetime(1970, 8, 10, 11, 57, 39, tzinfo=datetime.timezone.utc)
+    assert results[0].command == 'debug "hello world"'
+    assert results[0].order == 0
+    assert results[0].shell == "citrix-netscaler-bash"
+    assert results[0].source.as_posix() == path
+
+
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/var/log/bash.log.0.gz",
+        "/var/log/sh.log.1.gz",
+    ],
+)
+def test_netscaler_commandhistory_decompress(target_citrix: Target, fs_bsd: VirtualFilesystem, path: str) -> None:
+    fs_bsd.map_file_fh(path, BytesIO(gzip.compress(textwrap.dedent(COMMANDHISTORY_DATA).encode())))
+    target_citrix.add_plugin(CitrixCommandHistoryPlugin)
+
+    results = list(target_citrix.commandhistory())
+    assert len(results) == 2
+    # Due to the usage of year_rollover_history, the results are returned back-to-front
+    assert results[1].ts == datetime.datetime(1970, 8, 9, 12, 56, 0, tzinfo=datetime.timezone.utc)
+    assert results[1].command == "find . -name '*ci.php*'"
+    assert results[1].order == -1
+    assert results[1].shell == "citrix-netscaler-bash"
+    assert results[1].source.as_posix() == path
+
+    assert results[0].ts == datetime.datetime(1970, 8, 10, 11, 57, 39, tzinfo=datetime.timezone.utc)
+    assert results[0].command == 'debug "hello world"'
+    assert results[0].order == 0
+    assert results[0].shell == "citrix-netscaler-bash"
+    assert results[0].source.as_posix() == path
+
+
+def test_netscaler_cli_history(target_citrix: Target, fs_bsd: VirtualFilesystem) -> None:
+    commandhistory_data = """\
+    _HiStOrY_V2_
+    help
+    shell
+    """
+
+    fs_bsd.map_file_fh("/var/nstmp/user/.nscli_history", BytesIO(textwrap.dedent(commandhistory_data).encode()))
+
+    target_citrix.add_plugin(CitrixCommandHistoryPlugin)
+
+    results = list(target_citrix.commandhistory())
+    assert len(results) == 2
+
+    assert not results[0].ts
+    assert results[0].command == "help"
+    assert results[0].order == 0
+    assert results[0].shell == "citrix-netscaler-cli"
+    assert results[0].source.as_posix() == "/var/nstmp/user/.nscli_history"
+
+    assert not results[1].ts
+    assert results[1].command == "shell"
+    assert results[1].order == 1
+    assert results[1].shell == "citrix-netscaler-cli"
+    assert results[1].source.as_posix() == "/var/nstmp/user/.nscli_history"
---
+++ packages/dissect.target/dissect_target-3.25.dev42.tar.gz/dissect_target-3.25.dev42/tests/tools/test_mount.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from io import BytesIO
+from typing import TYPE_CHECKING
+from unittest.mock import patch
+
+from dissect.target.filesystem import VirtualFilesystem
+from dissect.target.tools.mount import main as target_mount
+from dissect.target.volume import Volume
+
+if TYPE_CHECKING:
+    import pytest
+
+    from dissect.target.target import Target
+
+
+def test_duplicate_volume_name(target_bare: Target, monkeypatch: pytest.MonkeyPatch) -> None:
+    with monkeypatch.context() as m:
+        m.setattr("sys.argv", ["target-mount", "mock-target", "mock-mount"])
+        m.setattr("dissect.target.tools.mount.HAS_FUSE", True)
+
+        with (
+            patch("dissect.target.tools.mount.open_target", return_value=target_bare),
+            patch("dissect.target.tools.mount.FUSE", create=True) as MockFUSE,
+            patch("dissect.target.tools.mount.DissectMount", create=True) as MockDissectMount,
+        ):
+            target_bare.volumes.add(Volume(BytesIO(), 1, 0, 0, None, name="first"))
+            target_bare.volumes.add(Volume(BytesIO(), 2, 0, 0, None, name="second"))
+            target_bare.volumes.add(Volume(BytesIO(), 3, 0, 0, None, name="second_1"))
+            target_bare.volumes.add(Volume(BytesIO(), 4, 0, 0, None, name="second"))
+
+            mock_fs = VirtualFilesystem()
+            mock_fs.volume = target_bare.volumes[1]
+            target_bare.filesystems.add(mock_fs)
+            mock_fs = VirtualFilesystem()
+            mock_fs.volume = target_bare.volumes[2]
+            target_bare.filesystems.add(mock_fs)
+            mock_fs = VirtualFilesystem()
+            mock_fs.volume = target_bare.volumes[3]
+            target_bare.filesystems.add(mock_fs)
+
+            target_mount()
+
+            MockFUSE.assert_called_once()
+            MockDissectMount.assert_called_once()
+            vfs = MockDissectMount.call_args[0][0]
+
+            assert vfs.listdir("/volumes") == ["first", "second", "second_1", "second_2"]
+            assert vfs.listdir("/filesystems") == ["second", "second_1", "second_2"]
+
+
+def test_mounting_multi_volume_filesystem(target_bare: Target, monkeypatch: pytest.MonkeyPatch) -> None:
+    with monkeypatch.context() as m:
+        m.setattr("sys.argv", ["target-mount", "mock-target", "mock-mount"])
+        m.setattr("dissect.target.tools.mount.HAS_FUSE", True)
+
+        with (
+            patch("dissect.target.tools.mount.open_target", return_value=target_bare),
+            patch("dissect.target.tools.mount.FUSE", create=True) as MockFUSE,
+            patch("dissect.target.tools.mount.DissectMount", create=True) as MockDissectMount,
+        ):
+            volumes = [
+                Volume(BytesIO(), 1, 0, 0, None, name="first"),
+                Volume(BytesIO(), 2, 0, 0, None, name="second"),
+            ]
+
+            for vol in volumes:
+                target_bare.volumes.add(vol)
+
+            mock_fs = VirtualFilesystem()
+            mock_fs.volume = volumes
+            target_bare.filesystems.add(mock_fs)
+
+            target_mount()
+
+            MockFUSE.assert_called_once()
+            MockDissectMount.assert_called_once()
+            vfs = MockDissectMount.call_args[0][0]
+
+            assert vfs.listdir("/filesystems") == ["first", "second"]
