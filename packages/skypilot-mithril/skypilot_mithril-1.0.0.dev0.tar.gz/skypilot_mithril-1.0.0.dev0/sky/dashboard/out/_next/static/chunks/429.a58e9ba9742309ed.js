78664aef5828cf14419af5015be2272bfc6c0b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:31:01 +0000

    Add package skypilot-mithril/1.0.0.dev0/skypilot_mithril-1.0.0.dev0.tar.gz

---
+++ packages/skypilot-mithril/skypilot_mithril-1.0.0.dev0.tar.gz/skypilot_mithril-1.0.0.dev0/sky/dashboard/out/_next/static/chunks/429.a58e9ba9742309ed.js
@@ -0,0 +1,16 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[429],{88507:function(e,n,t){t.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,t(60998).Z)("MessageSquare",[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]])},28586:function(e,n,t){t.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,t(60998).Z)("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]])},43767:function(e,n,t){t.d(n,{Z:function(){return r}});/**
+ * @license lucide-react v0.407.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let r=(0,t(60998).Z)("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]])},23001:function(e,n,t){t.d(n,{X:function(){return u}});var r=t(67294);function u(){let e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:768,[n,t]=(0,r.useState)(!1);return(0,r.useEffect)(()=>{let n=()=>{t(window.innerWidth<e)};return n(),window.addEventListener("resize",n),()=>{window.removeEventListener("resize",n)}},[e]),n}},32350:function(e,n,t){t.d(n,{cn:function(){return i}});var r=t(90512),u=t(98388);function i(){for(var e=arguments.length,n=Array(e),t=0;t<e;t++)n[t]=arguments[t];return(0,u.m6)((0,r.W)(n))}}}]);
