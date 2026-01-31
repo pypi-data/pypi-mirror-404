ad771c498a2c68c030ae8d89271f23ce862bade5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:51 +0000

    Add package lghorizon/0.9.0.dev4/lghorizon-0.9.0.dev4.tar.gz

---
+++ packages/lghorizon/lghorizon-0.9.0.dev4.tar.gz/lghorizon-0.9.0.dev4/lghorizon/helpers.py
@@ -0,0 +1,9 @@
+"""Helper functions."""
+
+import random
+
+
+def make_id(string_length=10):
+    """Create an id with given length."""
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    return "".join(random.choice(letters) for i in range(string_length))
