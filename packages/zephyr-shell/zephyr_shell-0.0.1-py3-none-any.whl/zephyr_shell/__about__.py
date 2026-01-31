a7783049c7dd7426da1f04b5178e1f69b14ec8f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:44 +0000

    Add package zephyr-shell/0.0.1/zephyr_shell-0.0.1-py3-none-any.whl

---
+++ packages/zephyr-shell/zephyr_shell-0.0.1-py3-none-any.whl/zephyr_shell/__about__.py
@@ -0,0 +1,3 @@
+__author__ = "Kapil Sachdeva"
+__application__ = "zephyr-shell"
+__version__ = "0.0.1"
---
+++ packages/zephyr-shell/zephyr_shell-0.0.1-py3-none-any.whl/zephyr_shell/__init__.py
@@ -0,0 +1,3 @@
+from .__about__ import __application__, __author__, __version__
+
+__all__ = ["__version__", "__application__", "__author__"]
