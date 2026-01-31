66cb40325818cc2aec8f9a5bd4085bc810e8fa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:05:21 +0000

    Add package fastapi-sqlalchemy-ease/0.1.0/fastapi_sqlalchemy_ease-0.1.0-py3-none-any.whl

---
+++ packages/fastapi-sqlalchemy-ease/fastapi_sqlalchemy_ease-0.1.0-py3-none-any.whl/fastapi_sqlalchemy_ease/__init__.py
@@ -0,0 +1,12 @@
+from .core import SQLAlchemy
+from .exceptions import DatabaseError, DatabaseNotInitializedError
+
+__version__='0.1.0'
+
+# when someone call: from fastapi_sqlalchemy import SQLAlchemy
+# only these things are available
+__all__ = [
+    "SQLAlchemy",
+    "DatabaseError",
+    "DatabaseNotInitializedError",
+] 
