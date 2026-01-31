8e7317e2c71cc3c6b7ebadeede1d4568fb692cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:28 +0000

    Add package najaeda/0.3.6/najaeda-0.3.6-cp310-cp310-win_amd64.whl

---
+++ packages/najaeda/najaeda-0.3.6-cp310-cp310-win_amd64.whl/najaeda/__init__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 The Naja authors
+# <https://github.com/najaeda/naja/blob/main/AUTHORS>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_12_0():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'najaeda.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_12_0()
+del _delvewheel_patch_1_12_0
+# end delvewheel patch
+
+from ._version import version, git_hash
+
+__all__ = ["version", "git_hash"]
