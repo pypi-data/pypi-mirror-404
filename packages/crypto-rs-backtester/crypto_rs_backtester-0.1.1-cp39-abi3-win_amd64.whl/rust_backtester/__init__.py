cd3e24bb794df03f666f1fdb597422b400758b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:50 +0000

    Add package crypto-rs-backtester/0.1.1/crypto_rs_backtester-0.1.1-cp39-abi3-win_amd64.whl

---
+++ packages/crypto-rs-backtester/crypto_rs_backtester-0.1.1-cp39-abi3-win_amd64.whl/rust_backtester/__init__.py
@@ -0,0 +1,9 @@
+from ._core import BacktestResult, Backtester, __version__, call_strategy_on_ticks
+
+__all__ = [
+    "BacktestResult",
+    "Backtester",
+    "__version__",
+    "call_strategy_on_ticks",
+]
+
