31fa106faf7151e07d7e5fa1c2d35310e72aab93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:30:54 +0000

    Add package cybrid-api-bank-python/0.126.83/cybrid_api_bank_python-0.126.83-py3-none-any.whl

---
+++ packages/cybrid-api-bank-python/cybrid_api_bank_python-0.126.83-py3-none-any.whl/cybrid_api_bank/apis/__init__.py
@@ -0,0 +1,38 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from cybrid_api_bank.api.accounts_bank_api import AccountsBankApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from cybrid_api_bank.api.accounts_bank_api import AccountsBankApi
+from cybrid_api_bank.api.assets_bank_api import AssetsBankApi
+from cybrid_api_bank.api.banks_bank_api import BanksBankApi
+from cybrid_api_bank.api.counterparties_bank_api import CounterpartiesBankApi
+from cybrid_api_bank.api.customers_bank_api import CustomersBankApi
+from cybrid_api_bank.api.deposit_addresses_bank_api import DepositAddressesBankApi
+from cybrid_api_bank.api.deposit_bank_accounts_bank_api import DepositBankAccountsBankApi
+from cybrid_api_bank.api.executions_bank_api import ExecutionsBankApi
+from cybrid_api_bank.api.external_bank_accounts_bank_api import ExternalBankAccountsBankApi
+from cybrid_api_bank.api.external_wallets_bank_api import ExternalWalletsBankApi
+from cybrid_api_bank.api.files_bank_api import FilesBankApi
+from cybrid_api_bank.api.identity_verifications_bank_api import IdentityVerificationsBankApi
+from cybrid_api_bank.api.invoices_bank_api import InvoicesBankApi
+from cybrid_api_bank.api.payment_instructions_bank_api import PaymentInstructionsBankApi
+from cybrid_api_bank.api.persona_sessions_bank_api import PersonaSessionsBankApi
+from cybrid_api_bank.api.plans_bank_api import PlansBankApi
+from cybrid_api_bank.api.prices_bank_api import PricesBankApi
+from cybrid_api_bank.api.quotes_bank_api import QuotesBankApi
+from cybrid_api_bank.api.symbols_bank_api import SymbolsBankApi
+from cybrid_api_bank.api.trades_bank_api import TradesBankApi
+from cybrid_api_bank.api.transfers_bank_api import TransfersBankApi
+from cybrid_api_bank.api.workflows_bank_api import WorkflowsBankApi
