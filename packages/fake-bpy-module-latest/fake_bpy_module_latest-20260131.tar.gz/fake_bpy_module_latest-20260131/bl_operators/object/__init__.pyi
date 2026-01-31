2f0b403c8969e0b2f952cb0fd251cebbf6ebce3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:05:37 +0000

    Add package fake-bpy-module-latest/20260131/fake_bpy_module_latest-20260131.tar.gz

---
+++ packages/fake-bpy-module-latest/fake_bpy_module_latest-20260131.tar.gz/fake_bpy_module_latest-20260131/bl_operators/object/__init__.pyi
@@ -0,0 +1,513 @@
+import typing
+import collections.abc
+import typing_extensions
+import numpy.typing as npt
+import _bpy_types
+import bpy.types
+
+class ClearAllRestrictRender(_bpy_types.Operator):
+    """Reveal all render objects by setting the hide render flag"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class DupliOffsetFromCursor(_bpy_types.Operator):
+    """Set offset used for collection instances based on cursor position"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class DupliOffsetFromObject(_bpy_types.Operator):
+    """Set offset used for collection instances based on the active object position"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class DupliOffsetToCursor(_bpy_types.Operator):
+    """Set cursor position to the offset used for collection instances"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class IsolateTypeRender(_bpy_types.Operator):
+    """Hide unselected render objects of same type as active by setting the hide render flag"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class JoinUVs(_bpy_types.Operator):
+    """Transfer UV Maps from active to selected objects (needs matching geometry)"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class MakeDupliFace(_bpy_types.Operator):
+    """Convert objects into instanced faces"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class OBJECT_OT_assign_property_defaults(_bpy_types.Operator):
+    """Assign the current values of custom properties as their defaults, for use as part of the rest pose state in NLA track mixing"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    @staticmethod
+    def assign_defaults(obj) -> None:
+        """
+
+        :param obj:
+        """
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class SelectCamera(_bpy_types.Operator):
+    """Select the active camera"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class SelectHierarchy(_bpy_types.Operator):
+    """Select object relative to the active objects position in the hierarchy"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class SelectPattern(_bpy_types.Operator):
+    """Select objects matching a naming pattern"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_property: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def draw(self, _context) -> None:
+        """
+
+        :param _context:
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    def invoke(self, context, event) -> None:
+        """
+
+        :param context:
+        :param event:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class ShapeTransfer(_bpy_types.Operator):
+    """Copy the active shape key of another selected object to this one"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class SubdivisionSet(_bpy_types.Operator):
+    """Sets a Subdivision Surface level (1 to 5)"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+class TransformsToDeltas(_bpy_types.Operator):
+    """Convert normal object transforms to delta transforms, any existing delta transforms will be included as well"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+    def transfer_location(self, obj) -> None:
+        """
+
+        :param obj:
+        """
+
+    def transfer_rotation(self, obj) -> None:
+        """
+
+        :param obj:
+        """
+
+    def transfer_scale(self, obj) -> None:
+        """
+
+        :param obj:
+        """
+
+class TransformsToDeltasAnim(_bpy_types.Operator):
+    """Convert object animation for normal transforms to delta transforms"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
