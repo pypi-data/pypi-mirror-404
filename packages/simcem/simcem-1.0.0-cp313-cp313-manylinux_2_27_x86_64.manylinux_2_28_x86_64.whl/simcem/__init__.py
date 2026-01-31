137b087c38acdb0ba480c1ddd009ab262b18aca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:05:12 +0000

    Add package simcem/1.0.0/simcem-1.0.0-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/simcem/simcem-1.0.0-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/simcem/__init__.py
@@ -0,0 +1,24 @@
+from simcem.core import *
+import simcem.kiln as kiln
+import os
+
+# Write some python repr and str implementations
+Isotope.__repr__ = lambda s: 'Isotope(symbol='+repr(s.symbol)+', name='+repr(s.name)+', Z='+repr(s.Z)+', N='+repr(s.N)+', mass='+repr(s.mass)+', mass_uncertainty='+repr(s.mass_uncertainty)+', abundance='+repr(s.abundance)+', category='+repr(s.category)+')'
+Isotope.__str__ = lambda s: 'Isotope(symbol='+repr(s.symbol)+', N='+repr(s.N)+', mass='+repr(s.mass)+')'
+
+Element.__repr__ = lambda s: 'Element(symbol='+repr(s.symbol)+', name='+repr(s.name)+', Z='+repr(s.Z)+', N='+repr(s.N)+', mass='+repr(s.mass)+', mass_uncertainty='+repr(s.mass_uncertainty)+', abundance='+repr(s.abundance)+', category='+repr(s.category)+", group="+repr(s.group)+', period='+repr(s.period)+', block='+repr(s.block)+', referenceComponentID='+repr(s.referenceComponentID)+', isotopes=IsotopeList('+repr([iso for iso in s])+'))'
+Element.__str__ = lambda s: 'Element(symbol='+repr(s.symbol)+', mass='+repr(s.mass)+', '+str(len(s))+' isotopes)'
+
+Components.__repr__ = lambda s: 'Components('+repr({k:v for k,v in s.items()})+')'
+Components.__str__ = Components.__repr__
+
+####
+Database.__str__ = lambda s: 'Database('+str(len(s.getElements()))+' elements, '+str(len(s.getComponents()))+' components)'
+
+Components.as_dict = lambda s: dict(s.items())
+
+def defaultDatabase():
+    import os
+    db = Database(os.path.join(os.path.dirname(__file__), 'free_database.xml'))
+    db.setHSLlib(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libhsl.so'))
+    return db
