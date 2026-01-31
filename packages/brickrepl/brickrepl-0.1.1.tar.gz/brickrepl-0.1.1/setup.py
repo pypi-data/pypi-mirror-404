5aaf3969b49a3a582111635575962ead770a0936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:05:35 +0000

    Add package brickrepl/0.1.1/brickrepl-0.1.1.tar.gz

---
+++ packages/brickrepl/brickrepl-0.1.1.tar.gz/brickrepl-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="brickrepl",
+    version="0.1.1",
+    description="Control LEGO Robot Inventor 51515 hubs from EV3 via RFCOMM using MAC address",
+    author="ollie",
+    packages=find_packages(),
+    install_requires=[],
+python_requires='>=3.5',
+)
