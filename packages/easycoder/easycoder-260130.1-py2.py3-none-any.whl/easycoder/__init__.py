90b67bf7372006b76376cbececa656e18931c223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:13 +0000

    Add package easycoder/260130.1/easycoder-260130.1-py2.py3-none-any.whl

---
+++ packages/easycoder/easycoder-260130.1-py2.py3-none-any.whl/easycoder/__init__.py
@@ -0,0 +1,18 @@
+'''EasyCoder for Python'''
+
+from .ec_border import *
+from .ec_classes import *
+from .ec_compiler import *
+from .ec_condition import *
+from .ec_core import *
+from .ec_gclasses import *
+from .ec_graphics import *
+from .ec_handler import *
+from .ec_keyboard import *
+from .ec_mqtt import *
+from .ec_program import *
+from .ec_psutil import *
+from .ec_timestamp import *
+from .ec_value import *
+
+__version__ = "260130.1"
