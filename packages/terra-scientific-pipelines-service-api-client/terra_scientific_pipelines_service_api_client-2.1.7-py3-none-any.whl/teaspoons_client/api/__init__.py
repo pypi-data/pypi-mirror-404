7ddafab1af117f29259c44aa336cdd06cffa9e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:40 +0000

    Add package terra-scientific-pipelines-service-api-client/2.1.7/terra_scientific_pipelines_service_api_client-2.1.7-py3-none-any.whl

---
+++ packages/terra-scientific-pipelines-service-api-client/terra_scientific_pipelines_service_api_client-2.1.7-py3-none-any.whl/teaspoons_client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from teaspoons_client.api.admin_api import AdminApi
+from teaspoons_client.api.jobs_api import JobsApi
+from teaspoons_client.api.pipeline_runs_api import PipelineRunsApi
+from teaspoons_client.api.pipelines_api import PipelinesApi
+from teaspoons_client.api.public_api import PublicApi
+from teaspoons_client.api.quotas_api import QuotasApi
+
