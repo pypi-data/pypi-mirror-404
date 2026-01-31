60966a1a5be55be43b4988c053cae1300d3bac89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:44:41 +0000

    Add package odaislib/1.0.7/odaislib-1.0.7.tar.gz

---
+++ packages/odaislib/odaislib-1.0.7.tar.gz/odaislib-1.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name="odaislib",
+    version="1.0.7",
+    author="Odai",
+    author_email="odaiobeidat00@gmail.com",
+    description="Facebook Authentication Library with multiple login methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.25.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords=["facebook", "login", "authentication", "api", "social"],
+)
