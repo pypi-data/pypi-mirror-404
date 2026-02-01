09426f557f7b8ce8f4e07a7a987df8a374d2be14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:02 +0000

    Add package architectonics/0.0.33/architectonics-0.0.33.tar.gz

---
+++ packages/architectonics/architectonics-0.0.33.tar.gz/architectonics-0.0.33/architectonics/infrastructure/entities/base_entity.py
@@ -0,0 +1,38 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.orm import as_declarative
+
+from architectonics.common.utils.utils import get_current_datetime
+from architectonics.core.models.base_model import BaseModel
+
+
+@as_declarative()
+class BaseEntity:
+    __abstract__ = True
+
+    id = Column(
+        String,
+        primary_key=True,
+        default=lambda: str(uuid.uuid4()),
+    )
+
+    created_at = Column(
+        DateTime,
+        default=get_current_datetime,
+    )
+
+    updated_at = Column(
+        DateTime,
+        default=get_current_datetime,
+        onupdate=get_current_datetime,
+    )
+
+    PK_FIELD = "id"
+
+    def to_model(self) -> BaseModel:
+        raise NotImplementedError()
+
+    @classmethod
+    def from_model(model: BaseModel) -> BaseEntity:
+        raise NotImplementedError()
