b6b52cecfa7778eee04212c79a4ab226fb716109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:20 +0000

    Add package passagemath-macaulay2/10.8.1a4/passagemath_macaulay2-10.8.1a4.tar.gz

---
+++ packages/passagemath-macaulay2/passagemath_macaulay2-10.8.1a4.tar.gz/passagemath_macaulay2-10.8.1a4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-macaulay2'],
+           recurse_packages=('sage', 'passagemath_macaulay2'),
+           spkgs=['macaulay2'],
+           package_data={},
+           py_limited_api=True)
