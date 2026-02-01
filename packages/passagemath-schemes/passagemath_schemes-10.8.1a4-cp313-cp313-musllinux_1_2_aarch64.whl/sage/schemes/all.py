09373e17cd3a0a53cffc775c29f53bfde1afae40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:43 +0000

    Add package passagemath-schemes/10.8.1a4/passagemath_schemes-10.8.1a4-cp313-cp313-musllinux_1_2_aarch64.whl

---
+++ packages/passagemath-schemes/passagemath_schemes-10.8.1a4-cp313-cp313-musllinux_1_2_aarch64.whl/sage/schemes/all.py
@@ -0,0 +1,41 @@
+# sage_setup: distribution = sagemath-schemes
+"""
+all.py -- export of schemes to Sage
+"""
+
+# *****************************************************************************
+#
+#   Sage: Open Source Mathematical Software
+#
+#       Copyright (C) 2005 William Stein <wstein@gmail.com>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  https://www.gnu.org/licenses/
+# *****************************************************************************
+
+from sage.schemes.all__sagemath_categories import *
+from sage.schemes.all__sagemath_polyhedra import *
+
+from sage.schemes.jacobians.all import *
+
+from sage.schemes.hyperelliptic_curves.all import *
+
+from sage.schemes.curves.all import *
+
+from sage.schemes.plane_conics.all import *
+
+from sage.schemes.elliptic_curves.all import *
+
+from sage.schemes.plane_quartics.all import *
+
+from sage.schemes.cyclic_covers.all import *
+
+from sage.schemes.berkovich.all import *
