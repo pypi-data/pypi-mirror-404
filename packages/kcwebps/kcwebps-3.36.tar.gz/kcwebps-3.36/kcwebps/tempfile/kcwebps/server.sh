999d7c07aa1788c10bf337c36898e9464887e412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:52:55 +0000

    Add package kcwebps/3.36/kcwebps-3.36.tar.gz

---
+++ packages/kcwebps/kcwebps-3.36.tar.gz/kcwebps-3.36/kcwebps/tempfile/kcwebps/server.sh
@@ -0,0 +1,6 @@
+chmod -R 777 /kcwebps
+cd /kcwebps
+kcwebps server -stop
+sleep 1
+pkill kcwebps
+nohup kcwebps --host 0.0.0.0 --port 39001 --processcount 4 --timeout 600 server -start > app/runtime/log/server.log 2>&1 &
