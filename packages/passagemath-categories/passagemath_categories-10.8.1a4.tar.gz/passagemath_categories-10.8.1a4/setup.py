bfbafaff77b9e0b57ec8426b02cf1f8e347eefd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:58 +0000

    Add package passagemath-categories/10.8.1a4/passagemath_categories-10.8.1a4.tar.gz

---
+++ packages/passagemath-categories/passagemath_categories-10.8.1a4.tar.gz/passagemath_categories-10.8.1a4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(
+    ['sagemath-categories'],
+    recurse_packages=('sage', 'passagemath_categories'),
+    interpreters=['Element', 'Python'],  # RDF uses gsl --> sagemath-modules
+    package_data={
+        "sage.data_structures": [
+            "bitset_intrinsics.h",
+            "pairing_heap.h",
+        ],
+        "sage.rings.finite_rings": [
+            "integer_mod_limits.h",
+        ],
+    })
