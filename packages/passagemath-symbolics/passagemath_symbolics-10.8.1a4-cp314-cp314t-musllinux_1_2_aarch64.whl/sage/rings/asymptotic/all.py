6a9901389a522f03bc7b1c25b6f0cb6cdec526bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:54 +0000

    Add package passagemath-symbolics/10.8.1a4/passagemath_symbolics-10.8.1a4-cp314-cp314t-musllinux_1_2_aarch64.whl

---
+++ packages/passagemath-symbolics/passagemath_symbolics-10.8.1a4-cp314-cp314t-musllinux_1_2_aarch64.whl/sage/rings/asymptotic/all.py
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-symbolics
+from sage.misc.lazy_import import lazy_import
+lazy_import('sage.rings.asymptotic.asymptotic_ring', 'AsymptoticRing')
+lazy_import('sage.rings.asymptotic.asymptotic_expansion_generators',
+            'asymptotic_expansions')
+del lazy_import
