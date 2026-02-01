0376e6964a13191682d773ee6ef0265ffddeccce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:50 +0000

    Add package passagemath-polyhedra/10.8.1a4/passagemath_polyhedra-10.8.1a4-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp312-cp312-musllinux_1_2_aarch64.whl/sage/numerical/backends/cvxpy_backend_test.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-polyhedra
+import pytest
+from sage.numerical.backends.generic_backend_test import GenericBackendTests
+from sage.numerical.backends.generic_backend import GenericBackend
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+
+@pytest.importorskip("cvxpy")
+class TestCVXPYBackend(GenericBackendTests):
+
+    @pytest.fixture
+    def backend(self) -> GenericBackend:
+        return MixedIntegerLinearProgram(solver="CVXPY").get_backend()
