3cc8f4038f23455d2962546cb4c52f47dcc38451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:28 +0000

    Add package Geode-Simplex/10.0.4/geode_simplex-10.0.4-cp311-cp311-win_amd64.whl

---
+++ packages/Geode-Simplex/geode_simplex-10.0.4-cp311-cp311-win_amd64.whl/geode_simplex/section_simplex.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_numerics as geode_numerics
+from geode_simplex.bin.geode_simplex_py_section import SimplexSectionLibrary
+from geode_simplex.bin.geode_simplex_py_section import section_simplex_remesh
+import opengeode as opengeode
+__all__: list[str] = ['SimplexSectionLibrary', 'geode_background', 'geode_common', 'geode_numerics', 'opengeode', 'section_simplex_remesh']
