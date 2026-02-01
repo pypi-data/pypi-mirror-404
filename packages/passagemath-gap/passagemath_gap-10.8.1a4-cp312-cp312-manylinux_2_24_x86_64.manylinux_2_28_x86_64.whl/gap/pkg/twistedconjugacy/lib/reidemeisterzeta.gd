ba413a44414949e4854b3de9c906a9ee0fbc08ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:18:20 +0000

    Add package passagemath-gap/10.8.1a4/passagemath_gap-10.8.1a4-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/passagemath-gap/passagemath_gap-10.8.1a4-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/gap/pkg/twistedconjugacy/lib/reidemeisterzeta.gd
@@ -0,0 +1,20 @@
+DeclareGlobalName( "ReidemeisterZetaCoefficients" );
+DeclareOperation(
+    "ReidemeisterZetaCoefficientsOp",
+    [ IsGroupHomomorphism, IsGroupHomomorphism ]
+);
+DeclareGlobalName( "IsRationalReidemeisterZeta" );
+DeclareOperation(
+    "IsRationalReidemeisterZetaOp",
+    [ IsGroupHomomorphism, IsGroupHomomorphism ]
+);
+DeclareGlobalName( "ReidemeisterZeta" );
+DeclareOperation(
+    "ReidemeisterZetaOp",
+    [ IsGroupHomomorphism, IsGroupHomomorphism ]
+);
+DeclareGlobalName( "PrintReidemeisterZeta" );
+DeclareOperation(
+    "PrintReidemeisterZetaOp",
+    [ IsGroupHomomorphism, IsGroupHomomorphism ]
+);
