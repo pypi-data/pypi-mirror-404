1abfcc15f9a36dc5b45e3d5a5969dbc41d2becca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:45 +0000

    Add package oh-my-ralph/0.3.0/oh_my_ralph-0.3.0.tar.gz

---
+++ packages/oh-my-ralph/oh_my_ralph-0.3.0.tar.gz/oh_my_ralph-0.3.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from oh_my_ralph import __version__
+
+setup(
+    name="oh-my-ralph",
+    version=__version__,
+    description="An opinionated orchestrator for Ralph Wiggum loops",
+    author="vivganes",
+    license="MIT",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vivganes/oh-my-ralph",
+    project_urls={
+        "Homepage": "https://github.com/vivganes/oh-my-ralph",
+        "Repository": "https://github.com/vivganes/oh-my-ralph"
+    },
+)
