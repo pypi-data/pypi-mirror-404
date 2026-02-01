de1eb1c5a5211f2b508e2c815ee57d608ec12cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:59 +0000

    Add package passagemath-database-cremona-ellcurve/10.8.1a4/passagemath_database_cremona_ellcurve-10.8.1a4.tar.gz

---
+++ packages/passagemath-database-cremona-ellcurve/passagemath_database_cremona_ellcurve-10.8.1a4.tar.gz/passagemath_database_cremona_ellcurve-10.8.1a4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-database-cremona-ellcurve'],
+           recurse_packages=('sage', 'passagemath_database_cremona_ellcurve'),
+           spkgs=['database_cremona_ellcurve'])
