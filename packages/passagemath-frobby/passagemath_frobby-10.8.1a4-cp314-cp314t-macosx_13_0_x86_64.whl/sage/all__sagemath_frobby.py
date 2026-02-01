2fb11d2879f3ae60afc943988d1f1a154318e437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:17:11 +0000

    Add package passagemath-frobby/10.8.1a4/passagemath_frobby-10.8.1a4-cp314-cp314t-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-frobby/passagemath_frobby-10.8.1a4-cp314-cp314t-macosx_13_0_x86_64.whl/sage/all__sagemath_frobby.py
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-frobby
+# delvewheel: patch
+
+from sage.all__sagemath_categories import *
+from sage.all__sagemath_graphs import *
+from sage.all__sagemath_singular import *
