88b1015803ece7d93003141faae3a9b9ef3dc443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:22 +0000

    Add package passagemath-linbox/10.8.1a4/passagemath_linbox-10.8.1a4.tar.gz

---
+++ packages/passagemath-linbox/passagemath_linbox-10.8.1a4.tar.gz/passagemath_linbox-10.8.1a4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-linbox'],
+           recurse_packages=('sage', 'passagemath_linbox'),
+           required_modules=('fflas-ffpack', 'givaro', 'gsl', 'linbox', 'cblas',
+                             'zlib'),
+           spkgs=['linbox', 'gsl'],
+           package_data={})
