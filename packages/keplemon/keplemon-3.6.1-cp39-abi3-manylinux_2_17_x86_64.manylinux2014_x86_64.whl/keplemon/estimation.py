ab42712060e8c4218e3ee21a03997268c4faeb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:19 +0000

    Add package keplemon/3.6.1/keplemon-3.6.1-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/keplemon/keplemon-3.6.1-cp39-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/keplemon/estimation.py
@@ -0,0 +1,19 @@
+from keplemon._keplemon.estimation import (  # type: ignore
+    Observation,
+    ObservationResidual,
+    BatchLeastSquares,
+    Covariance,
+    ObservationAssociation,
+    ObservationCollection,
+    CollectionAssociationReport,
+)
+
+__all__ = [
+    "Observation",
+    "ObservationResidual",
+    "BatchLeastSquares",
+    "Covariance",
+    "ObservationAssociation",
+    "ObservationCollection",
+    "CollectionAssociationReport",
+]
