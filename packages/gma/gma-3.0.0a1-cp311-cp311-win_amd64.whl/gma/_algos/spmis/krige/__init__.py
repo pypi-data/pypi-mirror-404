3e72cab5c3af6a0ee84a2b4489499063d8cf3e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:07:17 +0000

    Add package gma/3.0.0a1/gma-3.0.0a1-cp311-cp311-win_amd64.whl

---
+++ packages/gma/gma-3.0.0a1-cp311-cp311-win_amd64.whl/gma/_algos/spmis/krige/__init__.py
@@ -0,0 +1,4 @@
+
+# -*- coding: utf-8 -*-
+from .. import np, error, init_points, to_numeric_array, spatial
+from ..meatran import great_circle_distance
---
+++ packages/gma/gma-3.0.0a1-cp311-cp311-win_amd64.whl/gma/ana/math/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import correction, evaluation, kernel, prepro, smooth, stretch
+
