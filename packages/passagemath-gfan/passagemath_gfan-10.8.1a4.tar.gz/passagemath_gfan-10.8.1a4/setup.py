5769e961800bf93da8c60c809df494d2ab4596fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:12 +0000

    Add package passagemath-gfan/10.8.1a4/passagemath_gfan-10.8.1a4.tar.gz

---
+++ packages/passagemath-gfan/passagemath_gfan-10.8.1a4.tar.gz/passagemath_gfan-10.8.1a4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-gfan'],
+           recurse_packages=('sage', 'passagemath_gfan'),
+           spkgs=['gfan'],
+           package_data={},
+           py_limited_api=True)
