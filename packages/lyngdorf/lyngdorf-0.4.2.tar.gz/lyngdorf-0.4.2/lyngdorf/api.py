54200153bf3d2d68a5e81e22ffa3e951a973ea4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:29 +0000

    Add package lyngdorf/0.4.2/lyngdorf-0.4.2.tar.gz

---
+++ packages/lyngdorf/lyngdorf-0.4.2.tar.gz/lyngdorf-0.4.2/lyngdorf/api.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Automation Library for Lyngdorf receivers.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+import asyncio
+import contextlib
+import time
+import logging
+import attr
+import traceback
+
+from asyncio import timeout as asyncio_timeout
+from typing import Callable, Dict, Optional, cast, List
+
+from .const import (
+    DEFAULT_LYNGDORF_PORT,
+    RECONNECT_BACKOFF, RECONNECT_MAX_WAIT, RECONNECT_SCALE, MONITOR_INTERVAL,
+    Msg, LyngdorfModel
+)
+
+
+
+_LOGGER = logging.getLogger(__package__)
+
+
+class LyngdorfProtocol(asyncio.Protocol):
+    """Protocol for the Lyngdorf interface."""
+
+    def __init__(
+        self,
+        on_message: Callable[[str], None],
+        on_connection_lost: Callable[[], None],
+    ) -> None:
+        """Initialize the protocol."""
+        self._buffer = b""
+        self.transport: Optional[asyncio.Transport] = None
+        self._on_message = on_message
+        self._on_connection_lost = on_connection_lost
+
+    @property
+    def connected(self) -> bool:
+        """Return True if transport is connected."""
+        if self.transport is None:
+            return False
+        return not self.transport.is_closing()
+
+    def write(self, data: str) -> None:
+        """Write data to the transport."""
+        if self.transport is None or self.transport.is_closing():
+            return
+        self.transport.write(data.encode("utf-8"))
+
+    def close(self) -> None:
+        """Close the connection."""
+        if self.transport is not None:
+            self.transport.close()
+
+    def eof_received(self):
+        _LOGGER.info("Pipe closed")
+        self.close()
+        self._on_connection_lost()
+        return True
+
+    def data_received(self, data: bytes) -> None:
+        """Handle data received."""
+        self._buffer += data
+        while b"\r" in self._buffer:
+            line, _, self._buffer = self._buffer.partition(b"\r")
+            with contextlib.suppress(UnicodeDecodeError):
+                self._on_message(line.decode("utf-8"))
+
+    def connection_made(self, transport: asyncio.Transport) -> None:  # type: ignore
+        """Handle connection made."""
+        _LOGGER.debug("connection made")
+        self.transport = transport  # type: ignore
+
+    def connection_lost(self, exc: Optional[Exception]) -> None:
+        """Handle connection lost."""
+        self.close()
+        self._on_connection_lost()
+        return super().connection_lost(exc)
+
+
+class LyngdorfApi:
+    """Handle responses from the Lyngdorf interface."""
+
+    def __init__(self, host: str, model: LyngdorfModel):
+        """Initialize the client."""
+        self._connection_enabled = False
+        self.host = host
+        self._model: LyngdorfModel = model
+        self._connect_lock = asyncio.Lock()
+        self.host: str
+        self.timeout: float
+        self._connection_enabled: bool
+        self._healthy: Optional[bool] = attr.ib(
+            converter=attr.converters.optional(bool), default=None
+        )
+        self._last_message_time: float = -1.0
+        self._connect_lock: asyncio.Lock  # = attr.ib(default=attr.Factory(asyncio.Lock))
+        self._reconnect_task: Optional[asyncio.Task[None]] = None
+        self._monitor_handle: asyncio.TimerHandle
+        self._protocol: LyngdorfProtocol
+        self._callbacks: Dict[str, Callable] = {}
+        self._notification_callbacks: List = list()
+
+    async def async_connect(self) -> None:
+        """Connect to the receiver asynchronously."""
+        _LOGGER.debug("%s: connecting", self.host)
+        if self._connection_enabled:
+            return
+        await self._async_establish_connection()
+
+    async def _async_establish_connection(self) -> None:
+        """Establish a connection to the receiver."""
+        _LOGGER.info("%s: establishing connection", self.host)
+        loop = asyncio.get_event_loop()
+        try:
+            async with asyncio_timeout(2.0):
+                transport_protocol = await loop.create_connection(
+                    lambda: LyngdorfProtocol(
+                        on_connection_lost=self._handle_disconnected,
+                        on_message=self._process_event,
+                    ),
+                    self.host,
+                    DEFAULT_LYNGDORF_PORT,
+                )
+        except asyncio.TimeoutError as err:
+            _LOGGER.debug("%s: Timeout exception on connect", self.host)
+            raise asyncio.TimeoutError(f"TimeoutException: {err}", "connect") from err
+        except ConnectionRefusedError as err:
+            _LOGGER.debug("%s: Connection refused on connect", self.host, exc_info=True)
+            raise ConnectionRefusedError(
+                f"ConnectionRefusedError: {err}", "connect"
+            ) from err
+        self._protocol = cast(LyngdorfProtocol, transport_protocol[1])  # type: ignore
+        self._connection_enabled = True
+        self._last_message_time = time.monotonic()
+        self._schedule_monitor()
+        self._writeSetup()
+        _LOGGER.debug("%s: connection complete", self.host)
+
+    def _schedule_monitor(self) -> None:
+        """Start the monitor task."""
+        loop = asyncio.get_event_loop()
+        self._monitor_handle = loop.call_later(MONITOR_INTERVAL, self._monitor)
+
+    def _stop_monitor(self) -> None:
+        """Stop the monitor task."""
+        if self._monitor_handle is not None:
+            self._monitor_handle.cancel()
+            self._monitor_handle = None
+
+    def _monitor(self) -> None:
+        """Monitor the connection."""
+        time_since_response = time.monotonic() - self._last_message_time
+        if time_since_response > MONITOR_INTERVAL * 4:
+            _LOGGER.info(
+                "%s: Keep alive failed, disconnecting and reconnecting", self.host
+            )
+            if self._protocol is not None:
+                self._protocol.close()
+            self._handle_disconnected()
+            return
+
+        if time_since_response > MONITOR_INTERVAL and self._protocol:
+            # Keep the connection alive
+            self._writeCommand(f'{self._model.lookup_command(Msg.PING)}?')
+        self._schedule_monitor()
+
+    def _handle_disconnected(self) -> None:
+        """Handle disconnected."""
+        _LOGGER.debug("%s: disconnected", self.host)
+        self._protocol = None
+        self._stop_monitor()
+        if not self._connection_enabled:
+            return
+        self._reconnect_task = asyncio.create_task(self._async_reconnect())
+
+    async def async_disconnect(self) -> None:
+        """Close the connection to the receiver asynchronously."""
+        async with self._connect_lock:
+            self._connection_enabled = False
+            self._stop_monitor()
+            if self._reconnect_task is not None:
+                self._reconnect_task.cancel()
+                self._reconnect_task = None
+            if self._protocol is not None:
+                self._protocol.close()
+                self._protocol = None
+
+    async def _async_reconnect(self) -> None:
+        """Reconnect to the receiver asynchronously."""
+        backoff = RECONNECT_BACKOFF
+        _LOGGER.debug("Trying to reconnect")
+        while self._connection_enabled and not self.healthy:
+            _LOGGER.debug("Trying to reconnect...")
+            async with self._connect_lock:
+                try:
+                    await self._async_establish_connection()
+                except Exception:  # pylint: disable=broad-except
+                    _LOGGER.error(
+                        "%s: Unexpected exception on Lyngdorf reconnect",
+                        self.host,
+                        exc_info=True,
+                    )
+                else:
+                    _LOGGER.info("%s: Lyngdorf reconnected", self.host)
+                    return
+
+            await asyncio.sleep(backoff)
+            backoff = min(RECONNECT_MAX_WAIT, backoff * RECONNECT_SCALE)
+
+    def _writeSetup(self):
+        for cmd in self._model.setup_commands:
+            self._writeCommand(cmd)
+
+    def _writeCommand(self, command):
+        """Send a command to the receiver."""
+        self._protocol.write(f"!{command}\r")
+        _LOGGER.debug("%s send: '!%s'", self.host, command)
+
+    def power_on(self, enabled: bool):
+        if enabled:
+            self._writeCommand(f'{self._model.lookup_command(Msg.POWER_ON)}')
+        else:
+            self._writeCommand(f'{self._model.lookup_command(Msg.POWER_OFF)}')
+
+    def zone_b_power_on(self, enabled: bool):
+        if enabled:
+            self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_POWER_ON)}')
+        else:
+            self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_POWER_OFF)}')
+
+    def mute_enabled(self, mute: bool):
+        if mute:
+            self._writeCommand(f'{self._model.lookup_command(Msg.MUTE_ON)}')
+        else:
+            self._writeCommand(f'{self._model.lookup_command(Msg.MUTE_OFF)}')
+
+    def zone_b_mute_enabled(self, mute: bool):
+        if mute:
+            self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_MUTE_ON)}')
+        else:
+            self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_MUTE_OFF)}')
+
+    def volume_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.VOLUME)}+')
+
+    def volume_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.VOLUME)}-')
+
+    def zone_b_volume_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_VOLUME)}+')
+
+    def zone_b_volume_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.ZONE_B_VOLUME)}-')
+
+    def volume(self, volume: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.VOLUME)}({volume*10.0:.0f})")
+
+    def zone_b_volume(self, volume: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.VOLUME)}({volume*10.0:.0f})")
+
+    def change_source(self, source: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.SOURCE)}({source})")
+
+    def change_zone_b_source(self, zone_b_source: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.ZONE_B_SOURCE)}({zone_b_source})")
+
+    def change_sound_mode(self, sound_mode: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.AUDIO_MODE)}({sound_mode})")
+        
+    def change_hdmi_main_out(self, hdmi_index: int):
+        self._writeCommand(f"HDMIMAINOUT({hdmi_index})")
+        
+    def change_room_perfect_position(self, room_perfect_position_index: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.ROOM_PERFECT_POSITION)}({room_perfect_position_index})")
+    
+    def change_lipsync(self, lipsync: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.LIP_SYNC)}({lipsync})")
+    
+    def change_voicing(self, voicing: int):
+        self._writeCommand(f"{self._model.lookup_command(Msg.ROOM_PERFECT_VOICING)}({voicing})")
+        
+    def change_trim_bass(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_BASS)}({trim*10.0:.0f})")
+    
+    def change_trim_centre(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_CENTRE)}({trim*10.0:.0f})")
+        
+    def change_trim_height(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_HEIGHT)}({trim*10.0:.0f})")
+        
+    def change_trim_lfe(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_LFE)}({trim*10.0:.0f})")
+        
+    def change_trim_surround(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_SURROUND)}({trim*10.0:.0f})")
+        
+    def change_trim_treble(self, trim: float):
+        self._writeCommand(f"{self._model.lookup_command(Msg.TRIM_TREBLE_SET)}({trim*10.0:.0f})")
+        
+    def trim_bass_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_BASS)}+')
+
+    def trim_bass_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_BASS)}-')
+        
+    def trim_centre_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_CENTRE)}+')
+
+    def trim_centre_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_CENTRE)}-')
+    
+    def trim_height_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_HEIGHT)}+')
+
+    def trim_height_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_HEIGHT)}-')
+
+    def trim_lfe_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_LFE)}+')
+
+    def trim_lfe_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_LFE)}-')
+        
+    def trim_surround_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_SURROUND)}+')
+
+    def trim_surround_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_SURROUND)}-')
+        
+    def trim_treble_up(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_TREBLE_SET)}+')
+
+    def trim_treble_down(self):
+        self._writeCommand(f'{self._model.lookup_command(Msg.TRIM_TREBLE_SET)}-')
+        
+    def _process_event(self, message: str) -> None:
+        """Process a realtime event."""
+
+        _LOGGER.debug("%s recv: '%s'", self.host, message)
+        # print("\r"+message+"\r")
+        self._last_message_time = time.monotonic()
+        if message.startswith("!"):
+            cmd: str = ""
+            first: str = ""
+            second: str = ""
+            message = message[1:]
+            if 1 < message.find("(") < message.find(")"):
+                cmd = message[: message.find("(")]
+                first = message[1 + message.find("(") : message.find(")")]
+                second = message[1 + message.find(")") :]
+            else:
+                cmd = message
+
+            if cmd == self._model.lookup_command(Msg.PONG):
+                return
+
+            if len(second) > 0 and second.startswith('"') and second.endswith('"'):
+                second = second[1:-1]
+            asyncio.create_task(self._async_run_callbacks(cmd, first, second))
+            asyncio.create_task(self._notify_notification_callbacks())
+
+    def register_notification_callback(self, callback: Callable[[], None]) -> None:
+        self._notification_callbacks.append(callback)
+
+    def un_register_notification_callback(self, callback: Callable[[], None]) -> None:
+        self._notification_callbacks.remove(callback)
+
+    async def _notify_notification_callbacks(self) -> None:
+        for callback in self._notification_callbacks:
+            try:
+                callback()
+            except Exception as err:
+                # We don't want a single bad callback to trip up the
+                # whole system and prevent further execution
+                # TIM. TODO. need to log the stack trace of the error found here, as at the moment v hard to find errors
+
+                _LOGGER.error(
+                    "%s: Event callback caused an unhandled exception '%s' (%s)",
+                    self.host,
+                    traceback.format_exc(),
+                )
+
+    def register_callback(
+        self, command: str, callback: Callable[[str, str], None]
+    ) -> None:
+        """Register a callback handler for an event type."""
+
+        if command not in self._callbacks.keys():
+            self._callbacks[command] = []
+        self._callbacks[command].append(callback)
+
+    async def _async_run_callbacks(
+        self, command: str, param1: str, param2: str
+    ) -> None:
+        """Handle triggering the registered callbacks for the event."""
+        if command in self._callbacks.keys():
+            for callback in self._callbacks[command]:
+                try:
+                    # _LOGGER.debug("Command %s callback (%s, %s) calling %s", command, param1, param2, callback)
+                    callback(param1, param2)
+                except Exception as err:  # pylint: disable=broad-except
+                    # We don't want a single bad callback to trip up the
+                    # whole system and prevent further execution
+                    # TIM. TODO. need to log the stack trace of the error found here, as at the moment v hard to find errors
+
+                    _LOGGER.error(
+                        "%s: Event callback caused an unhandled exception '%s' for Command %s callback (%s, %s) calling %s",
+                        self.host,
+                        traceback.format_exc(),
+                        command,
+                        param1,
+                        param2,
+                        callback,
+                    )
+
+    @property
+    def connected(self) -> bool:
+        """Return True if telnet connection is enabled."""
+        return self._connection_enabled
+
+    @property
+    def healthy(self) -> bool:
+        """Return True if the connection is healthy."""
+        return self._protocol is not None and self._protocol.connected
---
+++ packages/lyngdorf/lyngdorf-0.4.2.tar.gz/lyngdorf-0.4.2/lyngdorf/const.py
@@ -0,0 +1,357 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This module inherits constants for Lyngdorf receivers.
+
+:license: MIT, see LICENSE for more details.
+"""
+
+import attr
+from enum import Enum
+from typing import Dict
+from dataclasses import dataclass
+
+ATTR_SETATTR = [attr.setters.validate, attr.setters.convert]
+
+DEFAULT_LYNGDORF_PORT = 84
+RECONNECT_BACKOFF = 0.5 # half a second to wait for the first reconnect
+RECONNECT_SCALE = 2.5 # each reconnect attempt waits this times longer than the previous one
+RECONNECT_MAX_WAIT = 30.0 # Reconnect tasks will wait this many seconds at a maximum between each attempt
+MONITOR_INTERVAL = 90 # 90 seconds between PING commands
+
+POWER_ON = "1"
+POWER_OFF = "0"
+
+
+MP60_VIDEO_INPUTS = {
+    0: "None",
+    1: "HDMI 1",
+    2: "HDMI 2",
+    3: "HDMI 3",
+    4: "HDMI 4",
+    5: "HDMI 5",
+    6: "HDMI 6",
+    7: "HDMI 7",
+    8: "HDMI 8",
+    9: "Internal",
+}
+
+MP60_VIDEO_OUTPUTS = {0: "None", 1: "HDMI Out 1", 2: "HDMI Out 2", 3: "HDBT Out"}
+
+MP60_AUDIO_INPUTS = {
+    0: "None",
+    1: "HDMI",
+    3: "Spdif 1 (Opt.)",
+    4: "Spdif 2 (Opt.)",
+    5: "Spdif 3 (Opt.)",
+    6: "Spdif 4 (Opt.)",
+    7: "Spdif 5 (AES)",
+    8: "Spdif 6 (Coax)",
+    9: "Spdif 7 (Coax)",
+    10: "Spdif 8 (Coax)",
+    11: "Internal Player",
+    12: "USB",
+    20: "16-Channel (optional AES module)",
+    21: "16-Channel 2.0 (optional AES module)",
+    22: "16-Channel 5.1 (optional AES module)",
+    23: "16-Channel 7.1 (optional AES module)",
+    24: "Audio Return Channel",
+    35: "vTuner",
+    36: "TIDAL",
+    37: "Spotify",
+    38: "Airplay",
+    39: "Roon",
+    40: "DLNA",
+    41: "Storage",
+    42: "airable"
+}
+
+MP60_ROOM_PERFECT_POSITIONS = {
+    0: "Bypass",
+    1: "Focus 1",
+    2: "Focus 2",
+    3: "Focus 3",
+    4: "Focus 4",
+    9: "Global"
+}
+
+MP60_STREAM_TYPES = {
+    0: "None",
+    1: "vTuner",
+    2: "Spotify",
+    3: "AirPlay",
+    4: "UPnP",
+    5: "Storage",
+    6: "Roon ready",
+    7: "Unknown",
+}
+
+# TDAI-1120 Constants
+TDAI1120_STREAM_TYPES = {
+    0: "None",
+    1: "vTuner",
+    2: "Spotify",
+    3: "AirPlay",
+    4: "uPnP",
+    5: "USB File",
+    6: "Roon Ready",
+    7: "Bluetooth",
+    8: "GoogleCast",
+    9: "Unknown",
+}
+
+TDAI1120_ROOM_PERFECT_POSITIONS = {
+    0: "Bypass",
+    1: "Focus 1",
+    2: "Focus 2",
+    3: "Focus 3",
+    4: "Focus 4",
+    5: "Focus 5",
+    6: "Focus 6",
+    7: "Focus 7",
+    8: "Focus 8",
+    9: "Global"
+}
+    
+Msg = Enum('Msg',
+    ['DEVICE','VERBOSE', 'PING','PONG',
+     'POWER', 'POWER_ON', 'POWER_OFF', 'VOLUME', 'MUTE', 'MUTE_ON', "MUTE_OFF", 'SOURCES_COUNT', 'SOURCE',
+     'ZONE_B_POWER', 'ZONE_B_POWER_ON', 'ZONE_B_POWER_OFF', 'ZONE_B_VOLUME', 'ZONE_B_MUTE_ON', "ZONE_B_MUTE_OFF", 'ZONE_B_SOURCES_COUNT', 'ZONE_B_SOURCE',
+     'AUDIO_IN', 'ZONE_B_AUDIO_IN', 'VIDEO_IN', 'STREAM_TYPE', 'ZONE_B_STREAM_TYPE',
+     'VIDEO_TYPE', 'AUDIO_TYPE', 'AUDIO_MODES_COUNT', 'AUDIO_MODE',
+     'ROOM_PERFECT_POSITIONS_COUNT', 'ROOM_PERFECT_POSITION', 'ROOM_PERFECT_VOICINGS_COUNT', 'ROOM_PERFECT_VOICING',
+     'LIP_SYNC', 'LIP_SYNC_MIN_MAX',
+     'TRIM_BASS', 'TRIM_CENTRE', 'TRIM_HEIGHT', 'TRIM_LFE', 'TRIM_SURROUND', 'TRIM_TREBLE', 'TRIM_TREBLE_SET'
+     ]
+    )
+
+
+
+MP60_MESSAGES: Dict[Msg, str] = {
+    Msg.DEVICE: "DEVICE",
+    Msg.VERBOSE: "VERB",
+    Msg.PING: "PING",
+    Msg.PONG: "PONG",
+    Msg.POWER: "POWER",
+    Msg.POWER_ON: "POWERONMAIN",
+    Msg.POWER_OFF: "POWEROFFMAIN",
+    Msg.VOLUME: "VOL",
+    Msg.MUTE: "MUTE",
+    Msg.MUTE_ON: "MUTEON",
+    Msg.MUTE_OFF: "MUTEOFF",
+    Msg.SOURCES_COUNT: "SRCCOUNT",
+    Msg.SOURCE: "SRC",
+    Msg.AUDIO_IN: "AUDIN",
+    Msg.STREAM_TYPE: "STREAMTYPE",
+    Msg.VIDEO_IN: "VIDIN",
+    Msg.AUDIO_MODES_COUNT: "AUDMODECOUNT",
+    Msg.AUDIO_MODE: "AUDMODE",
+    Msg.AUDIO_TYPE: "AUDTYPE",
+    Msg.VIDEO_TYPE: "VIDTYPE",
+    Msg.ROOM_PERFECT_POSITIONS_COUNT: "RPFOCCOUNT",
+    Msg.ROOM_PERFECT_POSITION: "RPFOC",
+    Msg.ROOM_PERFECT_VOICINGS_COUNT: "RPVOICOUNT",
+    Msg.ROOM_PERFECT_VOICING: "RPVOI",
+    Msg.LIP_SYNC: "LIPSYNC",
+    Msg.LIP_SYNC_MIN_MAX: "LIPSYNCRANGE",
+    Msg.ZONE_B_POWER: "POWERZONE2",
+    Msg.ZONE_B_POWER_ON: "POWERONZONE2",
+    Msg.ZONE_B_POWER_OFF: "POWEROFFZONE2",
+    Msg.ZONE_B_VOLUME: "ZVOL",
+    Msg.ZONE_B_MUTE_ON: "ZMUTEON",
+    Msg.ZONE_B_MUTE_OFF: "ZMUTEOFF",
+    Msg.ZONE_B_SOURCES_COUNT: "ZSRCCOUNT",
+    Msg.ZONE_B_SOURCE: "ZSRC",
+    Msg.ZONE_B_AUDIO_IN: "ZAUDIN",
+    Msg.ZONE_B_STREAM_TYPE: "ZSTREAMTYPE",
+    Msg.TRIM_BASS: "TRIMBASS",
+    Msg.TRIM_CENTRE: "TRIMCENTER",
+    Msg.TRIM_HEIGHT: "TRIMHEIGHT",
+    Msg.TRIM_LFE: "TRIMLFE",
+    Msg.TRIM_SURROUND: "TRIMSURRS",
+    Msg.TRIM_TREBLE: "TRIMTREBLE",
+    Msg.TRIM_TREBLE_SET: "TRIMTREB"
+}
+
+TDAI1120_MESSAGES: Dict[Msg, str] = {
+    Msg.DEVICE: "DEVICE",
+    Msg.VERBOSE: "VERB",
+    Msg.PING: "PING",
+    Msg.PONG: "PONG",
+    Msg.POWER: "PWR",
+    Msg.POWER_ON: "ON",
+    Msg.POWER_OFF: "OFF",
+    Msg.VOLUME: "VOL",
+    Msg.MUTE: "MUTE",
+    Msg.MUTE_ON: "MUTEON",
+    Msg.MUTE_OFF: "MUTEOFF",
+    Msg.SOURCES_COUNT: "SRCCOUNT",
+    Msg.SOURCE: "SRC",
+    Msg.STREAM_TYPE: "STREAMTYPE",
+    Msg.ROOM_PERFECT_POSITIONS_COUNT: "RPCOUNT",
+    Msg.ROOM_PERFECT_POSITION: "RP",
+    Msg.ROOM_PERFECT_VOICINGS_COUNT: "VOICOUNT",
+    Msg.ROOM_PERFECT_VOICING: "VOI",
+    Msg.TRIM_BASS: "BASS",
+    Msg.TRIM_TREBLE: "TREBLE",
+}
+ 
+
+MP60_SETUP_MESSAGES = [
+    "VERB(1)",
+    "DEVICE?",
+    "POWER?",
+    "POWERZONE2?",
+    "AUDMODEL?",
+    "SRCS?",
+    "ZSRCS?",
+    "RPFOCS?",
+    "RPVOIS?",
+
+    "AUDMODE?",
+    "SRC?",
+    "ZSRC?",
+    "RPFOC?",
+    "RPVOI?",
+    "VIDTYPE?",
+    "STREAMTYPE?",
+    "LIPSYNC?"
+    "ZSTREAMTYPE?",
+
+    "AUDIN?",
+    "VIDIN?",
+    "AUDTYPE?",
+    "VOL?",
+    "ZVOL?",
+    "MUTE?",
+    "ZMUTE?",
+
+    "TRIMBASS?",
+    "TRIMCENTER?",
+    "TRIMHEIGHT?",
+    "TRIMLFE?",
+    "TRIMSURRS?",
+    "TRIMTREB?"
+]
+
+TDAI1120_SETUP_MESSAGES = [
+    "VERB(1)",
+    "DEVICE?",
+    "PWR?",
+    "SRCLIST?",
+    "RPLIST?",
+    "VOILIST?",
+
+    "SRC?",
+    "RP?",
+    "VOI?",
+    "STREAMTYPE?",
+
+    "VOL?",
+    "MUTE?",
+
+    "BASS?",
+    "TREBLE?",
+    "BAL?"
+]
+
+
+# @dataclass
+# class LyngdorfModelMixin:
+#     _model: str
+#     _manufacterer: str
+#     _commands: Dict[Msg, str]
+#     _setup_commands: list[str]
+    
+#     @property
+#     def model(self) -> str:
+#         return self._model
+    
+#     @property
+#     def manufacturer(self) -> str:
+#         return self._manufacterer
+    
+#     @property
+#     def setup_commands(self) -> list[str]:
+#         return self._setup_commands
+    
+#     @property
+#     def commands(self) -> Dict[Msg, str]:
+#         return self._commands
+    
+#     def lookup_command(self, key: Msg) -> str:
+#         return self.commands[key]
+@dataclass
+class LyngdorfModelMixin:
+    _model: str
+    _manufacterer: str
+    _commands: Dict[Msg, str]
+    _setup_commands: list[str]
+    
+    @property
+    def model(self) -> str:
+        return self._model
+    
+    @property
+    def manufacturer(self) -> str:
+        return self._manufacterer
+    
+    @property
+    def setup_commands(self) -> list[str]:
+        return self._setup_commands
+    
+    def lookup_command(self, key: Msg) -> str:
+        return self._commands[key]
+    
+
+class LyngdorfModel(LyngdorfModelMixin, Enum):
+    MP_60 = "mp-60", "Lyngdorf", MP60_MESSAGES, MP60_SETUP_MESSAGES
+    TDAI_1120 = "tdai-1120", "Lyngdorf", TDAI1120_MESSAGES, TDAI1120_SETUP_MESSAGES
+
+
+# RESPONSES = {
+#     "AUDIN": "Selected audio input",
+#     "AUDMODE": "Audio processing mode",
+#     "AUDMODECOUNT": "List of audio processing modes",
+#     "AUDTYPE": "String describing the current audio input type",
+#     "DEFVOL": "Default volume setting",
+#     "DEVICE": "Device Name",
+#     "DTSDIALOG": "DTS Dialog Control",
+#     "DTSDIALOGAVAILABLE": "Current availability of DTS Dialog Control",
+#     "HDMIMAINOUT": "Which HDMI output is used for main out",
+#     "LIPSYNC": 
+#     "LOUDNESS": "Loudness status",
+#     "MAXVOL": "Maximum volume setting",
+#     "POWER": "Power status",
+#     "POWERZONE2": "Power status for Zone B",
+#     "MUTEON": "Mute is ON",
+#     "MUTEOFF": "Mute is OFF",
+#     "RPFOC": "RoomPerfect position",
+#     "RPFOCCOUNT": "Available RoomPerfect positions",
+#     "RPVOI": "Active voicing",
+#     "RPVOICOUNT": "List of available voicings",
+#     "SRCOFF": "Source volume offset for current source",
+#     "SRCCOUNT": "List of available sources",
+#     "STANDBYLEVEL": "Current setting for standby level",
+#     "STREAMTYPE": "Type of the current network player source",
+#     "TRIMBASS": "Bass level trim (10 = 1dB)",
+#     "TRIMCENTER": "Center channel level trim (10 = 1dB)",
+#     "TRIMHEIGHT": "Height channels level trim (10 = 1dB)",
+#     "TRIMLFE": "LFE channel level trim (10 = 1dB)",
+#     "TRIMSURRS": "Surround channels level trim (10 = 1dB)",
+#     "TRIMTREB": "Treble level trim (10 = 1dB)",
+#     "VERB": "Verbosity level of active interface.",
+#     "VIDIN": "Selected video input",
+#     "VIDTYPE": "Current video input",
+#     "VOL": "Main Volume",
+#     "ZAUDIN": "Zone B audio input. (See list)",
+#     "ZSRCCOUNT": "List of available Zone B sources",
+#     "ZSTREAMTYPE": "Type of the network player when playing on Zone B",
+#     "ZVOL": "Zone B volume",
+#     "SRC": "Active source",
+#     "ZSRC": "Zone B source",
+#     "ZMUTEON": "Zone B Mute is ON",
+#     "ZMUTEOFF": "Zone B Mute is OFF",
+# }
+
+# RESPONSES_KEYS_ALL = sorted(set(RESPONSES.keys()))
