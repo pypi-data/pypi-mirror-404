e7fd7b0a0362add15514a7ac1283b624fba2b24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:32 +0000

    Add package pactus-grpc/1.11.1/pactus_grpc-1.11.1-py3-none-any.whl

---
+++ packages/pactus-grpc/pactus_grpc-1.11.1-py3-none-any.whl/pactus_grpc/network_pb2.pyi
@@ -0,0 +1,173 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Direction(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    DIRECTION_UNKNOWN: _ClassVar[Direction]
+    DIRECTION_INBOUND: _ClassVar[Direction]
+    DIRECTION_OUTBOUND: _ClassVar[Direction]
+DIRECTION_UNKNOWN: Direction
+DIRECTION_INBOUND: Direction
+DIRECTION_OUTBOUND: Direction
+
+class GetNetworkInfoRequest(_message.Message):
+    __slots__ = ()
+    ONLY_CONNECTED_FIELD_NUMBER: _ClassVar[int]
+    only_connected: bool
+    def __init__(self, only_connected: _Optional[bool] = ...) -> None: ...
+
+class GetNetworkInfoResponse(_message.Message):
+    __slots__ = ()
+    NETWORK_NAME_FIELD_NUMBER: _ClassVar[int]
+    CONNECTED_PEERS_COUNT_FIELD_NUMBER: _ClassVar[int]
+    CONNECTED_PEERS_FIELD_NUMBER: _ClassVar[int]
+    METRIC_INFO_FIELD_NUMBER: _ClassVar[int]
+    network_name: str
+    connected_peers_count: int
+    connected_peers: _containers.RepeatedCompositeFieldContainer[PeerInfo]
+    metric_info: MetricInfo
+    def __init__(self, network_name: _Optional[str] = ..., connected_peers_count: _Optional[int] = ..., connected_peers: _Optional[_Iterable[_Union[PeerInfo, _Mapping]]] = ..., metric_info: _Optional[_Union[MetricInfo, _Mapping]] = ...) -> None: ...
+
+class GetNodeInfoRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class GetNodeInfoResponse(_message.Message):
+    __slots__ = ()
+    MONIKER_FIELD_NUMBER: _ClassVar[int]
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    PEER_ID_FIELD_NUMBER: _ClassVar[int]
+    STARTED_AT_FIELD_NUMBER: _ClassVar[int]
+    REACHABILITY_FIELD_NUMBER: _ClassVar[int]
+    SERVICES_FIELD_NUMBER: _ClassVar[int]
+    SERVICES_NAMES_FIELD_NUMBER: _ClassVar[int]
+    LOCAL_ADDRS_FIELD_NUMBER: _ClassVar[int]
+    PROTOCOLS_FIELD_NUMBER: _ClassVar[int]
+    CLOCK_OFFSET_FIELD_NUMBER: _ClassVar[int]
+    CONNECTION_INFO_FIELD_NUMBER: _ClassVar[int]
+    ZMQ_PUBLISHERS_FIELD_NUMBER: _ClassVar[int]
+    CURRENT_TIME_FIELD_NUMBER: _ClassVar[int]
+    moniker: str
+    agent: str
+    peer_id: str
+    started_at: int
+    reachability: str
+    services: int
+    services_names: str
+    local_addrs: _containers.RepeatedScalarFieldContainer[str]
+    protocols: _containers.RepeatedScalarFieldContainer[str]
+    clock_offset: float
+    connection_info: ConnectionInfo
+    zmq_publishers: _containers.RepeatedCompositeFieldContainer[ZMQPublisherInfo]
+    current_time: int
+    def __init__(self, moniker: _Optional[str] = ..., agent: _Optional[str] = ..., peer_id: _Optional[str] = ..., started_at: _Optional[int] = ..., reachability: _Optional[str] = ..., services: _Optional[int] = ..., services_names: _Optional[str] = ..., local_addrs: _Optional[_Iterable[str]] = ..., protocols: _Optional[_Iterable[str]] = ..., clock_offset: _Optional[float] = ..., connection_info: _Optional[_Union[ConnectionInfo, _Mapping]] = ..., zmq_publishers: _Optional[_Iterable[_Union[ZMQPublisherInfo, _Mapping]]] = ..., current_time: _Optional[int] = ...) -> None: ...
+
+class ZMQPublisherInfo(_message.Message):
+    __slots__ = ()
+    TOPIC_FIELD_NUMBER: _ClassVar[int]
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    HWM_FIELD_NUMBER: _ClassVar[int]
+    topic: str
+    address: str
+    hwm: int
+    def __init__(self, topic: _Optional[str] = ..., address: _Optional[str] = ..., hwm: _Optional[int] = ...) -> None: ...
+
+class PeerInfo(_message.Message):
+    __slots__ = ()
+    STATUS_FIELD_NUMBER: _ClassVar[int]
+    MONIKER_FIELD_NUMBER: _ClassVar[int]
+    AGENT_FIELD_NUMBER: _ClassVar[int]
+    PEER_ID_FIELD_NUMBER: _ClassVar[int]
+    CONSENSUS_KEYS_FIELD_NUMBER: _ClassVar[int]
+    CONSENSUS_ADDRESSES_FIELD_NUMBER: _ClassVar[int]
+    SERVICES_FIELD_NUMBER: _ClassVar[int]
+    LAST_BLOCK_HASH_FIELD_NUMBER: _ClassVar[int]
+    HEIGHT_FIELD_NUMBER: _ClassVar[int]
+    LAST_SENT_FIELD_NUMBER: _ClassVar[int]
+    LAST_RECEIVED_FIELD_NUMBER: _ClassVar[int]
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    DIRECTION_FIELD_NUMBER: _ClassVar[int]
+    PROTOCOLS_FIELD_NUMBER: _ClassVar[int]
+    TOTAL_SESSIONS_FIELD_NUMBER: _ClassVar[int]
+    COMPLETED_SESSIONS_FIELD_NUMBER: _ClassVar[int]
+    METRIC_INFO_FIELD_NUMBER: _ClassVar[int]
+    OUTBOUND_HELLO_SENT_FIELD_NUMBER: _ClassVar[int]
+    status: int
+    moniker: str
+    agent: str
+    peer_id: str
+    consensus_keys: _containers.RepeatedScalarFieldContainer[str]
+    consensus_addresses: _containers.RepeatedScalarFieldContainer[str]
+    services: int
+    last_block_hash: str
+    height: int
+    last_sent: int
+    last_received: int
+    address: str
+    direction: Direction
+    protocols: _containers.RepeatedScalarFieldContainer[str]
+    total_sessions: int
+    completed_sessions: int
+    metric_info: MetricInfo
+    outbound_hello_sent: bool
+    def __init__(self, status: _Optional[int] = ..., moniker: _Optional[str] = ..., agent: _Optional[str] = ..., peer_id: _Optional[str] = ..., consensus_keys: _Optional[_Iterable[str]] = ..., consensus_addresses: _Optional[_Iterable[str]] = ..., services: _Optional[int] = ..., last_block_hash: _Optional[str] = ..., height: _Optional[int] = ..., last_sent: _Optional[int] = ..., last_received: _Optional[int] = ..., address: _Optional[str] = ..., direction: _Optional[_Union[Direction, str]] = ..., protocols: _Optional[_Iterable[str]] = ..., total_sessions: _Optional[int] = ..., completed_sessions: _Optional[int] = ..., metric_info: _Optional[_Union[MetricInfo, _Mapping]] = ..., outbound_hello_sent: _Optional[bool] = ...) -> None: ...
+
+class ConnectionInfo(_message.Message):
+    __slots__ = ()
+    CONNECTIONS_FIELD_NUMBER: _ClassVar[int]
+    INBOUND_CONNECTIONS_FIELD_NUMBER: _ClassVar[int]
+    OUTBOUND_CONNECTIONS_FIELD_NUMBER: _ClassVar[int]
+    connections: int
+    inbound_connections: int
+    outbound_connections: int
+    def __init__(self, connections: _Optional[int] = ..., inbound_connections: _Optional[int] = ..., outbound_connections: _Optional[int] = ...) -> None: ...
+
+class MetricInfo(_message.Message):
+    __slots__ = ()
+    class MessageSentEntry(_message.Message):
+        __slots__ = ()
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        key: int
+        value: CounterInfo
+        def __init__(self, key: _Optional[int] = ..., value: _Optional[_Union[CounterInfo, _Mapping]] = ...) -> None: ...
+    class MessageReceivedEntry(_message.Message):
+        __slots__ = ()
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        key: int
+        value: CounterInfo
+        def __init__(self, key: _Optional[int] = ..., value: _Optional[_Union[CounterInfo, _Mapping]] = ...) -> None: ...
+    TOTAL_INVALID_FIELD_NUMBER: _ClassVar[int]
+    TOTAL_SENT_FIELD_NUMBER: _ClassVar[int]
+    TOTAL_RECEIVED_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_SENT_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_RECEIVED_FIELD_NUMBER: _ClassVar[int]
+    total_invalid: CounterInfo
+    total_sent: CounterInfo
+    total_received: CounterInfo
+    message_sent: _containers.MessageMap[int, CounterInfo]
+    message_received: _containers.MessageMap[int, CounterInfo]
+    def __init__(self, total_invalid: _Optional[_Union[CounterInfo, _Mapping]] = ..., total_sent: _Optional[_Union[CounterInfo, _Mapping]] = ..., total_received: _Optional[_Union[CounterInfo, _Mapping]] = ..., message_sent: _Optional[_Mapping[int, CounterInfo]] = ..., message_received: _Optional[_Mapping[int, CounterInfo]] = ...) -> None: ...
+
+class CounterInfo(_message.Message):
+    __slots__ = ()
+    BYTES_FIELD_NUMBER: _ClassVar[int]
+    BUNDLES_FIELD_NUMBER: _ClassVar[int]
+    bytes: int
+    bundles: int
+    def __init__(self, bytes: _Optional[int] = ..., bundles: _Optional[int] = ...) -> None: ...
+
+class PingRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class PingResponse(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
---
+++ packages/pactus-grpc/pactus_grpc-1.11.1-py3-none-any.whl/pactus_grpc/transaction_pb2.pyi
@@ -0,0 +1,278 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class PayloadType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    PAYLOAD_TYPE_UNSPECIFIED: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_TRANSFER: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_BOND: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_SORTITION: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_UNBOND: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_WITHDRAW: _ClassVar[PayloadType]
+    PAYLOAD_TYPE_BATCH_TRANSFER: _ClassVar[PayloadType]
+
+class TransactionVerbosity(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = ()
+    TRANSACTION_VERBOSITY_DATA: _ClassVar[TransactionVerbosity]
+    TRANSACTION_VERBOSITY_INFO: _ClassVar[TransactionVerbosity]
+PAYLOAD_TYPE_UNSPECIFIED: PayloadType
+PAYLOAD_TYPE_TRANSFER: PayloadType
+PAYLOAD_TYPE_BOND: PayloadType
+PAYLOAD_TYPE_SORTITION: PayloadType
+PAYLOAD_TYPE_UNBOND: PayloadType
+PAYLOAD_TYPE_WITHDRAW: PayloadType
+PAYLOAD_TYPE_BATCH_TRANSFER: PayloadType
+TRANSACTION_VERBOSITY_DATA: TransactionVerbosity
+TRANSACTION_VERBOSITY_INFO: TransactionVerbosity
+
+class GetTransactionRequest(_message.Message):
+    __slots__ = ()
+    ID_FIELD_NUMBER: _ClassVar[int]
+    VERBOSITY_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    verbosity: TransactionVerbosity
+    def __init__(self, id: _Optional[str] = ..., verbosity: _Optional[_Union[TransactionVerbosity, str]] = ...) -> None: ...
+
+class GetTransactionResponse(_message.Message):
+    __slots__ = ()
+    BLOCK_HEIGHT_FIELD_NUMBER: _ClassVar[int]
+    BLOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    TRANSACTION_FIELD_NUMBER: _ClassVar[int]
+    block_height: int
+    block_time: int
+    transaction: TransactionInfo
+    def __init__(self, block_height: _Optional[int] = ..., block_time: _Optional[int] = ..., transaction: _Optional[_Union[TransactionInfo, _Mapping]] = ...) -> None: ...
+
+class CalculateFeeRequest(_message.Message):
+    __slots__ = ()
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    PAYLOAD_TYPE_FIELD_NUMBER: _ClassVar[int]
+    FIXED_AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    amount: int
+    payload_type: PayloadType
+    fixed_amount: bool
+    def __init__(self, amount: _Optional[int] = ..., payload_type: _Optional[_Union[PayloadType, str]] = ..., fixed_amount: _Optional[bool] = ...) -> None: ...
+
+class CalculateFeeResponse(_message.Message):
+    __slots__ = ()
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    amount: int
+    fee: int
+    def __init__(self, amount: _Optional[int] = ..., fee: _Optional[int] = ...) -> None: ...
+
+class BroadcastTransactionRequest(_message.Message):
+    __slots__ = ()
+    SIGNED_RAW_TRANSACTION_FIELD_NUMBER: _ClassVar[int]
+    signed_raw_transaction: str
+    def __init__(self, signed_raw_transaction: _Optional[str] = ...) -> None: ...
+
+class BroadcastTransactionResponse(_message.Message):
+    __slots__ = ()
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    def __init__(self, id: _Optional[str] = ...) -> None: ...
+
+class GetRawTransferTransactionRequest(_message.Message):
+    __slots__ = ()
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    sender: str
+    receiver: str
+    amount: int
+    fee: int
+    memo: str
+    def __init__(self, lock_time: _Optional[int] = ..., sender: _Optional[str] = ..., receiver: _Optional[str] = ..., amount: _Optional[int] = ..., fee: _Optional[int] = ..., memo: _Optional[str] = ...) -> None: ...
+
+class GetRawBondTransactionRequest(_message.Message):
+    __slots__ = ()
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    STAKE_FIELD_NUMBER: _ClassVar[int]
+    PUBLIC_KEY_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    sender: str
+    receiver: str
+    stake: int
+    public_key: str
+    fee: int
+    memo: str
+    def __init__(self, lock_time: _Optional[int] = ..., sender: _Optional[str] = ..., receiver: _Optional[str] = ..., stake: _Optional[int] = ..., public_key: _Optional[str] = ..., fee: _Optional[int] = ..., memo: _Optional[str] = ...) -> None: ...
+
+class GetRawUnbondTransactionRequest(_message.Message):
+    __slots__ = ()
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    VALIDATOR_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    validator_address: str
+    memo: str
+    def __init__(self, lock_time: _Optional[int] = ..., validator_address: _Optional[str] = ..., memo: _Optional[str] = ...) -> None: ...
+
+class GetRawWithdrawTransactionRequest(_message.Message):
+    __slots__ = ()
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    VALIDATOR_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    ACCOUNT_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    validator_address: str
+    account_address: str
+    amount: int
+    fee: int
+    memo: str
+    def __init__(self, lock_time: _Optional[int] = ..., validator_address: _Optional[str] = ..., account_address: _Optional[str] = ..., amount: _Optional[int] = ..., fee: _Optional[int] = ..., memo: _Optional[str] = ...) -> None: ...
+
+class GetRawBatchTransferTransactionRequest(_message.Message):
+    __slots__ = ()
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECIPIENTS_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    lock_time: int
+    sender: str
+    recipients: _containers.RepeatedCompositeFieldContainer[Recipient]
+    fee: int
+    memo: str
+    def __init__(self, lock_time: _Optional[int] = ..., sender: _Optional[str] = ..., recipients: _Optional[_Iterable[_Union[Recipient, _Mapping]]] = ..., fee: _Optional[int] = ..., memo: _Optional[str] = ...) -> None: ...
+
+class GetRawTransactionResponse(_message.Message):
+    __slots__ = ()
+    RAW_TRANSACTION_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    raw_transaction: str
+    id: str
+    def __init__(self, raw_transaction: _Optional[str] = ..., id: _Optional[str] = ...) -> None: ...
+
+class PayloadTransfer(_message.Message):
+    __slots__ = ()
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    sender: str
+    receiver: str
+    amount: int
+    def __init__(self, sender: _Optional[str] = ..., receiver: _Optional[str] = ..., amount: _Optional[int] = ...) -> None: ...
+
+class PayloadBond(_message.Message):
+    __slots__ = ()
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    STAKE_FIELD_NUMBER: _ClassVar[int]
+    PUBLIC_KEY_FIELD_NUMBER: _ClassVar[int]
+    sender: str
+    receiver: str
+    stake: int
+    public_key: str
+    def __init__(self, sender: _Optional[str] = ..., receiver: _Optional[str] = ..., stake: _Optional[int] = ..., public_key: _Optional[str] = ...) -> None: ...
+
+class PayloadSortition(_message.Message):
+    __slots__ = ()
+    ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    PROOF_FIELD_NUMBER: _ClassVar[int]
+    address: str
+    proof: str
+    def __init__(self, address: _Optional[str] = ..., proof: _Optional[str] = ...) -> None: ...
+
+class PayloadUnbond(_message.Message):
+    __slots__ = ()
+    VALIDATOR_FIELD_NUMBER: _ClassVar[int]
+    validator: str
+    def __init__(self, validator: _Optional[str] = ...) -> None: ...
+
+class PayloadWithdraw(_message.Message):
+    __slots__ = ()
+    VALIDATOR_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    ACCOUNT_ADDRESS_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    validator_address: str
+    account_address: str
+    amount: int
+    def __init__(self, validator_address: _Optional[str] = ..., account_address: _Optional[str] = ..., amount: _Optional[int] = ...) -> None: ...
+
+class PayloadBatchTransfer(_message.Message):
+    __slots__ = ()
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    RECIPIENTS_FIELD_NUMBER: _ClassVar[int]
+    sender: str
+    recipients: _containers.RepeatedCompositeFieldContainer[Recipient]
+    def __init__(self, sender: _Optional[str] = ..., recipients: _Optional[_Iterable[_Union[Recipient, _Mapping]]] = ...) -> None: ...
+
+class Recipient(_message.Message):
+    __slots__ = ()
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    receiver: str
+    amount: int
+    def __init__(self, receiver: _Optional[str] = ..., amount: _Optional[int] = ...) -> None: ...
+
+class TransactionInfo(_message.Message):
+    __slots__ = ()
+    ID_FIELD_NUMBER: _ClassVar[int]
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    VERSION_FIELD_NUMBER: _ClassVar[int]
+    LOCK_TIME_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    FEE_FIELD_NUMBER: _ClassVar[int]
+    PAYLOAD_TYPE_FIELD_NUMBER: _ClassVar[int]
+    TRANSFER_FIELD_NUMBER: _ClassVar[int]
+    BOND_FIELD_NUMBER: _ClassVar[int]
+    SORTITION_FIELD_NUMBER: _ClassVar[int]
+    UNBOND_FIELD_NUMBER: _ClassVar[int]
+    WITHDRAW_FIELD_NUMBER: _ClassVar[int]
+    BATCH_TRANSFER_FIELD_NUMBER: _ClassVar[int]
+    MEMO_FIELD_NUMBER: _ClassVar[int]
+    PUBLIC_KEY_FIELD_NUMBER: _ClassVar[int]
+    SIGNATURE_FIELD_NUMBER: _ClassVar[int]
+    BLOCK_HEIGHT_FIELD_NUMBER: _ClassVar[int]
+    CONFIRMED_FIELD_NUMBER: _ClassVar[int]
+    CONFIRMATIONS_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    data: str
+    version: int
+    lock_time: int
+    value: int
+    fee: int
+    payload_type: PayloadType
+    transfer: PayloadTransfer
+    bond: PayloadBond
+    sortition: PayloadSortition
+    unbond: PayloadUnbond
+    withdraw: PayloadWithdraw
+    batch_transfer: PayloadBatchTransfer
+    memo: str
+    public_key: str
+    signature: str
+    block_height: int
+    confirmed: bool
+    confirmations: int
+    def __init__(self, id: _Optional[str] = ..., data: _Optional[str] = ..., version: _Optional[int] = ..., lock_time: _Optional[int] = ..., value: _Optional[int] = ..., fee: _Optional[int] = ..., payload_type: _Optional[_Union[PayloadType, str]] = ..., transfer: _Optional[_Union[PayloadTransfer, _Mapping]] = ..., bond: _Optional[_Union[PayloadBond, _Mapping]] = ..., sortition: _Optional[_Union[PayloadSortition, _Mapping]] = ..., unbond: _Optional[_Union[PayloadUnbond, _Mapping]] = ..., withdraw: _Optional[_Union[PayloadWithdraw, _Mapping]] = ..., batch_transfer: _Optional[_Union[PayloadBatchTransfer, _Mapping]] = ..., memo: _Optional[str] = ..., public_key: _Optional[str] = ..., signature: _Optional[str] = ..., block_height: _Optional[int] = ..., confirmed: _Optional[bool] = ..., confirmations: _Optional[int] = ...) -> None: ...
+
+class DecodeRawTransactionRequest(_message.Message):
+    __slots__ = ()
+    RAW_TRANSACTION_FIELD_NUMBER: _ClassVar[int]
+    raw_transaction: str
+    def __init__(self, raw_transaction: _Optional[str] = ...) -> None: ...
+
+class DecodeRawTransactionResponse(_message.Message):
+    __slots__ = ()
+    TRANSACTION_FIELD_NUMBER: _ClassVar[int]
+    transaction: TransactionInfo
+    def __init__(self, transaction: _Optional[_Union[TransactionInfo, _Mapping]] = ...) -> None: ...
