e43f0044d856721da0ed943e56fbbd70e4e4a445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:59 +0000

    Add package passagemath-database-stein-watkins-mini/10.8.1a4/passagemath_database_stein_watkins_mini-10.8.1a4.tar.gz

---
+++ packages/passagemath-database-stein-watkins-mini/passagemath_database_stein_watkins_mini-10.8.1a4.tar.gz/passagemath_database_stein_watkins_mini-10.8.1a4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-database-stein-watkins-mini'],
+           recurse_packages=('sage', 'passagemath_database_stein_watkins_mini'),
+           spkgs=['database_stein_watkins_mini'])
