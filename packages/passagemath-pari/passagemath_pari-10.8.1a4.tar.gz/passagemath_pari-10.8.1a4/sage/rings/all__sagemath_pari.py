95c93a9faffda77c0d8961f7d8fd28c4617c7328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:27 +0000

    Add package passagemath-pari/10.8.1a4/passagemath_pari-10.8.1a4.tar.gz

---
+++ packages/passagemath-pari/passagemath_pari-10.8.1a4.tar.gz/passagemath_pari-10.8.1a4/sage/rings/all__sagemath_pari.py
@@ -0,0 +1,15 @@
+# sage_setup: distribution = sagemath-pari
+
+from sage.rings.number_field.all__sagemath_pari import *
+
+# Pseudo-ring of PARI objects.
+from sage.rings.pari_ring import PariRing, Pari
+
+# p-adic field
+from sage.rings.padics.all__sagemath_pari import *
+
+# valuations
+from sage.rings.valuation.all import *
+
+# Tate algebras
+from sage.rings.tate_algebra import TateAlgebra
