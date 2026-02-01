fd90da518e285207c2380390c1805d31eede5796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:59 +0000

    Add package rust_civit_comfy_bindings/0.1.0/rust_civit_comfy_bindings-0.1.0-cp312-cp312-win_amd64.whl

---
+++ packages/rust_civit_comfy_bindings/rust_civit_comfy_bindings-0.1.0-cp312-cp312-win_amd64.whl/rust_civit_comfy_bindings/__init__.py
@@ -0,0 +1,5 @@
+from .rust_civit_comfy_bindings import *
+
+__doc__ = rust_civit_comfy_bindings.__doc__
+if hasattr(rust_civit_comfy_bindings, "__all__"):
+    __all__ = rust_civit_comfy_bindings.__all__
