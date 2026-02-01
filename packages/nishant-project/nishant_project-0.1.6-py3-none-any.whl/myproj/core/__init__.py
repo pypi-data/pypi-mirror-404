d981711f010ff297a2e434ef248f5679ad9e2b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:07 +0000

    Add package nishant-project/0.1.6/nishant_project-0.1.6-py3-none-any.whl

---
+++ packages/nishant-project/nishant_project-0.1.6-py3-none-any.whl/myproj/core/__init__.py
@@ -0,0 +1,4 @@
+from .math_ops import add, multiply, subtract, division
+from .string_ops import upper
+
+__all__ = ["add", "multiply", "upper", "subtract", "division"]
---
+++ packages/nishant-project/nishant_project-0.1.6-py3-none-any.whl/myproj/core/math_ops.py
@@ -0,0 +1,11 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+def division(a: int, b: int) -> float:
+    return a / b
