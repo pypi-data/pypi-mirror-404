68a2547ce817fe4ef006760b75d277d8ed6e2acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:12 +0000

    Add package passagemath-categories/10.8.1a4/passagemath_categories-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-categories/passagemath_categories-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl/sage/categories/examples/coxeter_groups.py
@@ -0,0 +1,8 @@
+# sage_setup: distribution = sagemath-categories
+"""
+Examples of Coxeter groups
+"""
+# temporary until someone implements an appropriate example
+
+from . import finite_weyl_groups
+Example = finite_weyl_groups.Example
