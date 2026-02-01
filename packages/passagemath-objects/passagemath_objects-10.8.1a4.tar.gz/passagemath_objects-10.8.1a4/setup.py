2b94e64300951362d3b25750791ca7e7461e891a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:26 +0000

    Add package passagemath-objects/10.8.1a4/passagemath_objects-10.8.1a4.tar.gz

---
+++ packages/passagemath-objects/passagemath_objects-10.8.1a4.tar.gz/passagemath_objects-10.8.1a4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(
+    ['sagemath-objects'],
+    recurse_packages=('sage', 'passagemath_objects'),
+    spkgs=['gmp', 'mpc', 'mpfr'],
+    package_data={
+        "sage.cpython": [
+            "pycore_long.h",
+            "pyx_visit.h",
+            "string_impl.h",
+            "cython_metaclass.h",
+            "python_debug.h",
+        ],
+        "sage.ext": [
+            "ccobject.h",
+            "mod_int.h",
+        ],
+        "sage.rings": [
+            "integer_fake.h",
+        ]
+    },
+    py_limited_api=False,
+)
