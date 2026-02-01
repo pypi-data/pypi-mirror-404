c685f1867e83b8c864e1726dc91f2ac32f904446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:02:55 +0000

    Add package isagellm-comm/0.4.0.6/isagellm_comm-0.4.0.6-py2.py3-none-any.whl

---
+++ packages/isagellm-comm/isagellm_comm-0.4.0.6-py2.py3-none-any.whl/sagellm_comm/topology/__init__.py
@@ -0,0 +1,12 @@
+"""Topology utilities for sagellm-comm."""
+
+from __future__ import annotations
+
+from sagellm_comm.topology.device import DeviceId
+from sagellm_comm.topology.link import Link, LinkKind
+
+__all__ = [
+    "DeviceId",
+    "Link",
+    "LinkKind",
+]
