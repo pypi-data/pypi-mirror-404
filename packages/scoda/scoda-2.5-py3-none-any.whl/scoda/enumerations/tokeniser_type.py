ec1f3cf7123b907b482e4b14169e42d00e599ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:53 +0000

    Add package scoda/2.5/scoda-2.5-py3-none-any.whl

---
+++ packages/scoda/scoda-2.5-py3-none-any.whl/scoda/enumerations/tokeniser_type.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+import enum
+
+
+class TokeniserType(enum.Enum):
+    MULTI_TRACK_LARGE_VOCABULARY_NOTELIKE_TOKENISER = "multi_track_large_vocabulary_notelike_tokeniser"
