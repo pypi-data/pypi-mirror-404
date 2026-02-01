fd08217c7cda44198846da23896ab60fabcd222e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:28 +0000

    Add package Geode-Simplex/10.0.4/geode_simplex-10.0.4-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Simplex/geode_simplex-10.0.4-cp310-cp310-win_amd64.whl/geode_simplex/brep_simplex.pyi
@@ -0,0 +1,9 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_numerics as geode_numerics
+from geode_simplex.bin.geode_simplex_py_brep import SimplexBRepLibrary
+from geode_simplex.bin.geode_simplex_py_brep import brep_simplex_remesh
+import opengeode as opengeode
+import opengeode_inspector as opengeode_inspector
+__all__: list[str] = ['SimplexBRepLibrary', 'brep_simplex_remesh', 'geode_background', 'geode_common', 'geode_numerics', 'opengeode', 'opengeode_inspector']
