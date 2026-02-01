c19a5db89c2f06f2504ffa7f4d58f58f45ccccae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:07 +0000

    Add package nishant-project/0.1.6/nishant_project-0.1.6.tar.gz

---
+++ packages/nishant-project/nishant_project-0.1.6.tar.gz/nishant_project-0.1.6/src/myproj/__init__.py
@@ -0,0 +1,5 @@
+from .core import add, multiply, subtract, division, upper
+from .utils import greet
+from .version import __version__
+
+__all__ = ["add", "multiply", "subtract", "division", "upper", "greet", "__version__"]
