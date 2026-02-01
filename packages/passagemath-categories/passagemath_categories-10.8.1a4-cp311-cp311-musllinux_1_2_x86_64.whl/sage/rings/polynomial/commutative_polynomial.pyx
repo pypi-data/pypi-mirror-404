c7326156680dd394acdf4af7c8fc68d6734302dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:54 +0000

    Add package passagemath-categories/10.8.1a4/passagemath_categories-10.8.1a4-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/passagemath-categories/passagemath_categories-10.8.1a4-cp311-cp311-musllinux_1_2_x86_64.whl/sage/rings/polynomial/commutative_polynomial.pyx
@@ -0,0 +1,24 @@
+# sage_setup: distribution = sagemath-categories
+cdef class CommutativePolynomial(CommutativeAlgebraElement):
+    r"""
+    Abstract base class for commutative polynomials in any number of variables.
+
+    It is a common base for :class:`~sage.rings.polynomial.polynomial_element.Polynomial`,
+    :class:`~sage.rings.polynomial.multi_polynomial.MPolynomial`, and
+    :class:`~sage.rings.polynomial.infinite_polynomial_element.InfinitePolynomial`.
+
+    EXAMPLES::
+
+        sage: from sage.rings.polynomial.commutative_polynomial import CommutativePolynomial
+        sage: K.<x> = PolynomialRing(QQ)
+        sage: isinstance(x, CommutativePolynomial)
+        True
+        sage: K.<x,y> = PolynomialRing(QQ)
+        sage: isinstance(x, CommutativePolynomial)
+        True
+        sage: X.<x,y> = InfinitePolynomialRing(ZZ, implementation='sparse')             # needs sage.modules
+        sage: isinstance(x[2], CommutativePolynomial)                                   # needs sage.modules
+        True
+    """
+
+    pass
