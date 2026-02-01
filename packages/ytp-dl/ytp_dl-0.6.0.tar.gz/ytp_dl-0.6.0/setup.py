2295e17eeb06e6f5e7fb5c2735b5de7eb8a72b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:32:02 +0000

    Add package ytp-dl/0.6.0/ytp_dl-0.6.0.tar.gz

---
+++ packages/ytp-dl/ytp_dl-0.6.0.tar.gz/ytp_dl-0.6.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="ytp-dl",
+    version="0.6.0", 
+    author="dumgum82",
+    author_email="dumgum42@gmail.com",
+    description="YouTube video downloader with Mullvad VPN integration and Flask API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/ytp-dl",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "ytp-dl-api=scripts.api:main",
+        ],
+    },
+    include_package_data=True,
+)
