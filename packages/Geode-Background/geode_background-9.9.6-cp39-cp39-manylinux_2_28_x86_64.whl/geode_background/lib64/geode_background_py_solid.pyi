e1fa4f190cfdd641f1d51d7cc327fc520ae0d076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:04:00 +0000

    Add package Geode-Background/9.9.6/geode_background-9.9.6-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Background/geode_background-9.9.6-cp39-cp39-manylinux_2_28_x86_64.whl/geode_background/lib64/geode_background_py_solid.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Background Python binding for solid
+"""
+from __future__ import annotations
+__all__: list[str] = ['BackgroundSolidLibrary']
+class BackgroundSolidLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
