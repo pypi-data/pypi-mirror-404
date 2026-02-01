e74db5135742293307e1e36db96ec9bea94d5149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:23 +0000

    Add package openstream/0.1.0/openstream-0.1.0-py3-none-any.whl

---
+++ packages/openstream/openstream-0.1.0-py3-none-any.whl/openstream/__init__.py
@@ -0,0 +1,15 @@
+"""
+openstream - A Python package for streaming data processing.
+"""
+
+__version__ = "0.1.0"
+__author__ = "DONGHYEONG LEE"
+__email__ = "donghyeong.lee@hd.com"
+
+from .core import *
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__email__",
+]
