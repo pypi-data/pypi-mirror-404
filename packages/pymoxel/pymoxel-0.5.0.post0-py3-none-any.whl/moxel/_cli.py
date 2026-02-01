777dfda879f2efdd9229572051074f6566a953ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:30:40 +0000

    Add package pymoxel/0.5.0.post0/pymoxel-0.5.0.post0-py3-none-any.whl

---
+++ packages/pymoxel/pymoxel-0.5.0.post0-py3-none-any.whl/moxel/_cli.py
@@ -0,0 +1,31 @@
+# This file is part of MOXελ.
+# Copyright (C) 2023-2024 Antonios P. Sarikas
+
+# MOXελ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+r"""
+This module provides helper functions for the CLI.
+"""
+
+from jsonargparse import auto_cli
+
+from .utils import voxels_from_dir
+
+
+def moxel_cli():
+    r"""CLI for the moxel package."""
+    auto_cli(
+            voxels_from_dir,
+            epilog='For more information 👉 https://github.com/adosar/moxel',
+            )
