8aceb3f9f1f5faf2b1825342d1a56ed6542ffb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:53:06 +0000

    Add package spforge/0.8.27/spforge-0.8.27-py3-none-any.whl

---
+++ packages/spforge/spforge-0.8.27-py3-none-any.whl/spforge/performance_transformers/__init__.py
@@ -0,0 +1,10 @@
+from ._performance_manager import (
+    PerformanceManager as PerformanceManager,
+    PerformanceWeightsManager as PerformanceWeightsManager,
+)
+from ._performances_transformers import (
+    DiminishingValueTransformer as DiminishingValueTransformer,
+    MinMaxTransformer as MinMaxTransformer,
+    PartialStandardScaler as PartialStandardScaler,
+    SymmetricDistributionTransformer as SymmetricDistributionTransformer,
+)
