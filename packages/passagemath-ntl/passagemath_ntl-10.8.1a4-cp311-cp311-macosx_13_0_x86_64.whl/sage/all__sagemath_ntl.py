5cb3bc8af13fb2f7f9c6999aa3d106fcd3090c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:34 +0000

    Add package passagemath-ntl/10.8.1a4/passagemath_ntl-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-ntl/passagemath_ntl-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl/sage/all__sagemath_ntl.py
@@ -0,0 +1,7 @@
+# sage_setup: distribution = sagemath-ntl
+# delvewheel: patch
+
+from .all__sagemath_categories import *
+
+from sage.libs.all__sagemath_ntl import *
+from sage.rings.all__sagemath_ntl import *
