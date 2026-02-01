b33300876a4628f37c9e1fb4b7dc8691aa3b1531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:24 +0000

    Add package Geode-Hybrid-Geosciences/1.11.4/geode_hybrid_geosciences-1.11.4-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Hybrid-Geosciences/geode_hybrid_geosciences-1.11.4-cp310-cp310-manylinux_2_28_x86_64.whl/geode_hybrid_geosciences/lib64/geode_hybrid_geosciences_py_pillar.pyi
@@ -0,0 +1,22 @@
+"""
+Geode-Hybrid_Geosciences Python binding for Pillar
+"""
+from __future__ import annotations
+import opengeode.lib64.opengeode_py_basic
+__all__: list[str] = ['HybridGeosciencesPillarLibrary', 'PillarStructuralModelBuilder', 'PillarStructuralModelBuilderResult', 'PillarStructuralModelInspectionResult', 'PillarStructuralModelOptions']
+class HybridGeosciencesPillarLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
+class PillarStructuralModelBuilder:
+    def build(self, arg0: PillarStructuralModelOptions) -> PillarStructuralModelBuilderResult:
+        ...
+    def is_model_construction_possible(self) -> PillarStructuralModelInspectionResult:
+        ...
+class PillarStructuralModelBuilderResult:
+    slice_collection_order: list[opengeode.lib64.opengeode_py_basic.uuid]
+class PillarStructuralModelInspectionResult:
+    def string(self) -> str:
+        ...
+class PillarStructuralModelOptions:
+    add_block_mesh: bool
