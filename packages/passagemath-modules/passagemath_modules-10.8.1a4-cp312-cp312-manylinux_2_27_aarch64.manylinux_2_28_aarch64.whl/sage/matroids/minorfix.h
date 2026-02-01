abfe76792d8f952ef9515f9b9f694e8c336ea612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:43 +0000

    Add package passagemath-modules/10.8.1a4/passagemath_modules-10.8.1a4-cp312-cp312-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/passagemath-modules/passagemath_modules-10.8.1a4-cp312-cp312-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/sage/matroids/minorfix.h
@@ -0,0 +1,9 @@
+/* sage_setup: distribution = sagemath-modules
+ */
+// On some systems, macros "minor()" and "major()" are defined in system header files. This will undefine those:
+#ifdef major
+    #undef major
+#endif
+#ifdef minor
+    #undef minor
+#endif
