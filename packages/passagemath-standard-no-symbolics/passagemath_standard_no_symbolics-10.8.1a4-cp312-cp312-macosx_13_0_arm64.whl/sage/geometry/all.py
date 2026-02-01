29fe2b782299ce7eb53abaa58e61397389139b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:42 +0000

    Add package passagemath-standard-no-symbolics/10.8.1a4/passagemath_standard_no_symbolics-10.8.1a4-cp312-cp312-macosx_13_0_arm64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.8.1a4-cp312-cp312-macosx_13_0_arm64.whl/sage/geometry/all.py
@@ -0,0 +1,12 @@
+from sage.geometry.all__sagemath_polyhedra import *
+
+try:
+    from sage.geometry.all__sagemath_symbolics import *
+except ImportError:
+    pass
+
+
+try:
+    from sage.geometry.all__sagemath_gap import *
+except ImportError:
+    pass
