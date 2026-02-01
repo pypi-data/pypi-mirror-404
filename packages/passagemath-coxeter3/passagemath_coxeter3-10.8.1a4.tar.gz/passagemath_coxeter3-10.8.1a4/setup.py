542fcaefe63622f1b652b9208d4c8a9f71267171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:59 +0000

    Add package passagemath-coxeter3/10.8.1a4/passagemath_coxeter3-10.8.1a4.tar.gz

---
+++ packages/passagemath-coxeter3/passagemath_coxeter3-10.8.1a4.tar.gz/passagemath_coxeter3-10.8.1a4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-coxeter3'],
+           recurse_packages=('sage', 'passagemath_coxeter3'),
+           spkgs=['coxeter3'],
+           package_data={
+               "sage.libs.coxeter3": [
+                   "coxeter.pxd",
+                   "decl.pxd",
+               ],
+           },
+           py_limited_api=False)
