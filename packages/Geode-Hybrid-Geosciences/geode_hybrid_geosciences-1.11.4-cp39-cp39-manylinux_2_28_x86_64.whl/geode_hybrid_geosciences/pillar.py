2b4c447caf8f855b890554c4d95125c59c476227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:24 +0000

    Add package Geode-Hybrid-Geosciences/1.11.4/geode_hybrid_geosciences-1.11.4-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Hybrid-Geosciences/geode_hybrid_geosciences-1.11.4-cp39-cp39-manylinux_2_28_x86_64.whl/geode_hybrid_geosciences/pillar.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import opengeode_geosciences
+import opengeode_inspector
+import geode_common
+import geode_conversion
+import geode_background
+
+from .lib64.geode_hybrid_geosciences_py_pillar import *
+
+HybridGeosciencesPillarLibrary.initialize()
