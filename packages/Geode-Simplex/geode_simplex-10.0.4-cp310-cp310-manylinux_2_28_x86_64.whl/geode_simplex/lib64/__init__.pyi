cc4aea98c87f6cd375b5445e37cf808a497dce82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:26 +0000

    Add package Geode-Simplex/10.0.4/geode_simplex-10.0.4-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Simplex/geode_simplex-10.0.4-cp310-cp310-manylinux_2_28_x86_64.whl/geode_simplex/lib64/__init__.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+from . import geode_simplex_py_brep
+from . import geode_simplex_py_metric
+from . import geode_simplex_py_section
+__all__: list[str] = ['geode_simplex_py_brep', 'geode_simplex_py_metric', 'geode_simplex_py_section']
---
+++ packages/Geode-Simplex/geode_simplex-10.0.4-cp310-cp310-manylinux_2_28_x86_64.whl/geode_simplex/metric_simplex.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .lib64.geode_simplex_py_metric import *
+SimplexMetricLibrary.initialize()
