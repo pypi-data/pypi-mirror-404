e995200de4bdc8f93b7265f1e1915a4a4bb9c3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:11 +0000

    Add package passagemath-tachyon/10.8.1a4/passagemath_tachyon-10.8.1a4-cp312-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/passagemath-tachyon/passagemath_tachyon-10.8.1a4-cp312-abi3-musllinux_1_2_x86_64.whl/sage/all__sagemath_tachyon.py
@@ -0,0 +1,7 @@
+# sage_setup: distribution = sagemath-tachyon
+# delvewheel: patch
+
+from sage.all__sagemath_objects import *
+
+from sage.interfaces.all__sagemath_tachyon import *
+from sage.libs.all__sagemath_tachyon import *
