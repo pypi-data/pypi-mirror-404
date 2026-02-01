53f78a4a44a7da66674d00e082fb9d6b6fb9f578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:06:33 +0000

    Add package rotavision-gati/0.0.1/rotavision_gati-0.0.1.tar.gz

---
+++ packages/rotavision-gati/rotavision_gati-0.0.1.tar.gz/rotavision_gati-0.0.1/src/rotavision_gati/__init__.py
@@ -0,0 +1,16 @@
+"""
+Rotavision Gati - Fleet & Mobility Intelligence
+
+This is a namespace placeholder. Please install the main `rotavision` package.
+
+    pip install rotavision
+
+Documentation: https://rotavision.com/docs
+"""
+
+__version__ = "0.0.1"
+
+raise ImportError(
+    "This is a namespace placeholder package. "
+    "Please install the main package: pip install rotavision"
+)
