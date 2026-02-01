8cd1d6d849d81dd737a063caf4cd2bf09f584c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 08:05:55 +0000

    Add package aiinbx/0.358.0/aiinbx-0.358.0-py3-none-any.whl

---
+++ packages/aiinbx/aiinbx-0.358.0-py3-none-any.whl/aiinbx/types/email_retrieve_response.py
@@ -0,0 +1,90 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from typing_extensions import Literal
+
+from pydantic import Field as FieldInfo
+
+from .._models import BaseModel
+
+__all__ = ["EmailRetrieveResponse", "Attachment"]
+
+
+class Attachment(BaseModel):
+    id: str
+
+    cid: Optional[str] = None
+
+    content_type: str = FieldInfo(alias="contentType")
+
+    created_at: str = FieldInfo(alias="createdAt")
+
+    disposition: Optional[str] = None
+
+    expires_at: str = FieldInfo(alias="expiresAt")
+
+    file_name: str = FieldInfo(alias="fileName")
+
+    signed_url: str = FieldInfo(alias="signedUrl")
+
+    size_in_bytes: float = FieldInfo(alias="sizeInBytes")
+
+
+class EmailRetrieveResponse(BaseModel):
+    id: str
+
+    attachments: List[Attachment]
+
+    bcc_addresses: List[str] = FieldInfo(alias="bccAddresses")
+
+    cc_addresses: List[str] = FieldInfo(alias="ccAddresses")
+
+    created_at: str = FieldInfo(alias="createdAt")
+
+    direction: Literal["INBOUND", "OUTBOUND"]
+
+    from_address: str = FieldInfo(alias="fromAddress")
+
+    from_name: Optional[str] = FieldInfo(alias="fromName", default=None)
+
+    html: Optional[str] = None
+
+    in_reply_to_id: Optional[str] = FieldInfo(alias="inReplyToId", default=None)
+
+    message_id: str = FieldInfo(alias="messageId")
+
+    received_at: Optional[str] = FieldInfo(alias="receivedAt", default=None)
+
+    references: List[str]
+
+    reply_to_addresses: List[str] = FieldInfo(alias="replyToAddresses")
+
+    sent_at: Optional[str] = FieldInfo(alias="sentAt", default=None)
+
+    snippet: Optional[str] = None
+
+    status: Literal[
+        "DRAFT",
+        "QUEUED",
+        "ACCEPTED",
+        "SENT",
+        "RECEIVED",
+        "FAILED",
+        "BOUNCED",
+        "COMPLAINED",
+        "REJECTED",
+        "READ",
+        "ARCHIVED",
+    ]
+
+    stripped_html: Optional[str] = FieldInfo(alias="strippedHtml", default=None)
+
+    stripped_text: Optional[str] = FieldInfo(alias="strippedText", default=None)
+
+    subject: Optional[str] = None
+
+    text: Optional[str] = None
+
+    thread_id: str = FieldInfo(alias="threadId")
+
+    to_addresses: List[str] = FieldInfo(alias="toAddresses")
