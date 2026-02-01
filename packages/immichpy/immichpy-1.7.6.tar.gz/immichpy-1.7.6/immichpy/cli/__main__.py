d46fb57d268aa3a1494b11d2c9b5b2e80d4b1dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:18 +0000

    Add package immichpy/1.7.6/immichpy-1.7.6.tar.gz

---
+++ packages/immichpy/immichpy-1.7.6.tar.gz/immichpy-1.7.6/immichpy/cli/__main__.py
@@ -0,0 +1,12 @@
+try:  # pragma: no cover
+    from .main import app
+except ImportError:  # pragma: no cover
+    import sys
+
+    message = 'To use the immichpy command, install the "cli" extra:\n\n\t- pip install immichpy[cli]\n\tOR\n\t- uv add immichpy --extra cli\n'
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def main() -> None:  # pragma: no cover
+    app()
