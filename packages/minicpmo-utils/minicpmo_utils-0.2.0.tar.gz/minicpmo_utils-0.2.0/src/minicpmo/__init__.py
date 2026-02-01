03949178c67ca4c5a64273cb5812047c7cecae00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:07:24 +0000

    Add package minicpmo-utils/0.2.0/minicpmo_utils-0.2.0.tar.gz

---
+++ packages/minicpmo-utils/minicpmo_utils-0.2.0.tar.gz/minicpmo_utils-0.2.0/src/minicpmo/__init__.py
@@ -0,0 +1,14 @@
+"""MiniCPM-o 顶层工具包 `minicpmo`。
+
+这个包聚合了语音 / 音频相关子包，并预留统一的 utils 入口：
+
+- 顶层子包：
+  - cosyvoice
+  - stepaudio2
+  - matcha
+  - s3tokenizer
+- 工具入口：
+  - from minicpmo.utils import ...
+"""
+
+from .version import __version__
