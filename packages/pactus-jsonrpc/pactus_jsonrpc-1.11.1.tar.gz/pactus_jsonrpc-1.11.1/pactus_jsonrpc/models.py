862851919e2593b2c8537caa54d646c48f5968f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:32 +0000

    Add package pactus-jsonrpc/1.11.1/pactus_jsonrpc-1.11.1.tar.gz

---
+++ packages/pactus-jsonrpc/pactus_jsonrpc-1.11.1.tar.gz/pactus_jsonrpc-1.11.1/pactus_jsonrpc/models.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from uuid import UUID
+import datetime
+from enum import Enum
+from typing import Any, Literal
+
+from pydantic import BaseModel, UUID1, UUID3, UUID4, UUID5
---
+++ packages/pactus-jsonrpc/pactus_jsonrpc-1.11.1.tar.gz/pactus_jsonrpc-1.11.1/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pactus-jsonrpc",
+    version="1.11.1",
+    author="Pactus Development Team",
+    author_email="info@pactus.org",
+    url="https://pactus.org",
+    description="Python client for interacting with the Pactus blockchain via JSON-RPC",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "jsonrpc2-pyclient>=5.2.0",
+        "py-undefined>=0.1.5",
+        "pydantic>=2.5.3"
+    ],
+    keywords=["pactus", "blockchain", "json-rpc"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
