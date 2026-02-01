a09b58533876e9f92f600706d0efad7336d1278f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:22 +0000

    Add package passagemath-libbraiding/10.8.1a4/passagemath_libbraiding-10.8.1a4.tar.gz

---
+++ packages/passagemath-libbraiding/passagemath_libbraiding-10.8.1a4.tar.gz/passagemath_libbraiding-10.8.1a4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-libbraiding'],
+           recurse_packages=('sage', 'passagemath_libbraiding'),
+           spkgs=['libbraiding'],
+           package_data={},
+           py_limited_api=True)
