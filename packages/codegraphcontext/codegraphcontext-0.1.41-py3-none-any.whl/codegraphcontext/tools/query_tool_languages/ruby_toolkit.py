ecb3bbe027199d58333e45befa0c919df21bf5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 16:05:45 +0000

    Add package codegraphcontext/0.1.41/codegraphcontext-0.1.41-py3-none-any.whl

---
+++ packages/codegraphcontext/codegraphcontext-0.1.41-py3-none-any.whl/codegraphcontext/tools/query_tool_languages/ruby_toolkit.py
@@ -0,0 +1,5 @@
+class RubyToolkit:
+    """Template placeholder for future implementation."""
+
+    def get_cypher_query(query: str) -> str:
+        raise NotImplementedError("AdvancedLanguageQuery is not implemented yet.")
