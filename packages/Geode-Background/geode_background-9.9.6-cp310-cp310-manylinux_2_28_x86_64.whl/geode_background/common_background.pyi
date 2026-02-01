576309ea9ba6e324e54171cf57d32f38d62c85cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:58 +0000

    Add package Geode-Background/9.9.6/geode_background-9.9.6-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Background/geode_background-9.9.6-cp310-cp310-manylinux_2_28_x86_64.whl/geode_background/common_background.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+from geode_background.lib64.geode_background_py_common import BackgroundCommonLibrary
+import geode_common as geode_common
+import opengeode as opengeode
+__all__: list[str] = ['BackgroundCommonLibrary', 'geode_common', 'opengeode']
---
+++ packages/Geode-Background/geode_background-9.9.6-cp310-cp310-manylinux_2_28_x86_64.whl/geode_background/lib64/geode_background_py_brep.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Background Python binding for brep
+"""
+from __future__ import annotations
+__all__: list[str] = ['BackgroundBRepLibrary']
+class BackgroundBRepLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
