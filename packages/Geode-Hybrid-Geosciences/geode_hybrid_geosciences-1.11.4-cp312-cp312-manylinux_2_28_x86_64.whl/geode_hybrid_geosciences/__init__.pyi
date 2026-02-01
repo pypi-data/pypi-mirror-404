b5f74e9d6e3967b3791d261b58fcf45c17c181f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:25 +0000

    Add package Geode-Hybrid-Geosciences/1.11.4/geode_hybrid_geosciences-1.11.4-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Hybrid-Geosciences/geode_hybrid_geosciences-1.11.4-cp312-cp312-manylinux_2_28_x86_64.whl/geode_hybrid_geosciences/__init__.pyi
@@ -0,0 +1,15 @@
+from __future__ import annotations
+import geode_background as geode_background
+import geode_common as geode_common
+import geode_conversion as geode_conversion
+from geode_hybrid_geosciences.lib64.geode_hybrid_geosciences_py_pillar import HybridGeosciencesPillarLibrary
+from geode_hybrid_geosciences.lib64.geode_hybrid_geosciences_py_pillar import PillarStructuralModelBuilder
+from geode_hybrid_geosciences.lib64.geode_hybrid_geosciences_py_pillar import PillarStructuralModelBuilderResult
+from geode_hybrid_geosciences.lib64.geode_hybrid_geosciences_py_pillar import PillarStructuralModelInspectionResult
+from geode_hybrid_geosciences.lib64.geode_hybrid_geosciences_py_pillar import PillarStructuralModelOptions
+import opengeode as opengeode
+import opengeode_geosciences as opengeode_geosciences
+import opengeode_inspector as opengeode_inspector
+from . import lib64
+from . import pillar
+__all__: list[str] = ['HybridGeosciencesPillarLibrary', 'PillarStructuralModelBuilder', 'PillarStructuralModelBuilderResult', 'PillarStructuralModelInspectionResult', 'PillarStructuralModelOptions', 'geode_background', 'geode_common', 'geode_conversion', 'lib64', 'opengeode', 'opengeode_geosciences', 'opengeode_inspector', 'pillar']
