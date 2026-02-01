bb606eafcc0095bf34cac94e2b67f19df09e209f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:06:41 +0000

    Add package Python-EasyGraph/1.6/python_easygraph-1.6-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/Python-EasyGraph/python_easygraph-1.6-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/easygraph/nn/tests/test_regularization.py
@@ -0,0 +1,16 @@
+import easygraph as eg
+import pytest
+import torch
+
+from easygraph.nn.regularization import EmbeddingRegularization
+
+
+def test_embedding_reg():
+    print("EmbeddingRegularization" in eg.__dir__())
+    emb_reg = EmbeddingRegularization(p=2, weight_decay=1e-4)
+    embs = [torch.randn(10, 3), torch.randn(10, 3)]
+    loss = emb_reg(*embs)
+    true_loss = 0
+    for emb in embs:
+        true_loss += 1 / 2 * emb.norm(2).pow(2) / 10
+    assert loss.item() == pytest.approx(1e-4 * true_loss.item())
