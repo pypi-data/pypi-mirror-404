d225f666c8194c96f28385e4e7bd425d13325947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:09 +0000

    Add package passagemath-combinat/10.8.1a4/passagemath_combinat-10.8.1a4-cp314-cp314-win_arm64.whl

---
+++ packages/passagemath-combinat/passagemath_combinat-10.8.1a4-cp314-cp314-win_arm64.whl/sage/combinat/counting.py
@@ -0,0 +1,12 @@
+# sage_setup: distribution = sagemath-combinat
+r"""
+Counting
+
+- :ref:`sage.combinat.combinat`
+- :ref:`sage.combinat.q_analogues`, :ref:`sage.combinat.q_bernoulli`
+- :ref:`sage.combinat.binary_recurrence_sequences`
+- :ref:`sage.combinat.expnums`
+- :ref:`sage.rings.cfinite_sequence`
+- :ref:`sage.databases.oeis`
+- :ref:`sage.combinat.sloane_functions`
+"""
