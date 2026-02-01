ab7adfced33adc32129cc980fa4353e57cd57c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:23 +0000

    Add package servify/0.0.16/servify-0.0.16-py3-none-any.whl

---
+++ packages/servify/servify-0.0.16-py3-none-any.whl/servify/__init__.py
@@ -0,0 +1,10 @@
+from app.commons import servify_read  # noqa: F401,F403
+
+_reader = servify_read()
+
+
+def read_data(path: str, formato: str, **kwargs):
+    return _reader.helper_reading_data.read_data(path, formato, **kwargs)
+
+
+__all__ = ["read_data", "servify_read"]
