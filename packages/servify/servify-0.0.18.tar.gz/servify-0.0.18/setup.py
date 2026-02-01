89a0aaf6ab861882e258c2a5a9b62335fca3f0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:16:13 +0000

    Add package servify/0.0.18/servify-0.0.18.tar.gz

---
+++ packages/servify/servify-0.0.18.tar.gz/servify-0.0.18/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as arq:
+    readme = arq.read()
+
+setup(
+    name="servify",
+    version="0.0.18",
+    license="MIT",
+    author="Felipe Pegoraro",
+    author_email="felipepegoraro93@gmail.com",
+    description="A Python library that simplifies data manipulation and workflow development with PySpark in Databricks environments.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords="databricks spark pyspark",
+    packages=find_packages(),
+    install_requires=[
+        "pyspark>=4.0.0",
+        "delta-spark>=4.0.1",
+        "loguru>=0.7.3",
+        "holidays>=0.88",
+        "pandas>=2.3.2",
+        "pyarrow>=21.0.0",
+        "tqdm>=4.67.1",
+        "openpyxl>=3.1.2",
+    ],
+)
