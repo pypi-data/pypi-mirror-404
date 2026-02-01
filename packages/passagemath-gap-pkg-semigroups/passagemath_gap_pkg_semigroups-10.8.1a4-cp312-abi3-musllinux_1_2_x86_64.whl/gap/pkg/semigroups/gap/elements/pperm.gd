73199ff5169486a84f7229a9db40d6ba44e0514a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:18:26 +0000

    Add package passagemath-gap-pkg-semigroups/10.8.1a4/passagemath_gap_pkg_semigroups-10.8.1a4-cp312-abi3-musllinux_1_2_x86_64.whl

---
+++ packages/passagemath-gap-pkg-semigroups/passagemath_gap_pkg_semigroups-10.8.1a4-cp312-abi3-musllinux_1_2_x86_64.whl/gap/pkg/semigroups/gap/elements/pperm.gd
@@ -0,0 +1,11 @@
+############################################################################
+##
+##  elements/pperm.gd
+##  Copyright (C) 2017-2022                                 Wilf A. Wilson
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+
+DeclareAttribute("CyclesOfPartialPerm", IsPartialPerm);
