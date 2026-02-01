897393fe5b5f104d3d0ed3d4d2abf8f4dab6314a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:18:30 +0000

    Add package passagemath-glpk/10.8.1a4/passagemath_glpk-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-glpk/passagemath_glpk-10.8.1a4-cp311-cp311-macosx_13_0_x86_64.whl/sage/numerical/backends/glpk_backend_test.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-glpk
+import pytest
+
+from sage.numerical.backends.generic_backend_test import GenericBackendTests
+from sage.numerical.backends.generic_backend import GenericBackend
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+
+class TestGLPKBackend(GenericBackendTests):
+
+    @pytest.fixture
+    def backend(self) -> GenericBackend:
+        return MixedIntegerLinearProgram(solver='GLPK').get_backend()
