8245f7001df5c22eaf32d8f656203b42c0b345cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 08:05:53 +0000

    Add package com-pac/0.0.3/com_pac-0.0.3-py3-none-any.whl

---
+++ packages/com-pac/com_pac-0.0.3-py3-none-any.whl/com_pac/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025-present andrew.will.owen
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.3"
---
+++ packages/com-pac/com_pac-0.0.3-py3-none-any.whl/com_pac/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present andrew.will.owen
+#
+# SPDX-License-Identifier: MIT
