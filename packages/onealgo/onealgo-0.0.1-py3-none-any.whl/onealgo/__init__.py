87b3930d7382cc8f98d10ac5b06301ad79147ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:02 +0000

    Add package onealgo/0.0.1/onealgo-0.0.1-py3-none-any.whl

---
+++ packages/onealgo/onealgo-0.0.1-py3-none-any.whl/onealgo/__init__.py
@@ -0,0 +1,8 @@
+"""
+OneAlgo - Full-stack algo trading framework.
+
+This is a placeholder package. The full version is available at:
+https://github.com/onealgo-team/onealgo
+"""
+
+__version__ = "0.0.1"
