dfc3266674ac02fe5e21abff05d43e1df294c253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:02:52 +0000

    Add package polars-uuid/0.5.2/polars_uuid-0.5.2-cp311-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/polars-uuid/polars_uuid-0.5.2-cp311-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/polars_uuid/__init__.py
@@ -0,0 +1,6 @@
+from polars_uuid._register_plugin import is_uuid as is_uuid
+from polars_uuid._register_plugin import u64_pair_to_uuid as u64_pair_to_uuid
+from polars_uuid._register_plugin import uuid_v4 as uuid_v4
+from polars_uuid._register_plugin import uuid_v7 as uuid_v7
+from polars_uuid._register_plugin import uuid_v7_extract_dt as uuid_v7_extract_dt
+from polars_uuid._register_plugin import uuid_v7_now as uuid_v7_now
