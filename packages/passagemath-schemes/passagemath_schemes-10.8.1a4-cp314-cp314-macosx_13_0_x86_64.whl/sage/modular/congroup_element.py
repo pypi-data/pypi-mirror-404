ac4fcee86803ab993175e42f026cb6e0862e80d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:01 +0000

    Add package passagemath-schemes/10.8.1a4/passagemath_schemes-10.8.1a4-cp314-cp314-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-schemes/passagemath_schemes-10.8.1a4-cp314-cp314-macosx_13_0_x86_64.whl/sage/modular/congroup_element.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-schemes
+###########################################################
+# Re-bindings for unpickling
+#
+# We want to ensure class
+# sage.modular.congroup_element.CongruenceSubgroupElement still exists, so we
+# can unpickle safely.
+#
+###########################################################
+
+from sage.modular.arithgroup.arithgroup_element import ArithmeticSubgroupElement
+
+CongruenceSubgroupElement = ArithmeticSubgroupElement
---
+++ packages/passagemath-schemes/passagemath_schemes-10.8.1a4-cp314-cp314-macosx_13_0_x86_64.whl/sage/modular/overconvergent/all.py
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-schemes
+from sage.modular.overconvergent.weightspace import WeightSpace_constructor as pAdicWeightSpace
+
+from sage.modular.overconvergent.genus0 import OverconvergentModularForms
+
+from sage.modular.overconvergent.hecke_series import hecke_series
