598147635f06e7d9ab8d5811785fcd0eb187c397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:24 +0000

    Add package passagemath-polyhedra/10.8.1a4/passagemath_polyhedra-10.8.1a4-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/sage/geometry/triangulation/functions.h
@@ -0,0 +1,10 @@
+/* sage_setup: distribution = sagemath-polyhedra
+ */
+#ifndef FUNCTIONS__H
+#define FUNCTIONS__H
+
+int factorial(int n);
+int binomial(int n, int D);
+
+
+#endif
---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp312-cp312-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/sage/numerical/backends/scip_backend_test.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-polyhedra
+import pytest
+from sage.numerical.backends.generic_backend_test import GenericBackendTests
+from sage.numerical.backends.generic_backend import GenericBackend
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+
+@pytest.importorskip("pyscipopt")
+class TestSCIPBackend(GenericBackendTests):
+
+    @pytest.fixture
+    def backend(self) -> GenericBackend:
+        return MixedIntegerLinearProgram(solver="SCIP").get_backend()
