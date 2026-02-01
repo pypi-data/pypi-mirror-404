2d7ed099fa7c1826d50296d83721071205abbc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:36 +0000

    Add package passagemath-polyhedra/10.8.1a4/passagemath_polyhedra-10.8.1a4-cp313-cp313-macosx_13_0_arm64.whl

---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp313-cp313-macosx_13_0_arm64.whl/sage/geometry/integral_points.py
@@ -0,0 +1,35 @@
+# sage_setup: distribution = sagemath-polyhedra
+r"""
+Cython helper methods to compute integral points in polyhedra
+"""
+
+try:
+    from .integral_points_integer_dense import (
+        parallelotope_points,
+        ray_matrix_normal_form,
+        loop_over_parallelotope_points,
+        simplex_points,
+        rectangular_box_points,
+        print_cache,
+        Inequality_generic,
+        Inequality_int,
+        InequalityCollection,
+    )
+except ImportError:
+    from .integral_points_generic_dense import (
+        parallelotope_points,
+        ray_matrix_normal_form,
+        loop_over_parallelotope_points,
+        simplex_points,
+        rectangular_box_points,
+        print_cache,
+        Inequality_generic,
+        Inequality_int,
+        InequalityCollection,
+    )
+
+
+# __all__ is needed to generate Sphinx documentation
+__all__ = ['InequalityCollection', 'Inequality_generic', 'Inequality_int',
+           'loop_over_parallelotope_points', 'parallelotope_points', 'print_cache',
+           'ray_matrix_normal_form', 'rectangular_box_points', 'simplex_points']
---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp313-cp313-macosx_13_0_arm64.whl/sage/numerical/linear_tensor_element.pxd
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-polyhedra
+from sage.structure.element cimport Element, ModuleElement
+
+cdef class LinearTensor(ModuleElement):
+    cdef dict _f
+    cpdef _add_(self, other)
