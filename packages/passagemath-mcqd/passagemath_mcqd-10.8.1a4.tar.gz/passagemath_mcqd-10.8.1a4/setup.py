a9d14ce668aaa80fe43a87e036e0a6cdcf268f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:21 +0000

    Add package passagemath-mcqd/10.8.1a4/passagemath_mcqd-10.8.1a4.tar.gz

---
+++ packages/passagemath-mcqd/passagemath_mcqd-10.8.1a4.tar.gz/passagemath_mcqd-10.8.1a4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-mcqd'],
+           recurse_packages=('sage', 'passagemath_mcqd'),
+           spkgs=['mcqd'],
+           package_data={"sage.graphs": ["mcqd.pxd"]},
+           py_limited_api=True)
