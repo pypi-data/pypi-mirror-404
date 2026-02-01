3b4e7568b541d55fbf1eb177f48b8606de573a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:34 +0000

    Add package passagemath-modules/10.8.1a4/passagemath_modules-10.8.1a4-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/passagemath-modules/passagemath_modules-10.8.1a4-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/sage/combinat/family.py
@@ -0,0 +1,11 @@
+# sage_setup: distribution = sagemath-modules
+"""
+Families
+
+This is a backward compatibility stub. Use :mod:`sage.sets.family` instead.
+"""
+
+# Backward compatibility pointer
+# Needed for unpickling.
+from sage.sets.family import (Family, FiniteFamily, LazyFamily,
+                              FiniteFamilyWithHiddenKeys)
---
+++ packages/passagemath-modules/passagemath_modules-10.8.1a4-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/sage/matrix/template.pxd
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-modules
+from sage.matrix.matrix_dense cimport Matrix_dense
+
+
+cdef class Matrix_generic_dense(Matrix_dense):
+    pass
