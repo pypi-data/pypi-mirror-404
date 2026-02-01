8047821518e1db8739deb86ff9176b364cee511e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 16:05:43 +0000

    Add package prefect/3.6.16.dev3/prefect-3.6.16.dev3-py3-none-any.whl

---
+++ packages/prefect/prefect-3.6.16.dev3-py3-none-any.whl/prefect/server/ui-v2/assets/use-is-overflowing-CpSV4NPP.js
@@ -0,0 +1,2 @@
+import{r}from"./vendor-tanstack-BcZfOOfy.js";const o=t=>{const[e,s]=r.useState(!1);return r.useEffect(()=>{t.current&&s(t.current.scrollWidth>t.current.clientWidth)},[t]),e};export{o as u};
+//# sourceMappingURL=use-is-overflowing-CpSV4NPP.js.map
---
+++ packages/prefect/prefect-3.6.16.dev3-py3-none-any.whl/prefect/server/ui-v2/assets/use-page-title-DEH55Ovu.js
@@ -0,0 +1,2 @@
+import{r as n}from"./vendor-tanstack-BcZfOOfy.js";const r="Prefect Server",i=" • ";function l(...o){const t=[...o.filter(e=>e!=null),r].join(i);n.useEffect(()=>(document.title=t,()=>{document.title=r}),[t])}export{l as u};
+//# sourceMappingURL=use-page-title-DEH55Ovu.js.map
