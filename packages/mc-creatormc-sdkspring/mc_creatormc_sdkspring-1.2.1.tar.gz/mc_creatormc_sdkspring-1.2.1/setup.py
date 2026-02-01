0da5342d93bc79d9a936981561bbca1eb8ea2aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:52:19 +0000

    Add package mc-creatormc-sdkspring/1.2.1/mc_creatormc_sdkspring-1.2.1.tar.gz

---
+++ packages/mc-creatormc-sdkspring/mc_creatormc_sdkspring-1.2.1.tar.gz/mc_creatormc_sdkspring-1.2.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='mc_creatormc_sdkspring',
+    version='1.2.1',
+    description = "一个基于网易我的世界 MODSDK 开发的框架，可以让开发者更方便的使用 MODSDK。",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='CreatorMC',
+    url='https://github.com/CreatorMC/MODSDKSping',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'modsdkspring=modsdkspring.main:main',
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License"
+    ],
+    platforms='any'
+)
