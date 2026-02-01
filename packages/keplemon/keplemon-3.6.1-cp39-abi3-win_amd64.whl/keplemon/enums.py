1dc9a172386ba9f11ce10c580ac6f19c3bc9b564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:19 +0000

    Add package keplemon/3.6.1/keplemon-3.6.1-cp39-abi3-win_amd64.whl

---
+++ packages/keplemon/keplemon-3.6.1-cp39-abi3-win_amd64.whl/keplemon/enums.py
@@ -0,0 +1,25 @@
+from keplemon._keplemon.enums import (  # type: ignore
+    TimeSystem,
+    Classification,
+    KeplerianType,
+    ReferenceFrame,
+    CovarianceType,
+    AssociationConfidence,
+    GeodeticModel,
+    MeanEquinox,
+    UCTObservability,
+    UCTValidity,
+)
+
+__all__ = [
+    "TimeSystem",
+    "Classification",
+    "KeplerianType",
+    "ReferenceFrame",
+    "CovarianceType",
+    "AssociationConfidence",
+    "GeodeticModel",
+    "MeanEquinox",
+    "UCTObservability",
+    "UCTValidity",
+]
---
+++ packages/keplemon/keplemon-3.6.1-cp39-abi3-win_amd64.whl/keplemon/exceptions.py
@@ -0,0 +1,5 @@
+from keplemon._keplemon.exceptions import (  # type: ignore
+    SAALError,
+)
+
+__all__ = ["SAALError"]
