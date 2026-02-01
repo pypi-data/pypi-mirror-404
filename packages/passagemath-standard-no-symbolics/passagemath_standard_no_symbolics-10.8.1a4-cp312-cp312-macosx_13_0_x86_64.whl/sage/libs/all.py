1636d3ec41f0079b689e76915db42123ada9a62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:16:11 +0000

    Add package passagemath-standard-no-symbolics/10.8.1a4/passagemath_standard_no_symbolics-10.8.1a4-cp312-cp312-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.8.1a4-cp312-cp312-macosx_13_0_x86_64.whl/sage/libs/all.py
@@ -0,0 +1,11 @@
+from sage.libs.all__sagemath_combinat import *
+from sage.libs.all__sagemath_gap import *
+from sage.libs.all__sagemath_flint import *
+from sage.libs.all__sagemath_ntl import *
+from sage.libs.all__sagemath_pari import *
+from sage.libs.all__sagemath_eclib import *
+
+try:
+    from sage.libs.all__sagemath_symbolics import *
+except ImportError:
+    pass
