d274a1de264ac2ab28aa4ca19891007b52b25f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 08:05:49 +0000

    Add package toml-rs/0.3.0/toml_rs-0.3.0-cp313-cp313-win32.whl

---
+++ packages/toml-rs/toml_rs-0.3.0-cp313-cp313-win32.whl/toml_rs/__init__.py
@@ -0,0 +1,19 @@
+__all__ = (
+    "TOMLDecodeError",
+    "TOMLEncodeError",
+    "__version__",
+    "dump",
+    "dumps",
+    "load",
+    "loads",
+)
+
+from ._lib import (
+    TOMLDecodeError,
+    TOMLEncodeError,
+    __version__,
+    dump,
+    dumps,
+    load,
+    loads,
+)
