9ddd755ed58afd1131a17d64841b062cb1cef6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 16:05:38 +0000

    Add package anomaly-agent/0.12.0/anomaly_agent-0.12.0.tar.gz

---
+++ packages/anomaly-agent/anomaly_agent-0.12.0.tar.gz/anomaly_agent-0.12.0/anomaly_agent/__init__.py
@@ -0,0 +1,18 @@
+"""Anomaly detection agent package for time series data analysis.
+
+This package provides tools for detecting anomalies in time series data using
+various statistical and machine learning methods.
+"""
+
+from .agent import Anomaly, AnomalyAgent, AnomalyList
+from .plot import plot_df
+from .utils import make_anomaly_config, make_df
+
+__all__ = [
+    "AnomalyAgent",
+    "Anomaly",
+    "AnomalyList",
+    "plot_df",
+    "make_df",
+    "make_anomaly_config",
+]
