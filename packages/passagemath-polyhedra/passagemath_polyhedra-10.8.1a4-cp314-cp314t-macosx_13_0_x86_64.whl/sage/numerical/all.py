b2161f4de56c965d87fead82470e87ce60cbc3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:15:42 +0000

    Add package passagemath-polyhedra/10.8.1a4/passagemath_polyhedra-10.8.1a4-cp314-cp314t-macosx_13_0_x86_64.whl

---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp314-cp314t-macosx_13_0_x86_64.whl/sage/numerical/all.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-polyhedra
+from sage.numerical.all__sagemath_modules import *
+
+from sage.misc.lazy_import import lazy_import
+
+lazy_import("sage.numerical.mip", ["MixedIntegerLinearProgram"])
+lazy_import("sage.numerical.sdp", ["SemidefiniteProgram"])
+lazy_import("sage.numerical.backends.generic_backend", ["default_mip_solver"])
+lazy_import("sage.numerical.backends.generic_sdp_backend", ["default_sdp_solver"])
+
+lazy_import("sage.numerical.interactive_simplex_method",
+            ["InteractiveLPProblem", "InteractiveLPProblemStandardForm"])
+del lazy_import
