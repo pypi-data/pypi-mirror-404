2b9911fc6119d637a5621d992117247a82de87b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:51:53 +0000

    Add package satellitic/0.1.11/satellitic-0.1.11.tar.gz

---
+++ packages/satellitic/satellitic-0.1.11.tar.gz/satellitic-0.1.11/doc/satellitic.nix
@@ -0,0 +1,135 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  python = pkgs.python313;
+
+  pyPkgs = python.pkgs.overrideScope (self: super: {
+
+    satellitic = super.buildPythonPackage rec {
+      pname = "satellitic";
+      version = "0.1.10";
+      format = "pyproject";
+
+      src = super.fetchPypi {
+        inherit pname version;
+        sha256 = "upa1Cc49sTZWRNXcP3G0n4vidWLL9ZjVqQ2vh2sSj+s=";
+      };
+
+      nativeBuildInputs = with super; [
+        hatchling
+        numpy
+      ];
+
+      propagatedBuildInputs = with super; [
+          numpy
+pandas
+jax
+sgp4
+astropy
+vispy
+matplotlib
+requests
+      ];
+
+      pythonImportsCheck = [ "satellitic" ];
+    };
+
+    impetuous = super.buildPythonPackage rec {
+      pname = "impetuous-gfa";
+      version = "0.101.15";
+      format = "pyproject";
+
+      src = super.fetchPypi {
+        inherit pname version;
+        sha256 = "tE1ktjJzAZXw0LfGMz5IR9nFrXyDnbMZh0emA0N7L74=";
+      };
+
+      #nativeBuildInputs = with super; [
+      #  hatchling
+      #  numpy
+      #];
+      nativeBuildInputs = with super; [
+        setuptools
+        wheel
+      ];
+
+      propagatedBuildInputs = with super; [
+        numpy
+        pandas
+        scipy
+        scikit-learn
+        statsmodels
+      ];
+    };
+
+    miepython = super.buildPythonPackage rec {
+      pname = "miepython";
+      version = "3.0.2";
+      format = "pyproject";
+
+      src = super.fetchPypi {
+        inherit pname version;
+        sha256 = "II7Ool1CJBZQ5pRxc4cWlfxn5EkyYFReUZnKhf/XvJo=";
+      };
+
+      #nativeBuildInputs = with super; [
+      #  hatchling
+      #  numpy
+      #];
+      nativeBuildInputs = with super; [
+        setuptools
+        wheel
+      ];
+
+      propagatedBuildInputs = with super; [
+        numpy
+        scipy
+        numba
+        matplotlib
+      ];
+    };
+  });
+
+  pythonEnv = python.withPackages (ps: with ps; [
+    pip
+    ipython
+
+    numpy
+    pandas
+    scipy
+    numba
+    scikit-learn
+    statsmodels
+
+    jax
+    sgp4
+    vispy
+    astropy
+
+    pyqt5
+
+    pyPkgs.satellitic
+    pyPkgs.impetuous
+    pyPkgs.miepython
+  ]);
+
+in pkgs.mkShell {
+  name = "satellitic-dev-shell";
+
+  buildInputs = [
+    pythonEnv
+  ];
+
+  shellHook = ''
+    export PYTHONNOUSERSITE=1
+    export SOURCE_DATE_EPOCH=$(date +%s)
+
+    echo "🚀 satellitic dev shell"
+    echo "Python: $(python --version)"
+    echo "satellitic import test:"
+    python - <<EOF
+import satellitic
+print("OK:", satellitic.__version__)
+EOF
+  '';
+}
