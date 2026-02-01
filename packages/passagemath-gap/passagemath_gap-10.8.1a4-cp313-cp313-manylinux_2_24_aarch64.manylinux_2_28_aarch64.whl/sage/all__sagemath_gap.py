b421e9220b4e1f757009df07fdb0e2b8f90f1bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 06:17:43 +0000

    Add package passagemath-gap/10.8.1a4/passagemath_gap-10.8.1a4-cp313-cp313-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/passagemath-gap/passagemath_gap-10.8.1a4-cp313-cp313-manylinux_2_24_aarch64.manylinux_2_28_aarch64.whl/sage/all__sagemath_gap.py
@@ -0,0 +1,24 @@
+# sage_setup: distribution = sagemath-gap
+# delvewheel: patch
+r"""
+Top level of the distribution package sagemath-gap
+
+This distribution makes the following feature available::
+
+    sage: from sage.features.sagemath import *
+    sage: sage__libs__gap().is_present()
+    FeatureTestResult('sage.libs.gap', True)
+"""
+from sage.all__sagemath_categories import *
+
+import sage.groups.perm_gps.permgroup_element
+
+from sage.geometry.all__sagemath_gap import *
+
+from sage.groups.all__sagemath_gap import *
+
+import sage.libs.gap.element
+
+from sage.libs.gap.libgap import libgap
+
+from sage.rings.all__sagemath_gap import *
