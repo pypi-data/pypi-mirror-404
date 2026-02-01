c5286813c9ad457a0c1b4545cd1a19952b7ea8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:29 +0000

    Add package passagemath-singular/10.8.1a4/passagemath_singular-10.8.1a4.tar.gz

---
+++ packages/passagemath-singular/passagemath_singular-10.8.1a4.tar.gz/passagemath_singular-10.8.1a4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from setuptools import Extension
+
+from sage_setup import sage_setup
+from sage.env import cython_aliases
+
+if not (len(sys.argv) > 1 and (sys.argv[1] in ["sdist", "egg_info", "dist_info"])):
+    aliases = cython_aliases(required_modules=('Singular',), optional_modules=())
+    ext_modules = [Extension("PySingular", ["PySingular.cpp"],
+                             extra_compile_args=aliases['SINGULAR_CFLAGS'],
+                             include_dirs=aliases['SINGULAR_INCDIR'],
+                             libraries=aliases['SINGULAR_LIBRARIES'],
+                             library_dirs=aliases['SINGULAR_LIBDIR'],
+                             extra_link_args=aliases['SINGULAR_LIBEXTRA'])]
+else:
+    ext_modules = []
+
+sage_setup(['sagemath-singular'],
+           recurse_packages=('sage', 'passagemath_singular'),
+           required_modules=('Singular', 'factory', 'givaro', 'gsl'),
+           spkgs=['singular'],
+           package_data={"sage": [
+               "ext_data/singular/**",
+           ]},
+           ext_modules=ext_modules)
