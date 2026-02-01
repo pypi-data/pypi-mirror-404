253241424e585fe7a2a04366f845618383d5c956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:13 +0000

    Add package passagemath-maxima/10.8.1a4/passagemath_maxima-10.8.1a4-cp311-cp311-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl

---
+++ packages/passagemath-maxima/passagemath_maxima-10.8.1a4-cp311-cp311-manylinux1_x86_64.manylinux_2_28_x86_64.manylinux_2_5_x86_64.whl/sage/interfaces/sage-maxima.lisp
@@ -0,0 +1,8 @@
+; sage_setup: distribution = sagemath-maxima
+
+;(setf *prompt-prefix* "<prompt>")
+;(setf *prompt-suffix* "</prompt>")
+;(setf *general-display-prefix* "<sage-display>")
+
+(setf *prompt-prefix* "<sage-display>")
+
