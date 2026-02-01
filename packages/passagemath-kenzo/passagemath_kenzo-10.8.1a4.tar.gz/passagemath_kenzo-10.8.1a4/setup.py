7a913b25c557b0a20f7d3f9013265a70b6892185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:20 +0000

    Add package passagemath-kenzo/10.8.1a4/passagemath_kenzo-10.8.1a4.tar.gz

---
+++ packages/passagemath-kenzo/passagemath_kenzo-10.8.1a4.tar.gz/passagemath_kenzo-10.8.1a4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-kenzo'],
+           recurse_packages=('sage', 'passagemath_kenzo'),
+           spkgs=['kenzo'],
+           package_data={},
+           py_limited_api=True)
