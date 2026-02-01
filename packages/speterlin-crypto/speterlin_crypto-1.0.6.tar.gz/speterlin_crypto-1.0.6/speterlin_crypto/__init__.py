a1d5848a139b9372bad8982f604798954f171189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:17 +0000

    Add package speterlin-crypto/1.0.6/speterlin_crypto-1.0.6.tar.gz

---
+++ packages/speterlin-crypto/speterlin_crypto-1.0.6.tar.gz/speterlin_crypto-1.0.6/speterlin_crypto/__init__.py
@@ -0,0 +1,10 @@
+"""A Python package for a suite of quant-trading opportunities in crypto with API integration: Kucoin Exchange & Binance Exchange for storing crypto assets (cold storage) and spot and derivatives trading (hot storage), Coin Market Cap (CMC) & CoinGecko (CG) & Google Trends for data collection.
+
+.. moduleauthor:: Sebastian B. Peterlin
+
+"""
+
+__version__ = '1.0.6'
+
+# from .module1 import *
+# from . import module1
