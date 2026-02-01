b4a993ba8e8cd637874ade7fef5cc14d729da2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:06:34 +0000

    Add package passagemath-categories/10.8.1rc0/passagemath_categories-10.8.1rc0-cp312-cp312-win_arm64.whl

---
+++ packages/passagemath-categories/passagemath_categories-10.8.1rc0-cp312-cp312-win_arm64.whl/sage/rings/laurent_series_ring_element.pxd
@@ -0,0 +1,11 @@
+# sage_setup: distribution = sagemath-categories
+from sage.structure.element cimport AlgebraElement
+from sage.rings.power_series_ring_element cimport PowerSeries
+
+cdef class LaurentSeries(AlgebraElement):
+    cdef PowerSeries __u
+    cdef long __n
+
+    cdef _normalize(self)
+    cpdef _add_(self, other)
+    cpdef _mul_(self, other)
---
+++ packages/passagemath-categories/passagemath_categories-10.8.1rc0-cp312-cp312-win_arm64.whl/sage/rings/polynomial/all__sagemath_categories.py
@@ -0,0 +1,22 @@
+# sage_setup: distribution = sagemath-categories
+# Quotient of polynomial ring
+from sage.rings.polynomial.polynomial_quotient_ring import PolynomialQuotientRing
+from sage.rings.polynomial.polynomial_quotient_ring_element import PolynomialQuotientRingElement
+
+# Univariate Polynomial Rings
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.rings.polynomial.polynomial_ring import polygen, polygens
+from sage.rings.polynomial.polynomial_element import Polynomial
+
+# Boolean Polynomial Rings
+from sage.rings.polynomial.polynomial_ring_constructor import BooleanPolynomialRing_constructor as BooleanPolynomialRing
+
+# Multivariate Polynomial Rings
+from sage.rings.polynomial.term_order import TermOrder
+from sage.rings.polynomial.multi_polynomial_element import degree_lowest_rational_function
+
+# Laurent Polynomial Rings
+from sage.rings.polynomial.laurent_polynomial_ring import LaurentPolynomialRing
+
+# Evaluation of cyclotomic polynomials
+from sage.rings.polynomial.cyclotomic import cyclotomic_value
