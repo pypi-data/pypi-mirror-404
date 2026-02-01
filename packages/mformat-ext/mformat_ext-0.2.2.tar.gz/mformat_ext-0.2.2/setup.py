8cef06793b1c347e03ff317d55c4f25826431831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:35 +0000

    Add package mformat-ext/0.2.2/mformat_ext-0.2.2.tar.gz

---
+++ packages/mformat-ext/mformat_ext-0.2.2.tar.gz/mformat_ext-0.2.2/setup.py
@@ -0,0 +1,25 @@
+#! /usr/local/bin/python3
+"""Setup file specifying build of .whl."""
+
+from setuptools import setup
+
+setup(
+  name='mformat_ext',
+  version='0.2.2',
+  description='Uniform way to write simple text extended with DOCX and ODT files',
+  author='Tom Björkholm',
+  author_email='klausuler_linnet0q@icloud.com',
+  python_requires='>=3.12',
+  packages=['mformat_ext'],
+  package_dir={'mformat_ext': 'src/mformat_ext'},
+  package_data={'mformat_ext': ['src/py.typed']},
+  install_requires=[  # pylint: disable=duplicate-code
+    'mformat >= 0.2.2',
+    'python-docx >= 1.2.0',
+    'odfdo >= 3.19.0',
+    'pip >= 25.3',
+    'setuptools >= 80.10.2',
+    'build >= 1.4.0',
+    'wheel>=0.46.3'
+  ]
+)
---
+++ packages/mformat-ext/mformat_ext-0.2.2.tar.gz/mformat_ext-0.2.2/src/mformat_ext/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/local/bin/python3
+"""Initialize the mformat_ext package."""
+
+# Copyright (c) 2025 - 2026 Tom Björkholm
+# MIT License
+#
