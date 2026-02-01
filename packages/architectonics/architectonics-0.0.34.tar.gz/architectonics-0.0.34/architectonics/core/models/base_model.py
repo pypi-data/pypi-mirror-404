ef77e1fe5f1e575a8f0d02c7bd78ec45a4db92fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:06 +0000

    Add package architectonics/0.0.34/architectonics-0.0.34.tar.gz

---
+++ packages/architectonics/architectonics-0.0.34.tar.gz/architectonics-0.0.34/architectonics/core/models/base_model.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+import uuid
+from dataclasses import asdict
+
+
+@dataclass(
+    kw_only=True,
+)
+class BaseModel:
+    id: str = field(
+        default_factory=lambda: str(uuid.uuid4()),
+    )
+
+    created_at: datetime = field(
+        default_factory=datetime.utcnow,
+    )
+
+    updated_at: datetime = field(
+        default_factory=datetime.utcnow,
+    )
+
+    def to_update_dict(self) -> dict[str, any]:
+        now = datetime.utcnow()
+
+        values = {k: v for k, v in asdict(self).items() if k not in {"id", "created_at"} and v is not None}
+
+        values["updated_at"] = now
+        self.updated_at = now
+
+        return values
