b16ac1b6db306e5adf9e07254ed7eb45c8a3ff7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:02:48 +0000

    Add package ingredient-parser-nlp/2.5.0/ingredient_parser_nlp-2.5.0.tar.gz

---
+++ packages/ingredient-parser-nlp/ingredient_parser_nlp-2.5.0.tar.gz/ingredient_parser_nlp-2.5.0/ingredient_parser/parsers.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python3
+
+import warnings
+from collections.abc import Iterable
+
+from ingredient_parser.en import inspect_parser_en, parse_ingredient_en
+
+from . import SUPPORTED_LANGUAGES
+from .dataclasses import ParsedIngredient, ParserDebugInfo
+
+warnings.simplefilter("always", DeprecationWarning)
+
+
+SUPPORTED_VOLUMEETRIC_UNITS_SYSTEMS = {
+    "us_customary",
+    "imperial",
+    "metric",
+    "australian",
+    "japanese",
+}
+
+
+def parse_ingredient(
+    sentence: str,
+    lang: str = "en",
+    separate_names: bool = True,
+    discard_isolated_stop_words: bool = True,
+    expect_name_in_output: bool = True,
+    string_units: bool = False,
+    imperial_units: bool = False,
+    volumetric_units_system: str = "us_customary",
+    foundation_foods: bool = False,
+) -> ParsedIngredient:
+    """Parse an ingredient sentence to return structured data.
+
+    Parameters
+    ----------
+    sentence : str
+        Ingredient sentence to parse.
+    lang : str
+        Language of sentence.
+        Currently supported options are: en.
+    separate_names : bool, optional
+        If True and the sentence contains multiple alternative ingredients, return an
+        IngredientText object for each ingredient name, otherwise return a single
+        IngredientText object.
+        Default is True.
+    discard_isolated_stop_words : bool, optional
+        If True, any isolated stop words in the name, preparation, or comment fields
+        are discarded.
+        Default is True.
+    expect_name_in_output : bool, optional
+        If True, if the model doesn't label any words in the sentence as the name,
+        fallback to selecting the most likely name from all tokens even though the
+        model gives it a different label. Note that this does guarantee the output
+        contains a name.
+        Default is True.
+    string_units : bool, optional
+        If True, return all IngredientAmount units as strings.
+        If False, convert IngredientAmount units to pint.Unit objects where possible.
+        Default is False.
+    imperial_units : bool, optional
+        If True, use imperial units instead of US customary units for pint.Unit objects
+        for the the following units: fluid ounce, cup, pint, quart, gallon.
+        Default is False, which results in US customary units being used.
+        This has no effect if string_units=True.
+
+        .. deprecated:: v2.5.0
+
+            Use ``volumetric_units_system="imperial"`` for the same functionality.
+
+    volumetric_units_system : str, optional
+        Sets the units system for volumetric measurements, like "cup" or "tablespoon".
+        Available options are "us_customary" (default), "imperial", "metric",
+        "australian", "japanese".
+        This has no effect if string_units=True.
+
+        .. versionadded:: v2.5.0
+
+    foundation_foods : bool, optional
+        If True, extract foundation foods from ingredient name. Foundation foods are
+        the fundamental foods without any descriptive terms, e.g. 'cucumber' instead
+        of 'organic cucumber'.
+        Default is False.
+
+    Returns
+    -------
+    ParsedIngredient
+        ParsedIngredient object of structured data parsed from input string.
+    """
+    if lang not in SUPPORTED_LANGUAGES:
+        raise ValueError(f'Unsupported language "{lang}"')
+
+    if imperial_units:
+        warnings.warn(
+            (
+                "imperial_units=True argument is deprecated. "
+                "Use volumetric_units_system='imperial'"
+            ),
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        volumetric_units_system = "imperial"
+
+    if volumetric_units_system not in SUPPORTED_VOLUMEETRIC_UNITS_SYSTEMS:
+        raise ValueError(
+            f'Unsupported volumetric_units_system "{volumetric_units_system}"'
+        )
+
+    match lang:
+        case "en":
+            return parse_ingredient_en(
+                sentence,
+                separate_names=separate_names,
+                discard_isolated_stop_words=discard_isolated_stop_words,
+                expect_name_in_output=expect_name_in_output,
+                string_units=string_units,
+                volumetric_units_system=volumetric_units_system,
+                foundation_foods=foundation_foods,
+            )
+        case _:
+            raise ValueError(f'Unrecognised value "{lang}"')
+
+
+def parse_multiple_ingredients(
+    sentences: Iterable[str],
+    lang: str = "en",
+    separate_names: bool = True,
+    discard_isolated_stop_words: bool = True,
+    expect_name_in_output: bool = True,
+    string_units: bool = False,
+    imperial_units: bool = False,
+    volumetric_units_system: str = "us",
+    foundation_foods: bool = False,
+) -> list[ParsedIngredient]:
+    """Parse multiple ingredient sentences in one go.
+
+    This function accepts a list of sentences, with element of the list representing
+    one ingredient sentence.
+    A list of dictionaries is returned, with optional confidence values.
+    This function is a simple for-loop that iterates through each element of the
+    input list.
+
+    Parameters
+    ----------
+    sentences : Iterable[str]
+        Iterable of sentences to parse.
+    lang : str
+        Language of sentence.
+        Currently supported options are: en.
+    separate_names : bool, optional
+        If True and the sentence contains multiple alternative ingredients, return an
+        IngredientText object for each ingredient name, otherwise return a single
+        IngredientText object.
+        Default is True.
+    discard_isolated_stop_words : bool, optional
+        If True, any isolated stop words in the name, preparation, or comment fields
+        are discarded.
+        Default is True.
+    expect_name_in_output : bool, optional
+        If True, if the model doesn't label any words in the sentence as the name,
+        fallback to selecting the most likely name from all tokens even though the
+        model gives it a different label. Note that this does guarantee the output
+        contains a name.
+        Default is True.
+    string_units : bool
+        If True, return all IngredientAmount units as strings.
+        If False, convert IngredientAmount units to pint.Unit objects where possible.
+        Default is False.
+    imperial_units : bool
+        If True, use imperial units instead of US customary units for pint.Unit objects
+        for the the following units: fluid ounce, cup, pint, quart, gallon.
+        Default is False, which results in US customary units being used.
+        This has no effect if string_units=True.
+
+        .. deprecated:: v2.5.0
+
+            Use ``volumetric_units_system="imperial"`` for the same functionality.
+
+    volumetric_units_system : str, optional
+        Sets the units system for volumetric measurements, like "cup" or "tablespoon".
+        Available options are "us_customary" (default), "imperial", "metric",
+        "australian", "japanese".
+        This has no effect if string_units=True.
+
+        .. versionadded:: v2.5.0
+
+    foundation_foods : bool, optional
+        If True, extract foundation foods from ingredient name. Foundation foods are
+        the fundamental foods without any descriptive terms, e.g. 'cucumber' instead
+        of 'organic cucumber'.
+        Default is False.
+
+    Returns
+    -------
+    list[ParsedIngredient]
+        List of ParsedIngredient objects of structured data parsed from input sentences.
+    """
+    return [
+        parse_ingredient(
+            sentence,
+            lang=lang,
+            separate_names=separate_names,
+            discard_isolated_stop_words=discard_isolated_stop_words,
+            expect_name_in_output=expect_name_in_output,
+            string_units=string_units,
+            imperial_units=imperial_units,
+            volumetric_units_system=volumetric_units_system,
+            foundation_foods=foundation_foods,
+        )
+        for sentence in sentences
+    ]
+
+
+def inspect_parser(
+    sentence: str,
+    lang: str = "en",
+    separate_names: bool = True,
+    discard_isolated_stop_words: bool = True,
+    expect_name_in_output: bool = True,
+    string_units: bool = False,
+    imperial_units: bool = False,
+    volumetric_units_system: str = "us_customary",
+    foundation_foods: bool = False,
+) -> ParserDebugInfo:
+    """Return intermediate objects generated during parsing for inspection.
+
+    Parameters
+    ----------
+    sentence : str
+        Ingredient sentence to parse.
+    lang : str
+        Language of sentence.
+        Currently supported options are: en.
+    separate_names : bool, optional
+        If True and the sentence contains multiple alternative ingredients, return an
+        IngredientText object for each ingredient name, otherwise return a single
+        IngredientText object.
+        Default is True.
+    discard_isolated_stop_words : bool, optional
+        If True, any isolated stop words in the name, preparation, or comment fields
+        are discarded.
+        Default is True.
+    expect_name_in_output : bool, optional
+        If True, if the model doesn't label any words in the sentence as the name,
+        fallback to selecting the most likely name from all tokens even though the
+        model gives it a different label. Note that this does guarantee the output
+        contains a name.
+        Default is True.
+    string_units : bool
+        If True, return all IngredientAmount units as strings.
+        If False, convert IngredientAmount units to pint.Unit objects where possible.
+        Default is False.
+    imperial_units : bool
+        If True, use imperial units instead of US customary units for pint.Unit objects
+        for the the following units: fluid ounce, cup, pint, quart, gallon.
+        Default is False, which results in US customary units being used.
+        This has no effect if string_units=True.
+
+        .. deprecated:: v2.5.0
+
+            Use ``volumetric_units_system="imperial"`` for the same functionality.
+
+    volumetric_units_system : str, optional
+        Sets the units system for volumetric measurements, like "cup" or "tablespoon".
+        Available options are "us_customary" (default), "imperial", "metric",
+        "australian", "japanese".
+        This has no effect if string_units=True.
+
+        .. versionadded:: v2.5.0
+
+    foundation_foods : bool, optional
+        If True, extract foundation foods from ingredient name. Foundation foods are
+        the fundamental foods without any descriptive terms, e.g. 'cucumber' instead
+        of 'organic cucumber'.
+        Default is False.
+
+    Returns
+    -------
+    ParserDebugInfo
+        ParserDebugInfo object containing the PreProcessor object, PostProcessor
+        object and Tagger.
+    """
+    if lang not in SUPPORTED_LANGUAGES:
+        raise ValueError(f'Unsupported language "{lang}"')
+
+    if imperial_units:
+        warnings.warn(
+            (
+                "imperial_units=True argument is deprecated. "
+                "Use volumetric_units_system='imperal'"
+            ),
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        volumetric_units_system = "imperial"
+
+    if volumetric_units_system not in SUPPORTED_VOLUMEETRIC_UNITS_SYSTEMS:
+        raise ValueError(
+            f'Unsupported volumetric_units_system "{volumetric_units_system}"'
+        )
+
+    match lang:
+        case "en":
+            return inspect_parser_en(
+                sentence,
+                separate_names=separate_names,
+                discard_isolated_stop_words=discard_isolated_stop_words,
+                expect_name_in_output=expect_name_in_output,
+                string_units=string_units,
+                volumetric_units_system=volumetric_units_system,
+                foundation_foods=foundation_foods,
+            )
+        case _:
+            raise ValueError(f'Unrecognised value "{lang}"')
