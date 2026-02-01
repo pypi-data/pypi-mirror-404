ecf1e2f285ac55f4362e9074de0b058dbda83b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:02:52 +0000

    Add package passagemath-categories/10.8.1a4/passagemath_categories-10.8.1a4-cp312-cp312-win_amd64.whl

---
+++ packages/passagemath-categories/passagemath_categories-10.8.1a4-cp312-cp312-win_amd64.whl/sage/schemes/all__sagemath_categories.py
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-categories
+from sage.schemes.generic.all import *
+from sage.schemes.affine.all import *
+from sage.schemes.projective.all import *
+from sage.schemes.product_projective.all import *
+from sage.schemes.weighted_projective.all import *
