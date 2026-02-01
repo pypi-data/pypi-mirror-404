989203b74f1bf44bce7a0dbdb7e503280c4b1c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:32:09 +0000

    Add package passagemath-modules/10.8.1a4/passagemath_modules-10.8.1a4-cp314-cp314t-win_amd64.whl

---
+++ packages/passagemath-modules/passagemath_modules-10.8.1a4-cp314-cp314t-win_amd64.whl/sage/matrix/all.py
@@ -0,0 +1,9 @@
+# sage_setup: distribution = sagemath-modules
+from sage.misc.lazy_import import lazy_import
+from sage.matrix.matrix_space import MatrixSpace
+from sage.matrix.constructor import (matrix, Matrix, column_matrix, random_matrix,
+                                     diagonal_matrix, identity_matrix, block_matrix,
+                                     block_diagonal_matrix, jordan_block, zero_matrix,
+                                     ones_matrix, elementary_matrix, companion_matrix)
+Mat = MatrixSpace
+del lazy_import
