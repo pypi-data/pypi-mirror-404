aa20c061bebd2175412519a288e9b7b62e5f902e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 15:07:00 +0000

    Add package math3d-py/1.4.9/math3d_py-1.4.9-cp313-cp313-macosx_26_0_arm64.whl

---
+++ packages/math3d-py/math3d_py-1.4.9-cp313-cp313-macosx_26_0_arm64.whl/math3d/test/remap_test.py
@@ -0,0 +1,106 @@
+import os.path
+from typing import List, Tuple
+
+from math3d import AABB, Extent, Remap, Vector4
+
+from math3d import Vector3
+
+class OBJReader:
+
+    def __init__(self, filename:str):
+        self._filename = filename
+
+    @property
+    def data(self) -> Tuple[List[Vector3], List[Tuple[int, int, int]]]:
+        with open(self._filename, 'r') as obj_file:
+            vertices: List[Vector3] = []
+            faces: List[Tuple[int, int, int]] = []
+            content = obj_file.readlines()
+            for line in content:
+                if line.startswith('v '):
+                    _, x, y, z = line.split()
+                    vertices.append(Vector3(float(x), float(y), float(z)))
+                elif line.startswith('f '):
+                    _, i, j, k = line.split()
+                    faces.append((int(i), int(j), int(k)))
+            return vertices, faces
+
+
+class OBJWriter:
+
+    def __init__(self, filename:str):
+        self._filename = filename
+        self._vertices: List[Vector3] = []
+        self._faces: List[Tuple[int, int, int]] = []
+
+    @property
+    def vertices(self):
+        return self._vertices
+
+    @vertices.setter
+    def vertices(self, verts: List[Vector3]):
+        self._vertices = verts
+
+
+    @property
+    def faces(self):
+        return self._faces
+
+    @faces.setter
+    def faces(self, faces: List[Tuple[int, int, int]]):
+        self._faces = faces
+
+    def write(self) -> None:
+        with open(self._filename, 'w') as obj_file:
+            vertices: List[str] = []
+            for v in self._vertices:
+                vertices.append(f'v {v.x} {v.y} {v.z}\n')
+            obj_file.writelines(vertices)
+            faces: List[str] = []
+            for f in self._faces:
+                faces.append(f'f {f[0]} {f[1]} {f[2]}\n')
+            obj_file.writelines(faces)
+
+
+def test_mesh_normalization():
+
+    _TEST_OBJ_FILE = os.path.join(os.path.dirname(__file__), 'suzanne.obj')
+    _TEST_OBJ_FILE_OUTPUT = os.path.join(os.path.dirname(__file__), 'suzanne_normalized.obj')
+    _TEST_OBJ_FILE_UNMAPPED_OUTPUT = os.path.join(os.path.dirname(__file__), 'suzanne_unmapped.obj')
+
+    reader = OBJReader(_TEST_OBJ_FILE)
+    
+    vertices, faces = reader.data
+    
+    extent_x: Extent = Extent()
+    extent_y: Extent = Extent()
+    extent_z: Extent = Extent()
+    for vertex in vertices:
+        extent_x.update(vertex.x)
+        extent_y.update(vertex.y)
+        extent_z.update(vertex.z)
+    bounds = AABB(Vector3(extent_x.min, extent_y.min, extent_z.min),
+                  Vector3(extent_x.max, extent_y.max, extent_z.max))
+    
+    normalized_bounds = AABB(Vector3(-1, -1, -1), Vector3(1, 1, 1))
+    
+    remap: Remap = Remap(bounds, normalized_bounds)
+    normalized_vertices = [remap.remap(vertex) for vertex in vertices]
+
+    writer = OBJWriter(_TEST_OBJ_FILE_OUTPUT)
+    writer.vertices = normalized_vertices
+    writer.faces = faces
+    writer.write()
+
+    inverse = remap.decode()
+    homogenous_vertices = [inverse * Vector4(vertex) for vertex in normalized_vertices]
+    unmapped_vertices = [Vector3(vertex.x, vertex.y, vertex.z) for vertex in homogenous_vertices]
+
+    writer = OBJWriter(_TEST_OBJ_FILE_UNMAPPED_OUTPUT)
+    writer.vertices = unmapped_vertices
+    writer.faces = faces
+    writer.write()
+
+
+
+
---
+++ packages/math3d-py/math3d_py-1.4.9-cp313-cp313-macosx_26_0_arm64.whl/math3d/types.hpp
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "pybind11/pybind11.h"
+
+#include "SupportingTypes.h"
+#include "primitives/Triangle.h"
+
+namespace py = pybind11;
+namespace m3d = math3d;
+
+template<typename T>
+void bind_Extent(py::module_ const& module, std::string_view className) {
+    using Extent = m3d::Extent<T>;
+    py::class_<Extent>(module, className.data())
+    .def(py::init([] {
+        Extent extent;
+        extent.min = std::numeric_limits<T>::max();
+        extent.max = -std::numeric_limits<T>::max();
+        return extent;
+    }))
+    .def(py::init([](T min, T max) {
+        Extent extent {};
+        extent.min = min;
+        extent.max = max;
+        return extent;
+    }))
+    .def_property("min",
+        [](Extent const& extent) {
+            return extent.min;
+        },
+        [](Extent& extent, T min) {
+           extent.min = min;
+        }
+    )
+    .def_property("max",
+        [](Extent const& extent) {
+            return extent.max;
+        },
+        [](Extent& extent, T max) {
+           extent.max = max;
+        }
+    )
+    .def_property_readonly("length", &Extent::length)
+    .def_property_readonly("center", &Extent::center)
+    .def("update", [](Extent& extent, T val) {
+        extent.min = std::min(extent.min, val);
+        extent.max = std::max(extent.max, val);
+    })
+    .def("__str__", [](Extent const& extent) {
+        return extent.asString();
+    })
+    .def("__repr__", [](Extent const& extent) {
+        return std::format("Extent = {} Valid = {}", extent.asString(), extent.min < extent.max);
+    });
+}
+
+template<typename T>
+void bind_Bounds(py::module_ const& module, std::string_view className) {
+    using Bounds = m3d::Bounds3D<T>;
+    py::class_<Bounds>(module, className.data())
+    .def(py::init())
+    .def(py::init([](m3d::Extent<T> const& x, m3d::Extent<T> const& y, m3d::Extent<T> const& z) {
+        return Bounds {x, y, z};
+    }))
+    .def(py::init([](m3d::Vector3<T> const& min, m3d::Vector3<T> const& max) {
+        return Bounds(m3d::Extent<T>{min.x, max.x}, m3d::Extent<T>{min.y, max.y}, m3d::Extent<T>{min.z, max.z});
+    }))
+    .def_property_readonly("min", &Bounds::min)
+    .def_property_readonly("max", &Bounds::max)
+    .def_property_readonly("corners", &Bounds::corners)
+    .def_property_readonly("edges", &Bounds::edges)
+    .def("merge", &Bounds::merge)
+    .def("transform", [](Bounds const& self, m3d::Matrix<T, 4, 4> const& transform) {
+        auto min = m3d::Vector4<T>{self.min(), 1.0};
+        auto max = m3d::Vector4<T>{self.max(), 1.0};
+        auto newMin = transform * min;
+        auto newMax = transform * max;
+        return Bounds({newMin.x, newMax.x}, {newMin.y, newMax.y}, {newMin.z, newMax.z});
+    })
+    .def("__str__", [](Bounds const& bounds) {
+        return bounds.asString();
+    })
+    .def("__repr__", [](Bounds const& bounds) {
+        return std::format(
+            "{} Valid = {}\n"
+            "Center = {}\n"
+            "X Length = {}\n"
+            "Y Length = {}\n"
+            "Z Length = {}\n"
+            "Diagonal Length {}", bounds.asString(), bounds.isValid(), bounds.center().asString(),
+            bounds.x.length(), bounds.y.length(), bounds.z.length(), bounds.length());
+    });
+}
+
+template<typename T>
+void bind_Remapper(py::module_ const& module, std::string_view className) {
+    using Bounds = m3d::Bounds3D<T>;
+    using Remapper = m3d::Remapper<T>;
+    py::class_<Remapper>(module, className.data())
+    .def(py::init([](Bounds const& source, Bounds const& destination) {
+        return Remapper {source, destination};
+    }))
+    .def("remap", &Remapper::operator())
+    .def("decode", &Remapper::getInverseTransform);
+}
+
+inline void bind_Triangle(py::module_ const& module, std::string_view className) {
+    using Tri = m3d::Triangle;
+    using Points = m3d::Triangle::Points;
+
+    py::class_<Tri>(module, className.data())
+    .def(py::init([](Points const& vertices){
+        return Tri{vertices};
+    }))
+    .def(py::init([](Tri::Point3D const& a, Tri::Point3D const& b, Tri::Point3D const& c) {
+        return Tri{a, b, c};
+    }))
+    .def_property_readonly("points", &Tri::getPoints)
+    .def_property_readonly("area", &Tri::getArea)
+    .def_property_readonly("signed_area", &Tri::getSignedArea)
+    .def("barycentric", &Tri::getBarycentricCoordinates)
+    .def("contains", &Tri::isPointInTriangle)
+    .def("projection", &Tri::getProjection)
+    .def("distance", &Tri::getDistanceToPoint, py::arg("point"), py::arg("absolute") = true)
+    .def_property_readonly("normal", &Tri::getNormal);
+}
