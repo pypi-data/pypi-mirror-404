6d9505d5aae01678dbf2c28ac777c86757401a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 16:05:35 +0000

    Add package ttsign-mobile/1.0.0/ttsign_mobile-1.0.0-py3-none-any.whl

---
+++ packages/ttsign-mobile/ttsign_mobile-1.0.0-py3-none-any.whl/ttsign_mobile/__init__.py
@@ -0,0 +1,9 @@
+"""
+ttsign_mobile - Python library for generating TikTok mobile API signatures
+"""
+
+from .ttsign_mobile import ttsign_mobile
+
+__version__ = "1.0.0"
+__author__ = "djoub"
+__all__ = ["ttsign_mobile"]
