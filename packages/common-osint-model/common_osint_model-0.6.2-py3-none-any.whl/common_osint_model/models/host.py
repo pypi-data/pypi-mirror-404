ff98fdc90e8ef2e4dd877d422a64728d87c2c99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 08:05:52 +0000

    Add package common-osint-model/0.6.2/common_osint_model-0.6.2-py3-none-any.whl

---
+++ packages/common-osint-model/common_osint_model-0.6.2-py3-none-any.whl/common_osint_model/models/host.py
@@ -0,0 +1,233 @@
+import ipaddress
+import json
+from datetime import datetime, UTC
+from typing import Optional, Dict, List, Union
+
+from pydantic import field_validator, BaseModel
+
+from common_osint_model.models import ShodanDataHandler, CensysDataHandler, BinaryEdgeDataHandler, Logger
+from common_osint_model.models.autonomous_system import AutonomousSystem
+from common_osint_model.models.domain import Domain
+from common_osint_model.models.service import Service
+from common_osint_model.utils import flatten
+from censys_platform.models import HostAsset, HostAssetWithMatchedServices
+
+
+class Host(BaseModel, ShodanDataHandler, CensysDataHandler, BinaryEdgeDataHandler, Logger):
+    """This class represents a host and can be used to handle results from the common model in a pythonic way."""
+    ip: str
+    # Information about the autonomous system the host is assigned to
+    autonomous_system: Optional[AutonomousSystem] = None
+    # List of services running (listening) on the IP
+    services: Optional[List[Service]] = None
+    # List of open ports also mentioned in the open
+    ports: Optional[List[int]] = None
+    # Timestamps for activity tracking
+    first_seen: Optional[datetime] = datetime.now(UTC)
+    last_seen: Optional[datetime] = datetime.now(UTC)
+    # A list of domains, fqdns, common names - or other attributes which represent domainnames -  assigned to the host
+    domains: Optional[List[Domain]] = None
+    # This represents the source where the host information was obtained, e.g. shodan, censys...
+    source: Optional[str] = None
+    # Optionally, the used query to find the host can be assigned to the object also which might be useful for comparing
+    # different hosts later on
+    query: Optional[str] = None
+
+    @field_validator("ip")
+    @classmethod
+    def validates_ip(cls, v):
+        try:
+            ipaddress.ip_address(v)
+        except Exception as e:
+            raise ValueError(f"\"{v}\" is not a correct IP address or at least it is not parseable with the ipaddress"
+                             f"module: {e}")
+        return v
+
+    @property
+    def services_dict(self):
+        """Returns the services as dictionary in the form of {port: service}. Uses exclude_none to skip empty keys."""
+        # Load the JSON dump, so datetime objects are in iso format.
+        json_dict = json.loads(self.json(exclude_none=True))
+        json_dict.update({s["port"]: s for s in json_dict["services"]})
+        del json_dict["services"]
+        return json_dict
+
+    @property
+    def flattened_dict(self):
+        """Dict in the flattened format."""
+        return flatten(self.services_dict)
+
+    @property
+    def service_ports(self):
+        """Dynamic attribute which loops over available services and grabs the port number. This is kind of redundant
+        to the ports attribute, if given, but can help to easily get the values needed for the attribute. Unfortunately
+        Pydantic does not support these kind of properties in the data model right now."""
+        return [service.port for service in self.services]
+
+    def flattened_json(self) -> str:
+        """Returns in the structure formally introduced with the common model."""
+        return json.dumps(self.flattened_dict, indent=2)
+
+    @classmethod
+    def from_shodan(cls, d: Dict, skip_shodan_domains: Optional[bool] = False):
+        if "data" in d and isinstance(d["data"], List):
+            d = d["data"]
+        domains = []
+        domain_strings = []
+        if isinstance(d, List):
+            for entry in d:
+                if "domains" in entry and not skip_shodan_domains:
+                    for domain in entry["domains"]:
+                        if domain not in domain_strings:
+                            domain_strings.append(domain)
+                            domains.append(Domain(domain=domain, source="shodan", type="domain"))
+                # Check Shodans reverse dns lookups
+                if "hostnames" in entry and not skip_shodan_domains:
+                    for hostname in entry["hostnames"]:
+                        if hostname not in domain_strings:
+                            domain_strings.append(hostname)
+                            domains.append(Domain(domain=hostname, source="shodan", type="rdns"))
+            ip = d[0]["ip_str"]
+            services = [Service.from_shodan(service) for service in d]
+        else:
+            ip = d["ip_str"]
+            services = [Service.from_shodan(d)]
+        for service in services:
+            if service.tls:
+                for domain in service.tls.certificate.domains:
+                    if domain not in domain_strings:
+                        domain_strings.append(domain)
+                        domains.append(Domain(
+                            domain=domain,
+                            first_seen=service.tls.certificate.issued,
+                            last_seen=service.tls.certificate.expires,
+                            source="shodan",
+                            type="common_name"
+                        ))
+        autonomous_system = AutonomousSystem.from_shodan(d)
+        return Host(
+            ip=ip,
+            autonomous_system=autonomous_system,
+            services=services,
+            domains=domains,
+            source="shodan",
+            ports=[service.port for service in services]
+        )
+    
+    @classmethod
+    def from_censys(cls, host: Dict | HostAsset | HostAssetWithMatchedServices):
+        if isinstance(host, HostAsset) or isinstance(host, HostAssetWithMatchedServices):
+            domains = list()
+            services = list()
+            
+            # Handle Forward DNS
+            if host.resource.dns is not None and host.resource.dns.forward_dns is not None:
+                for domain in host.resource.dns.forward_dns.keys():
+                    domains.append(
+                        Domain(
+                            domain=domain,
+                            first_seen = host.resource.dns.forward_dns.get(domain).resolve_time,
+                            source = "censys",
+                            type = host.resource.dns.forward_dns.get(domain).record_type
+                        )
+                    )
+            
+            # Handle Reverse DNS
+            if host.resource.dns is not None and host.resource.dns.reverse_dns is not None:
+                for reverse_dns_domain in host.resource.dns.reverse_dns.names:
+                    domains.append(
+                        Domain(
+                            domain=reverse_dns_domain,
+                            first_seen = host.resource.dns.reverse_dns.resolve_time,
+                            source = "censys",
+                            type = "rdns"
+                        )
+                    )
+            
+            # Handle Services
+            if host.resource.services is not None:
+                for service in host.resource.services:
+                    services.append(Service.from_censys(service=service))
+            
+            # Return Host
+            return Host(
+                ip=host.resource.ip,
+                domains=domains,
+                source="censys",
+                services=services,
+                ports=[service.port for service in services],
+                autonomous_system=AutonomousSystem.from_censys(host.resource.autonomous_system)
+            )
+        if isinstance(host, Dict):
+            return cls._from_censys_dict(host)
+
+    @classmethod
+    def from_binaryedge(cls, d: Union[Dict, List]):
+        """This can either be a complete query result, or a list of services running on the same ip."""
+        if isinstance(d, Dict) and "results" in d:
+            # This is a complete result dictionary, extract the list of services.
+            d = d["results"][list(d["results"].keys())[0]]
+        elif isinstance(d, Dict) and "events" in d:
+            d = d["events"]
+
+        services = {}
+        for service in d:
+            port = service["target"]["port"]
+            if port not in services:
+                services[port] = [service]
+            else:
+                services[port].append(service)
+        services_objects = [Service.from_binaryedge(service) for service in services.values()]
+        ip = d[0]["target"]["ip"]
+        domains = []
+        domain_strings = []
+        for service in services_objects:
+            if service.tls:
+                for domain in service.tls.certificate.domains:
+                    if domain not in domain_strings:
+                        domain_strings.append(domain)
+                        domains.append(Domain(
+                            domain=domain,
+                            first_seen=service.tls.certificate.issued,
+                            last_seen=service.tls.certificate.expires,
+                            source="binaryedge",
+                            type="common_name"
+                        ))
+        return Host(
+            ip=ip,
+            services=services_objects,
+            domains=domains,
+            source="binaryedge",
+            ports=[service.port for service in services_objects]
+        )
+
+    @classmethod
+    def _from_censys_dict(cls, d: Dict):
+        ip = d["ip"]
+        services = []
+        for service in d["services"]:
+            services.append(Service.from_censys(service))
+
+        domains = []
+        domain_strings = []
+        for service in services:
+            if service.tls:
+                for domain in service.tls.certificate.domains:
+                    if domain not in domain_strings:
+                        domain_strings.append(domain)
+                        domains.append(Domain(
+                            domain=domain,
+                            # Currently not given by API
+                            # first_seen=service.tls.certificate.issued,
+                            # last_seen=service.tls.certificate.expires,
+                            source="censys",
+                            type="common_name"
+                        ))
+        return Host(
+            ip=ip,
+            autonomous_system=AutonomousSystem.from_censys(d),
+            services=services,
+            domains=domains,
+            source="censys",
+            ports=[service.port for service in services]
+        )
---
+++ packages/common-osint-model/common_osint_model-0.6.2-py3-none-any.whl/common_osint_model/models/service.py
@@ -0,0 +1,235 @@
+from datetime import datetime, UTC
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel
+
+from common_osint_model.models import (
+    ShodanDataHandler,
+    CensysDataHandler,
+    BinaryEdgeDataHandler,
+    Logger,
+)
+from common_osint_model.models.http import HTTPComponent
+from common_osint_model.models.ssh import SSHComponent
+from common_osint_model.models.tls import TLSComponent
+from common_osint_model.models.dns import DNSComponent
+from common_osint_model.utils import hash_all
+
+from censys_platform.models import Service as CensysService
+
+
+class Service(
+    BaseModel, ShodanDataHandler, CensysDataHandler, BinaryEdgeDataHandler, Logger
+):
+    """Represents a single service answering connections on specific ports."""
+
+    port: int
+    protocol: Optional[str] = None
+    # Banner is optional as not every scanning service offers complete banners as response. Banners might be
+    # reconstructed from the data, but some attributes might have the wrong order then (e.g. HTTP headers).
+    # The according hashes are also not reliable because of this.
+    banner: Optional[str] = None
+    md5: Optional[str] = None
+    sha1: Optional[str] = None
+    sha256: Optional[str] = None
+    murmur: Optional[str] = None
+    ja4tscan: Optional[str] = None
+    # Every service object should include these timestamps. "timestamp" can be used for tracking the observation
+    # timestamp from scanning services (e.g. Shodan)
+    first_seen: Optional[datetime] = datetime.now(UTC)
+    last_seen: Optional[datetime] = datetime.now(UTC)
+    timestamp: Optional[datetime] = None
+    # We need to include every possible service component here. In order to not export empty dictionary keys, the class
+    # object can be exported with dict(exclude_none=True), so e.g. empty tls keys are skipped.
+    http: Optional[HTTPComponent] = None
+    tls: Optional[TLSComponent] = None
+    ssh: Optional[SSHComponent] = None
+    dns: Optional[DNSComponent] = None
+    # Typically hosts consist of different services which might be discovered by different scanning services, so
+    # remarking which service was observed by which scanner might be a good idea.
+    source: str
+
+    @classmethod
+    def from_shodan(cls, d: Dict):
+        """Creates an instance of this class using a dictionary with typical shodan data."""
+        if isinstance(d, List):
+            cls.info(
+                "The dictionary given is a list. Typically this list represents multiple services. Iterate over "
+                "the list to create Service objects for every item available. "
+                "This method just uses the first item."
+            )
+            d = d[0]
+
+        port = d["port"]
+        sshobj = None
+        if "ssh" in d:
+            sshobj = SSHComponent.from_shodan(d)
+
+        httpobj = None
+        if "http" in d:
+            httpobj = HTTPComponent.from_shodan(d)
+
+        tlsobj = None
+        if "ssl" in d:
+            tlsobj = TLSComponent.from_shodan(d)
+
+        dnsobj = None
+        if "dns" in d:
+            dnsobj = DNSComponent.from_shodan(d)
+
+        banner = d["data"]
+        md5, sha1, sha256, murmur = hash_all(banner.encode("utf-8"))
+
+        return Service(
+            port=port,
+            banner=d["data"],
+            md5=md5,
+            sha1=sha1,
+            sha256=sha256,
+            murmur=murmur,
+            ssh=sshobj,
+            http=httpobj,
+            tls=tlsobj,
+            dns=dnsobj,
+            timestamp=datetime.fromisoformat(d["timestamp"]),
+            source="shodan",
+        )
+
+    @classmethod
+    def from_censys(cls, service: Dict | CensysService):
+        if isinstance(service, CensysService):
+            port = service.port
+            protocol = None
+            if service.protocol is not None and not service.protocol == "UNKNOWN":
+                protocol = service.protocol
+            else:
+                protocol = service.transport_protocol
+            
+            # Set various banner and pivot hashes
+            banner = service.banner
+            md5, sha1, sha256, murmur = None, None, None, None
+            if banner:
+                md5, sha1, sha256, murmur = hash_all(banner.encode("utf-8"))
+            # Overwrite calcuated sha256 hash with the one from source
+            sha256 = service.banner_hash_sha256
+            ja4tscan = None
+            if service.ja4tscan is not None:
+                ja4tscan = service.ja4tscan.fingerprint
+
+            # TODO: Implement TLSComponent, HTTPComponent, DNSComponent, SSHComponent
+
+            timestamp = None
+            try:
+                timestamp = datetime.fromisoformat(service.scan_time)
+            except ValueError as ve:
+                cls.warning(
+                    f"{service.scan_time} could not be parsed as timestamp: {ve}"
+                )
+            return Service(
+                port=port,
+                protocol=protocol,
+                banner=banner,
+                md5=md5,
+                sha1=sha1,
+                sha256=sha256,
+                murmur=murmur,
+                ja4tscan=ja4tscan,
+                timestamp=timestamp,
+                source="censys"
+            )
+
+            pass
+        if isinstance(service, Dict):
+            return cls._from_censys_dict(d=service)
+        
+
+    @classmethod
+    def from_binaryedge(cls, d: List):
+        """Creates an instance of this class using a dictionary with typical BinaryEdge data. Contrary to the other
+        scanning services, binaryedge provides multiple entries per port."""
+        port = d[0]["target"]["port"]
+        type_index: Dict[str, int] = {
+            service["origin"]["type"]: idx for idx, service in enumerate(d)
+        }
+
+        httpobj = None
+        if "webv2" in type_index:
+            httpobj = HTTPComponent.from_binaryedge(d[type_index["webv2"]])
+
+        tlsobj = None
+        if "ssl-simple" in type_index:
+            tlsobj = TLSComponent.from_binaryedge(d[type_index["ssl-simple"]])
+
+        sshobj = None
+        if "ssh" in type_index:
+            sshobj = SSHComponent.from_binaryedge(d[type_index["ssh"]])
+
+        banner = None
+        md5, sha1, sha256, murmur = None, None, None, None
+        if "service-simple" in type_index:
+            banner = d[type_index["service-simple"]]["result"]["data"]["service"].get(
+                "banner", None
+            )
+        if banner:
+            md5, sha1, sha256, murmur = hash_all(banner.encode("utf-8"))
+
+        return Service(
+            port=port,
+            http=httpobj,
+            tls=tlsobj,
+            ssh=sshobj,
+            banner=banner,
+            md5=md5,
+            sha1=sha1,
+            sha256=sha256,
+            murmur=murmur,
+            source="binaryedge",
+        )
+
+    @classmethod
+    def _from_censys_dict(cls, d: Dict):
+        """Creates an instance of this class using a dictionary with typical Censys data."""
+        port = d["port"]
+        banner = d.get("banner", None)
+        md5, sha1, sha256, murmur = None, None, None, None
+        if banner:
+            md5, sha1, sha256, murmur = hash_all(banner.encode("utf-8"))
+
+        httpobj = None
+        if "http" in d:
+            httpobj = HTTPComponent.from_censys(d)
+
+        tlsobj = None
+        if "tls" in d:
+            tlsobj = TLSComponent.from_censys(d)
+
+        sshobj = None
+        if "ssh" in d:
+            sshobj = SSHComponent.from_censys(d)
+
+        dnsobj = None
+        if "dns" in d:
+            dnsobj = DNSComponent.from_censys(d)
+
+        timestamp = None
+        if "observed_at" in d:
+            try:
+                timestamp = datetime.fromisoformat(d["observed_at"])
+            except ValueError as ve:
+                cls.warning(
+                    f"{d['observed_at']} could not be parsed as timestamp: {ve}"
+                )
+        return Service(
+            port=port,
+            banner=banner,
+            md5=md5,
+            sha1=sha1,
+            sha256=sha256,
+            murmur=murmur,
+            http=httpobj,
+            tls=tlsobj,
+            ssh=sshobj,
+            dns=dnsobj,
+            timestamp=timestamp,
+            source="censys",
+        )
