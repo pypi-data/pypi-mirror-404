f4089a03fcab4b967d9e35de37305ccb17bc6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:31:29 +0000

    Add package aspen-pysys/0.0.7/aspen_pysys-0.0.7-py3-none-any.whl

---
+++ packages/aspen-pysys/aspen_pysys-0.0.7-py3-none-any.whl/aspen_pysys/constants.py
@@ -0,0 +1,8 @@
+_NAME_STR = "name"
+_NAMES_STR = "Names"
+_TYPE_STR = "VisibleTypeName"
+_VALUE_STR = "Value"
+_GET_VALUES_STR = "GetValues"
+_VALUES_STR = "Values"
+
+_UPPERCASE_WORDS_TO_IGNORE = {"ID"}
---
+++ packages/aspen-pysys/aspen_pysys-0.0.7-py3-none-any.whl/aspen_pysys/types/object_type.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class HysysObjectType(Enum):
+    OBJECT = 0
+    CONSTANT = 1
+    ARRAY = 2
+    DICT = 3
