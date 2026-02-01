be3272889965a449de140efb9c46b3db45918b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:20 +0000

    Add package Geode-Common/33.16.4/geode_common-33.16.4-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/Geode-Common/geode_common-33.16.4-cp312-cp312-manylinux_2_28_x86_64.whl/geode_common/cutter_surface.pyi
@@ -0,0 +1,4 @@
+from __future__ import annotations
+from geode_common.lib64.geode_common_py_cutter_surface import CommonCutterSurfaceLibrary
+import opengeode as opengeode
+__all__: list[str] = ['CommonCutterSurfaceLibrary', 'opengeode']
---
+++ packages/Geode-Common/geode_common-33.16.4-cp312-cp312-manylinux_2_28_x86_64.whl/geode_common/lib64/geode_common_py_cutter_surface.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Common Python binding for cutter_surface
+"""
+from __future__ import annotations
+__all__: list[str] = ['CommonCutterSurfaceLibrary']
+class CommonCutterSurfaceLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
