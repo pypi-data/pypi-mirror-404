cbded3dc30d420ff662f0292ad08cb8efff6d61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:48 +0000

    Add package passagemath-singular/10.8.1a4/passagemath_singular-10.8.1a4-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/passagemath-singular/passagemath_singular-10.8.1a4-cp312-cp312-musllinux_1_2_aarch64.whl/sage/all__sagemath_singular.py
@@ -0,0 +1,11 @@
+# sage_setup: distribution = sagemath-singular
+# delvewheel: patch
+
+from sage.all__sagemath_flint import *
+from sage.all__sagemath_pari import *
+from sage.all__sagemath_modules import *
+
+from .algebras.all__sagemath_singular import *
+from .libs.all__sagemath_singular import *
+from .matrix.all__sagemath_singular import *
+from .rings.all__sagemath_singular import *
