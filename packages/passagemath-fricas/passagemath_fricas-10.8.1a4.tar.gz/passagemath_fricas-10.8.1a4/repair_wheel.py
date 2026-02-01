e71cf3bdb9e9ecb07c6dd1fbdf6b8ea4980cdc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:02 +0000

    Add package passagemath-fricas/10.8.1a4/passagemath_fricas-10.8.1a4.tar.gz

---
+++ packages/passagemath-fricas/passagemath_fricas-10.8.1a4.tar.gz/passagemath_fricas-10.8.1a4/repair_wheel.py
@@ -0,0 +1,34 @@
+# Add data to the wheel
+
+import os
+import shlex
+import sys
+
+from pathlib import Path
+
+from auditwheel.wheeltools import InWheel
+
+from sage.config import SAGE_LOCAL
+
+wheel = sys.argv[1]
+
+if "TMPDIR" in os.environ:
+    os.environ["TMPDIR"] = str(Path(os.environ["TMPDIR"]).resolve())
+
+wheel = Path(sys.argv[1])
+
+with InWheel(wheel, wheel):
+    # SAGE_LOCAL/bin/fricas --> sage_wheels/bin/fricas
+    command = f'set -o pipefail; (cd {shlex.quote(SAGE_LOCAL)} && tar cf - --dereference bin/{{fricas,efricas}} lib/fricas) | (mkdir -p sage_wheels && cd sage_wheels && tar xvf -)'
+    print(f'Running {command}')
+    sys.stdout.flush()
+    if os.system(f"bash -c {shlex.quote(command)}") != 0:
+        sys.exit(1)
+
+    # Remove the sage-conf dependency; it is not needed because our wheels ship what is needed.
+
+    command = 'sed -i.bak "/^Requires-Dist: passagemath-conf/d" *.dist-info/METADATA'
+    print(f'Running {command}')
+    sys.stdout.flush()
+    if os.system(f"bash -c {shlex.quote(command)}") != 0:
+        sys.exit(1)
