0bca330a0bb7fb96ae1b6122d36870df96791c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 08:05:50 +0000

    Add package glin-profanity/3.3.0/glin_profanity-3.3.0-py3-none-any.whl

---
+++ packages/glin-profanity/glin_profanity-3.3.0-py3-none-any.whl/glin_profanity/types/__init__.py
@@ -0,0 +1,23 @@
+"""Type definitions for glin-profanity."""
+
+from .types import (
+    CheckProfanityResult,
+    ContextAwareConfig,
+    FilterConfig,
+    FilteredProfanityResult,
+    Language,
+    LeetspeakLevel,
+    Match,
+    SeverityLevel,
+)
+
+__all__ = [
+    "CheckProfanityResult",
+    "ContextAwareConfig",
+    "FilterConfig",
+    "FilteredProfanityResult",
+    "Language",
+    "LeetspeakLevel",
+    "Match",
+    "SeverityLevel",
+]
