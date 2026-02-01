8e3df819178b50e680533dc5cb62a4363bba0842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 05:30:46 +0000

    Add package linkarchivetools/0.1.16/linkarchivetools-0.1.16-py3-none-any.whl

---
+++ packages/linkarchivetools/linkarchivetools-0.1.16-py3-none-any.whl/linkarchivetools/dbanalyzer.py
@@ -0,0 +1,356 @@
+"""
+Provides information about archive
+
+Examples:
+ - What was said about Musk
+  $ --search "title=*Musk*"
+ - What was said about Musk (title, link, description, etc)
+  $ --search "Musk"
+
+TODO
+ - Output formats? (md)?
+ - Maybe it could produce a chart?
+
+"""
+
+import argparse
+import time
+import os
+import json
+from sqlalchemy import create_engine
+
+from .utils.omnisearch import SingleSymbolEvaluator, EquationEvaluator, OmniSearch
+from .utils.alchemysearch import (
+    AlchemySymbolEvaluator,
+    AlchemyEquationEvaluator,
+    AlchemySearch,
+)
+from .utils.reflected import (
+    ReflectedTable,
+    ReflectedEntryTable,
+    ReflectedUserTags,
+    ReflectedSocialData,
+)
+
+
+def print_time_diff(start_time):
+    elapsed_time_seconds = time.time() - start_time
+    elapsed_minutes = int(elapsed_time_seconds // 60)
+    elapsed_seconds = int(elapsed_time_seconds % 60)
+    print(f"Time: {elapsed_minutes}:{elapsed_seconds}")
+
+
+class DisplayRowHandler(object):
+
+    def __init__(self, args=None, engine=None, connection=None):
+        self.args = args
+        self.start_time = time.time()
+        self.engine = engine
+        self.connection = connection
+
+        self.files = []
+
+        self.total_entries = 0
+        self.good_entries = 0
+        self.dead_entries = 0
+
+    def print_entry(self, entry):
+        level = self.args.verbosity
+        if level is None or level == 0:
+            return
+
+        text = ""
+
+        if self.args.description:
+            print("---------------------")
+
+        text = "[{:03d}] {}".format(entry.page_rating_votes, entry.link)
+
+        if self.args.title:
+            if entry.title:
+                text += " " + entry.title
+
+        if self.args.source:
+            source_id = entry.source
+            if source_id:
+                r = ReflectedEntryTable(self.engine, self.connection)
+                source = r.get_source(source_id)
+                text += " [{}]".format(source.title)
+
+        print(text)
+
+        if self.args.date_published:
+            date_published = entry.date_published
+            if date_published:
+                print(date_published)
+
+        if self.args.description:
+            description = entry.description
+            if description:
+                print(description)
+
+        if self.args.tags:
+            tags_table = ReflectedUserTags(self.engine, self.connection)
+            tags = tags_table.get_tags_string(entry.id)
+            if tags and tags != "":
+                self.print_tags(tags)
+
+        if self.args.social:
+            social_table = ReflectedSocialData(self.engine, self.connection)
+            social = social_table.get(entry.id)
+            if social is not None:
+                self.print_social(social)
+
+        if self.args.status:
+            print(entry.status_code)
+
+    def print_tags(self, tags):
+        print(tags)
+
+    def print_social(self, social):
+        if (
+            social.view_count is not None
+            and social.thumbs_up is not None
+            and social.thumbs_down is not None
+        ):
+            print(
+                f"V:{social.view_count} TU:{social.thumbs_up} TD:{social.thumbs_down}"
+            )
+        else:
+            if social.view_count:
+                print(f"F:{social.view_count}")
+
+            if social.thumbs_up:
+                print(f"F:{social.thumbs_up}")
+
+            if social.thumbs_down:
+                print(f"F:{social.thumbs_down}")
+
+            if social.upvote_diff:
+                print(f"S:{social.upvote_diff}")
+
+            if social.upvote_ratio:
+                print(f"S:{social.upvote_ratio}")
+
+            if social.followers_count:
+                print(f"F:{social.followers_count}")
+
+            if social.stars:
+                print(f"S:{social.stars}")
+
+    def get_time_diff(self):
+        return time.time() - self.start_time
+
+    def handle_row(self, row):
+        """
+        Row is to be expected a 'dict', eg. row["link"]
+        """
+        link = row.link
+
+        level = self.args.verbosity
+
+        self.print_entry(row)
+
+        self.total_entries += 1
+
+    def summary(self):
+        if self.args.summary:
+            if self.args.verify:
+                print(
+                    "total:{} good:{} dead:{}".format(
+                        self.total_entries, self.good_entries, self.dead_entries
+                    )
+                )
+            else:
+                print("total:{}".format(self.total_entries))
+
+
+class YieldRowHandler(object):
+
+    def __init__(self, args=None, engine=None, connection=None):
+        self.args = args
+        self.start_time = time.time()
+        self.engine = engine
+        self.connection = connection
+
+        self.files = []
+
+        self.total_entries = 0
+        self.good_entries = 0
+        self.dead_entries = 0
+
+    def handle_row(self, row):
+        """
+        Row is to be expected a 'dict', eg. row["link"]
+        """
+        yield row
+
+
+class DbAnalyzer(object):
+    def __init__(self, input_db, args=None):
+        self.args = args
+        self.result = None
+        self.engine = None
+        self.input_db = input_db
+
+    def print_summary(self, print_columns=False):
+        db = self.input_db
+
+        if not os.path.isfile(db):
+            print("File does not exist:{}".format(db))
+            return
+
+        self.engine = create_engine("sqlite:///" + db)
+        with self.engine.connect() as connection:
+            r = ReflectedTable(self.engine, connection)
+            r.print_summary(print_columns)
+
+    def search(self):
+        if self.is_db_scan():
+            file = self.input_db
+            if not os.path.isfile(file):
+                print("File does not exist:{}".format(file))
+                return
+
+            print("Creating engine")
+            self.engine = create_engine("sqlite:///" + self.input_db)
+            print("Creating engine DONE")
+
+            with self.engine.connect() as connection:
+                self.connection = connection
+                yield self.perform_search()
+
+    def perform_search(self):
+        row_handler = DisplayRowHandler(args=self.args, engine=self.engine, connection=self.connection)
+
+        search = None
+        if self.args:
+            search = self.args.search
+
+        print("Starting alchemy")
+        searcher = AlchemySearch(
+            self.engine,
+            search,
+            row_handler=row_handler,
+            args=self.args,
+            connection=self.connection,
+        )
+        print("Starting alchemy DONE")
+
+        print("Searching...")
+        yield from searcher.search()
+
+    def get_entries(self):
+        if self.is_db_scan():
+            file = self.input_db
+            if not os.path.isfile(file):
+                print("File does not exist:{}".format(file))
+                return
+
+            print("Creating engine")
+            self.engine = create_engine("sqlite:///" + self.input_db)
+            print("Creating engine DONE")
+
+            with self.engine.connect() as connection:
+                self.connection = connection
+                yield from self.perform_get_entries()
+
+    def perform_get_entries(self):
+        row_handler = YieldRowHandler(args=self.args, engine=self.engine, connection=self.connection)
+
+        search = None
+        if self.args:
+            search = self.args.search
+
+        print("Starting alchemy")
+        searcher = AlchemySearch(
+            self.engine,
+            search,
+            row_handler=row_handler,
+            args=self.args,
+            connection=self.connection,
+        )
+        print("Starting alchemy DONE")
+
+        print("Searching...")
+        yield from searcher.search()
+
+    def is_db_scan(self):
+        if self.input_db:
+            return True
+
+        return False
+
+
+class Parser(object):
+
+    def parse(self):
+        self.parser = argparse.ArgumentParser(description="Data analyzer program")
+        self.parser.add_argument("--db", help="DB to be scanned")
+
+        self.parser.add_argument(
+            "--search", help="Search, with syntax same as the main program / site."
+        )
+        self.parser.add_argument(
+            "--order-by", default="page_rating_votes", help="order by column."
+        )
+        self.parser.add_argument("--asc", action="store_true", help="order ascending")
+        self.parser.add_argument("--desc", action="store_true", help="order descending")
+        self.parser.add_argument("--table", default="linkdatamodel", help="Table name")
+
+        self.parser.add_argument("--title", action="store_true", help="displays title")
+        self.parser.add_argument(
+            "--description", action="store_true", help="displays description"
+        )
+        self.parser.add_argument(
+            "--status", action="store_true", help="displays status"
+        )
+        self.parser.add_argument("--tags", action="store_true", help="displays tags")
+        self.parser.add_argument(
+            "--social", action="store_true", help="displays social data"
+        )
+        self.parser.add_argument(
+            "--date-published", action="store_true", help="displays date-published"
+        )
+        self.parser.add_argument(
+            "--source", action="store_true", help="displays source"
+        )
+
+        self.parser.add_argument(
+            "--summary", action="store_true", help="displays summary of tables"
+        )
+        self.parser.add_argument(
+            "--columns",
+            action="store_true",
+            help="displays summary of tables column nmaes",
+        )
+
+        self.parser.add_argument(
+            "-i", "--ignore-case", action="store_true", help="Ignores case"
+        )
+        self.parser.add_argument("-v", "--verbosity",  type=int, default = 1, help="Verbosity level")
+
+        self.args = self.parser.parse_args()
+
+        return True
+
+
+def main():
+    p = Parser()
+    if not p.parse():
+        print("Could not parse options")
+        return
+
+    start_time = time.time()
+
+    m = DbAnalyzer(input_db=p.args.db, args=p.args)
+    if p.args.summary:
+        m.print_summary(p.args.columns)
+    else:
+        m.search()
+
+    print_time_diff(start_time)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/linkarchivetools/linkarchivetools-0.1.16-py3-none-any.whl/linkarchivetools/json2db.py
@@ -0,0 +1,237 @@
+"""
+@brief Converts JSON files to SQLite DB
+
+SQLite can easily be imported and used by other projects.
+"""
+
+import os
+import sqlite3
+import json
+import argparse
+import time
+from pathlib import Path
+from sqlalchemy import create_engine
+from dateutil import parser
+
+from .utils.reflected import *
+
+
+class DirReader(object):
+    def __init__(self, source_files_directory, accepted_extensions=None):
+        self.dir = source_files_directory
+        if accepted_extensions is None:
+            self.accepted_extensions = [".json"]
+
+    def get_files(self):
+        file_list = []
+        for root, dirs, files in os.walk(self.dir):
+            for file in files:
+                file_split = os.path.splitext(file)
+                if file_split[1] in self.accepted_extensions:
+                    file_list.append(os.path.join(root, file))
+
+        file_list = sorted(file_list)
+        return file_list
+
+
+class JSON2Db(object):
+    """
+    Performs actual conversion between from JSON to DB
+    """
+
+    def __init__(self, input_file=None, input_dir=None, output_db=None, preserve_id=False, vote_threshold=None, verbose=False):
+        self.input_file = input_file
+        self.input_dir = input_dir
+        self.output_db = output_db
+        self.preserve_id = preserve_id
+        self.vote_threshold = vote_threshold
+        self.verbose = verbose
+
+        if self.input_dir:
+            self.file_reader = DirReader(source_files_directory=self.input_dir)
+            self.files = self.file_reader.get_files()
+        elif self.input_file:
+            self.file_reader = None
+            self.files = [self.input_file]
+        else:
+            self.file_reader = None
+            self.files = []
+
+    def convert(self):
+        #path = Path(self.output_db)
+        #if path.exists():
+        #    path.unlink()
+
+        self.engine = create_engine(f"sqlite:///{self.output_db}")
+        with self.engine.connect() as connection:
+            self.connection = connection
+
+            total_num_files = len(self.files)
+
+            for row, afile in enumerate(self.files):
+                print("[{}/{}]: file:{}".format(row, total_num_files, afile))
+                self.convert_file(afile)
+
+    def convert_file(self, file_name):
+        data = self.read_file(file_name)
+        if not data:
+            return
+
+        total_rows = len(data)
+
+        for row, entry in enumerate(data):
+            entry = self.prepare_entry(entry)
+            if "link" in entry:
+                if self.preserve_id:
+                    if "id" not in entry:
+                        print("Entry {} is missing ID".format(entry["link"]))
+                        continue
+                else:
+                    entry["id"] = row
+
+                if self.is_entry_to_be_added(entry):
+                    table = ReflectedEntryTable(engine=self.engine, connection=self.connection)
+                    if table.insert_json(entry) is not None:
+                        if self.verbose:
+                            print(
+                                " -> [{}/{}] Link:{} Added".format(
+                                    row, total_rows, entry["link"]
+                                )
+                            )
+                    else:
+                        print(
+                            " -> [{}/{}] Link:{} NOT Added".format(
+                                row, total_rows, entry["link"]
+                            )
+                        )
+                else:
+                    if self.verbose:
+                        print(
+                            " -> [{}/{}] Link:{} Skipped".format(
+                                row, total_rows, entry["link"]
+                            )
+                        )
+
+    def prepare_entry(self, entry):
+        """
+        Drops any unwelcome keys
+        """
+        table = ReflectedEntryTable(engine=self.engine, connection=self.connection)
+        columns = table.get_column_names()
+        keys = list(entry.keys())
+
+        diff = list(set(keys) - set(columns))
+        for item in diff:
+            del entry[item]
+
+        for key in entry:
+            if key.startswith("date"):
+                if entry[key]:
+                    entry[key] = parser.parse(entry[key])
+
+        return entry
+
+    def is_entry_to_be_added(self, entry):
+        # entry already exists
+        table = ReflectedEntryTable(engine=self.engine, connection=self.connection)
+        if "id" in entry and table.exists(id=entry["id"]):
+            return False
+        if "link" in entry and table.exists(link=entry["link"]):
+            return False
+
+        if self.vote_threshold:
+            if "page_rating_votes" in entry:
+                if entry["page_rating_votes"]:
+                    if int(entry["page_rating_votes"]) < self.vote_threshold:
+                        return False
+                    else:
+                        return True
+                return False
+            return False
+
+        return True
+
+    def read_file_contents(self, file_name):
+        with open(file_name, "r") as f:
+            return f.read()
+
+    def read_file(self, file_name):
+        text = self.read_file_contents(file_name)
+
+        try:
+            j = json.loads(text)
+
+            if "links" in j:
+                return j["links"]
+            if "sources" in j:
+                return j["sources"]
+
+            return j
+        except Exception as e:
+            print("Could not read file: {}".format(afile))
+
+
+class Parser(object):
+    def parse(self):
+        self.parser = argparse.ArgumentParser(description="Data converter program")
+        self.parser.add_argument("--input-file", help="File to be scanned")
+        self.parser.add_argument("--input-dir", help="Directory to be scanned")
+        self.parser.add_argument(
+            "--output-db", default="converted.sqlite", help="Output db name"
+        )
+        self.parser.add_argument(
+            "--preserve-id", action="store_true", help="Preserves ID of objects"
+        )
+        self.parser.add_argument("--vote-min", help="Minimum amount of entry vote")
+        self.parser.add_argument("--language", help="Accept language")  # TODO implement
+        self.parser.add_argument("--entries", help="Convert entries")  # TODO implement
+        self.parser.add_argument("--sources", help="Convert sources")  # TODO implement
+        self.parser.add_argument(
+            "--verbose", action="store_true", help="Shows more info"
+        )
+
+        self.args = self.parser.parse_args()
+
+        if self.args.dir:
+            self.dir = self.args.dir
+        else:
+            self.dir = None
+
+        if self.args.preserve_id:
+            self.preserve_id = self.args.preserve_id
+        else:
+            self.preserve_id = None
+
+        if self.args.vote_min:
+            self.vote_min = int(self.args.vote_min)
+        else:
+            self.vote_min = None
+
+
+def main():
+    print("Starting processing")
+    parser = Parser()
+    parser.parse()
+
+    try:
+        start_time = time.time()
+
+        c = JSON2Db(input_file = parser.args.input_file, input_dir=parser.args.input_dir, output_db = parser.args.output_db)
+        c.convert()
+
+        elapsed_time_seconds = time.time() - start_time
+        elapsed_minutes = int(elapsed_time_seconds // 60)
+        elapsed_seconds = int(elapsed_time_seconds % 60)
+        print(f"Time: {elapsed_minutes}:{elapsed_seconds}")
+
+    except Exception as e:
+        print("Exception: {}".format(e))
+    except KeyboardInterrupt as e:
+        print("Exception: {}".format(e))
+
+    db.close()
+    print("Processing DONE")
+
+
+if __name__ == "__main__":
+    main()
