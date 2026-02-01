952da5e2b4936b22f8e353bf6ae250ce773f1283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:51:53 +0000

    Add package fbgemm-gpu-nightly-cpu/2026.1.31/fbgemm_gpu_nightly_cpu-2026.1.31-cp310-cp310-manylinux_2_28_aarch64.whl

---
+++ packages/fbgemm-gpu-nightly-cpu/fbgemm_gpu_nightly_cpu-2026.1.31-cp310-cp310-manylinux_2_28_aarch64.whl/fbgemm_gpu/split_embedding_optimizer_codegen/optimizer_args.py
@@ -0,0 +1,34 @@
+################################################################################
+## GENERATED FILE INFO
+##
+## Template Source: training/python/optimizer_args.py
+################################################################################
+
+__template_source_file__ = "training/python/optimizer_args.py"
+
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from typing import NamedTuple
+
+import torch
+from torch import nn
+
+
+class SplitEmbeddingOptimizerParams(NamedTuple):
+    weights_dev: nn.Parameter
+    # TODO: Enable weights_uvm and weights_lxu_cache support
+    # weights_uvm: nn.Parameter
+    # weights_lxu_cache: nn.Parameter
+
+
+class SplitEmbeddingArgs(NamedTuple):
+    weights_placements: torch.Tensor
+    weights_offsets: torch.Tensor
+    max_D: int
