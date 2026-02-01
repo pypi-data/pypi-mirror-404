a65ff8104b9f571f65bc1d5776b4dd3553c00516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 14:08:40 +0000

    Add package snailz/5.1.0/snailz-5.1.0-py3-none-any.whl

---
+++ packages/snailz/snailz-5.1.0-py3-none-any.whl/snailz/__init__.py
@@ -0,0 +1,10 @@
+"""Synthetic data generator for snail mutation survey."""
+
+from .assay import Assay as Assay
+from .grid import Grid as Grid
+from .machine import Machine as Machine
+from .parameters import Parameters as Parameters
+from .person import Person as Person
+from .rating import Rating as Rating
+from .species import Species as Species
+from .specimen import Specimen as Specimen
