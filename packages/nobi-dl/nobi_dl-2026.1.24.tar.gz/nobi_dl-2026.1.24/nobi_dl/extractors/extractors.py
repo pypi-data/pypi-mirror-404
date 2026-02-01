133070be1367b649d93727bca8c082c7e70ae903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:52:54 +0000

    Add package nobi-dl/2026.1.24/nobi_dl-2026.1.24.tar.gz

---
+++ packages/nobi-dl/nobi_dl-2026.1.24.tar.gz/nobi_dl-2026.1.24/nobi_dl/extractors/extractors.py
@@ -0,0 +1,7 @@
+from .bollyflix import BollyFlixME
+from .hdhub import Hdhub4uME
+from .vegamovies import VegaMoviesME
+from .fourkhdhub import FourkhdhubME
+from .moviesmod import MoviesModME
+
+ALL = [BollyFlixME, Hdhub4uME, VegaMoviesME, FourkhdhubME, MoviesModME]
