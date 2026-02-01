f96debaf9156b2e5de5402f34982501cc165ffd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:05 +0000

    Add package passagemath-gap-pkg-ctbllib-data/10.8.1a4/passagemath_gap_pkg_ctbllib_data-10.8.1a4.tar.gz

---
+++ packages/passagemath-gap-pkg-ctbllib-data/passagemath_gap_pkg_ctbllib_data-10.8.1a4.tar.gz/passagemath_gap_pkg_ctbllib_data-10.8.1a4/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-gap-pkg-ctbllib-data'],
+           recurse_packages=('sage', 'passagemath_gap_pkg_ctbllib_data'),
+           spkgs=['gap'])
