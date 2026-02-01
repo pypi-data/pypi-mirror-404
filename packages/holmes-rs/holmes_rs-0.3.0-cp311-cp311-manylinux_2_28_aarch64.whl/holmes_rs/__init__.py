9baf81b0d8e380e5b4b1939dbc523d6b2e1ce697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:05 +0000

    Add package holmes-rs/0.3.0/holmes_rs-0.3.0-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/holmes-rs/holmes_rs-0.3.0-cp311-cp311-manylinux_2_28_aarch64.whl/holmes_rs/__init__.py
@@ -0,0 +1,23 @@
+from .holmes_rs import (
+    __version__,
+    calibration,
+    hydro,
+    metrics,
+    pet,
+    snow,
+    HolmesError,
+    HolmesNumericalError,
+    HolmesValidationError,
+)
+
+__all__ = [
+    "__version__",
+    "calibration",
+    "hydro",
+    "metrics",
+    "pet",
+    "snow",
+    "HolmesError",
+    "HolmesNumericalError",
+    "HolmesValidationError",
+]
