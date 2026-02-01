7623da70c1a67a114ac6df69191e69f591f86d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:42 +0000

    Add package passagemath-standard-no-symbolics/10.8.1a4/passagemath_standard_no_symbolics-10.8.1a4-cp313-cp313-macosx_13_0_arm64.whl

---
+++ packages/passagemath-standard-no-symbolics/passagemath_standard_no_symbolics-10.8.1a4-cp313-cp313-macosx_13_0_arm64.whl/sage/rings/polynomial/all.py
@@ -0,0 +1,22 @@
+"""
+Polynomials
+"""
+# ****************************************************************************
+#       Copyright (C) 2005 William Stein <wstein@gmail.com>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from sage.rings.polynomial.all__sagemath_polyhedra import *
+
+# Generic convolution
+from sage.rings.polynomial.convolution import convolution
