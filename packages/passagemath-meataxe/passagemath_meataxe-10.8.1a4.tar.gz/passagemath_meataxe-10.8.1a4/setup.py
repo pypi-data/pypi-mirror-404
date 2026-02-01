e50085786b8a161b1d14221eff6a104ce057523e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:21 +0000

    Add package passagemath-meataxe/10.8.1a4/passagemath_meataxe-10.8.1a4.tar.gz

---
+++ packages/passagemath-meataxe/passagemath_meataxe-10.8.1a4.tar.gz/passagemath_meataxe-10.8.1a4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from sage_setup import sage_setup
+
+sage_setup(['sagemath-meataxe'],
+           recurse_packages=('sage', 'passagemath_meataxe'),
+           spkgs=['meataxe'],
+           package_data={
+               "sage.libs": ["meataxe.pxd"],
+               "sage.matrix": ["matrix_gfpn_dense.pxd"],
+           })
