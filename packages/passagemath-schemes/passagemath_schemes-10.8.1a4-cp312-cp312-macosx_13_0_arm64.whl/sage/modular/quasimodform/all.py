762f583e52395655e601ba9e2baba682cd067748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:44 +0000

    Add package passagemath-schemes/10.8.1a4/passagemath_schemes-10.8.1a4-cp312-cp312-macosx_13_0_arm64.whl

---
+++ packages/passagemath-schemes/passagemath_schemes-10.8.1a4-cp312-cp312-macosx_13_0_arm64.whl/sage/modular/quasimodform/all.py
@@ -0,0 +1,3 @@
+# sage_setup: distribution = sagemath-schemes
+# Quasimodular forms rings
+from sage.modular.quasimodform.ring import QuasiModularForms
---
+++ packages/passagemath-schemes/passagemath_schemes-10.8.1a4-cp312-cp312-macosx_13_0_arm64.whl/sage/schemes/hyperelliptic_curves/all.py
@@ -0,0 +1,6 @@
+# sage_setup: distribution = sagemath-schemes
+from sage.schemes.hyperelliptic_curves.constructor import HyperellipticCurve
+from sage.schemes.hyperelliptic_curves.kummer_surface import KummerSurface
+from sage.schemes.hyperelliptic_curves.mestre import (
+    Mestre_conic, HyperellipticCurve_from_invariants)
+from sage.schemes.hyperelliptic_curves import monsky_washnitzer
