12f9dc37c35f0e2c44f1d2e696318326adc8e006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 04:03:53 +0000

    Add package Geode-Background/9.9.6/geode_background-9.9.6-cp311-cp311-win_amd64.whl

---
+++ packages/Geode-Background/geode_background-9.9.6-cp311-cp311-win_amd64.whl/geode_background/bin/geode_background_py_common.pyi
@@ -0,0 +1,9 @@
+"""
+Geode-Background Python binding for common
+"""
+from __future__ import annotations
+__all__: list[str] = ['BackgroundCommonLibrary']
+class BackgroundCommonLibrary:
+    @staticmethod
+    def initialize() -> None:
+        ...
