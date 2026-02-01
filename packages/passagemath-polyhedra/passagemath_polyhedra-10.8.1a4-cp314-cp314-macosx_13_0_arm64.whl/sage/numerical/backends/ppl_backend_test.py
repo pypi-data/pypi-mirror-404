1fc7bc7f4256ff0dcbad7bae7f8644eacbef4252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-01 07:17:36 +0000

    Add package passagemath-polyhedra/10.8.1a4/passagemath_polyhedra-10.8.1a4-cp314-cp314-macosx_13_0_arm64.whl

---
+++ packages/passagemath-polyhedra/passagemath_polyhedra-10.8.1a4-cp314-cp314-macosx_13_0_arm64.whl/sage/numerical/backends/ppl_backend_test.py
@@ -0,0 +1,13 @@
+# sage_setup: distribution = sagemath-polyhedra
+import pytest
+
+from sage.numerical.backends.generic_backend_test import GenericBackendTests
+from sage.numerical.backends.generic_backend import GenericBackend
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+
+class TestPPLBackend(GenericBackendTests):
+
+    @pytest.fixture
+    def backend(self) -> GenericBackend:
+        return MixedIntegerLinearProgram(solver='PPL').get_backend()
