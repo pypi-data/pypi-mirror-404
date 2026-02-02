d1e19599c528cca61fa4f32b045854974bd8956d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:11 +0000

    Add package defistream/1.2.0/defistream-1.2.0-py3-none-any.whl

---
+++ packages/defistream/defistream-1.2.0-py3-none-any.whl/defistream/__init__.py
@@ -0,0 +1,58 @@
+"""
+DeFiStream Python Client
+
+Official Python client for the DeFiStream API.
+
+Example:
+    >>> from defistream import DeFiStream
+    >>> client = DeFiStream(api_key="dsk_...")
+    >>>
+    >>> # Builder pattern - returns pandas DataFrame
+    >>> df = (
+    ...     client.erc20.transfers("USDT")
+    ...     .network("ETH")
+    ...     .block_range(21000000, 21010000)
+    ...     .as_df()
+    ... )
+    >>>
+    >>> # Save to file
+    >>> query.as_file("transfers.parquet")
+"""
+
+from .client import AsyncDeFiStream, DeFiStream
+from .exceptions import (
+    AuthenticationError,
+    DeFiStreamError,
+    NotFoundError,
+    QuotaExceededError,
+    RateLimitError,
+    ServerError,
+    ValidationError,
+)
+from .query import (
+    AggregateQueryBuilder,
+    AsyncAggregateQueryBuilder,
+    AsyncQueryBuilder,
+    QueryBuilder,
+)
+
+__version__ = "1.2.0"
+
+__all__ = [
+    # Clients
+    "DeFiStream",
+    "AsyncDeFiStream",
+    # Query builders
+    "QueryBuilder",
+    "AsyncQueryBuilder",
+    "AggregateQueryBuilder",
+    "AsyncAggregateQueryBuilder",
+    # Exceptions
+    "DeFiStreamError",
+    "AuthenticationError",
+    "QuotaExceededError",
+    "RateLimitError",
+    "ValidationError",
+    "NotFoundError",
+    "ServerError",
+]
