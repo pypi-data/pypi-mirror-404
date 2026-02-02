2f34d67124994bc40e047f4909f5632ae24a6fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 04:41:56 +0000

    Add package casambi-bt-revamped/0.3.12.dev7/casambi_bt_revamped-0.3.12.dev7-py3-none-any.whl

---
+++ packages/casambi-bt-revamped/casambi_bt_revamped-0.3.12.dev7-py3-none-any.whl/CasambiBt/_cache.py
@@ -0,0 +1,111 @@
+import asyncio
+import logging
+import os
+import pathlib
+import shutil
+from types import TracebackType
+from typing import Final
+
+from anyio import Path
+
+_LOGGER = logging.getLogger(__name__)
+
+CACHE_PATH_DEFAULT: Final = Path(os.getcwd()) / "casambi-bt-store"
+CACHE_VERSION: Final = 2
+
+# We need a global lock since there could be multiple Casambi instances
+# with their own cache instances pointing to the same folder.
+_cacheLock = asyncio.Lock()
+
+
+def _blocking_delete(path: Path) -> None:
+    shutil.rmtree(pathlib.Path(path))
+
+
+class Cache:
+    def __init__(self, cachePath: Path | pathlib.Path | None) -> None:
+        if cachePath is None:
+            self._cachePath = CACHE_PATH_DEFAULT
+        elif not isinstance(cachePath, Path):
+            self._cachePath = Path(cachePath)
+        else:
+            self._cachePath = cachePath
+
+        self._cacheVersionFile = self._cachePath / ".cachever"
+        self._uuid: str | None = None
+        _LOGGER.info("Selecting cache path %s", self._cachePath)
+
+    async def setUuid(self, uuid: str) -> None:
+        async with _cacheLock:
+            self._uuid = uuid
+
+    async def _ensureCacheValid(self) -> None:
+        # We assume that we already have a lock when calling this function
+        assert _cacheLock.locked()
+
+        if await self._cachePath.exists():
+            cacheVer = None
+            if await self._cacheVersionFile.exists():
+                try:
+                    cacheVer = int(await self._cacheVersionFile.read_text())
+                    _LOGGER.debug("Read cache version %i.", cacheVer)
+                except ValueError:
+                    cacheVer = -1
+                    _LOGGER.error("Failed to parse cache version.", exc_info=True)
+            if cacheVer is None:
+                cacheVer = 0
+            if cacheVer < CACHE_VERSION:
+                _LOGGER.warning(
+                    "Cache is version %i, version %i is required. Recreating cache.",
+                    cacheVer,
+                    CACHE_VERSION,
+                )
+                loop = asyncio.get_running_loop()
+                await loop.run_in_executor(None, _blocking_delete, self._cachePath)
+
+        # This is not a redunant condition. We may have deleted the cache.
+        if not await self._cachePath.exists():
+            _LOGGER.info("Creating new cache.")
+            await self._cachePath.mkdir(mode=0o700)
+            await self._cacheVersionFile.write_text(str(CACHE_VERSION))
+
+    async def __aenter__(self) -> Path:
+        await _cacheLock.acquire()
+
+        if self._uuid is None:
+            raise ValueError("UUID not set.")
+
+        try:
+            await self._ensureCacheValid()
+
+            cacheDir = Path(self._cachePath / self._uuid)
+            if not await cacheDir.exists():
+                _LOGGER.debug("Creating cache entry for id %s", self._uuid)
+                await cacheDir.mkdir()
+
+            _LOGGER.debug("Returning cache path %s for id %s.", cacheDir, self._uuid)
+            return cacheDir
+        except Exception:
+            _cacheLock.release()
+            raise
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        _cacheLock.release()
+
+    async def invalidateCache(self) -> None:
+        async with _cacheLock:
+            if self._uuid is None:
+                raise ValueError("UUID not set.")
+            await self._ensureCacheValid()
+            if not await (self._cachePath / self._uuid).exists():
+                return
+            _LOGGER.info("Deleting cache entry %s", self._uuid)
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(
+                None, _blocking_delete, self._cachePath / self._uuid
+            )
