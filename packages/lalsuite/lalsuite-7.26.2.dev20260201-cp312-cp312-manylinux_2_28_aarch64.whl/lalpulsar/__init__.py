d47208ef20660e89d4e0bd63d0d604aa96febc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:23 +0000

    Add package lalsuite/7.26.2.dev20260201/lalsuite-7.26.2.dev20260201-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/lalsuite/lalsuite-7.26.2.dev20260201-cp312-cp312-manylinux_2_28_aarch64.whl/lalpulsar/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from ._lalpulsar_swig import *
+
+__version__ = "7.1.1.1"
+
+## \addtogroup lalpulsar_python
+"""This package provides Python wrappings and extensions to LALPulsar"""
