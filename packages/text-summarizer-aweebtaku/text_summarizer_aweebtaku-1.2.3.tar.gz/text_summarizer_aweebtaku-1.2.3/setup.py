6b2002e424526333a1d12b7260467b12e575bd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 04:42:22 +0000

    Add package text-summarizer-aweebtaku/1.2.3/text_summarizer_aweebtaku-1.2.3.tar.gz

---
+++ packages/text-summarizer-aweebtaku/text_summarizer_aweebtaku-1.2.3.tar.gz/text_summarizer_aweebtaku-1.2.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="text-summarizer-aweebtaku",
+    version="1.2.3",
+    author="Aditya Chaurasiya",
+    author_email="adityachaurasiya57527@gmail.com",
+    description="A text summarization tool using GloVe embeddings and PageRank algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AWeebTaku/Summarizer",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "text-summarizer-aweebtaku=text_summarizer.cli:main",
+            "text-summarizer-gui=text_summarizer.ui:main",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "textsummarizer": ["data/*.csv"],
+    },
+)
---
+++ packages/text-summarizer-aweebtaku/text_summarizer_aweebtaku-1.2.3.tar.gz/text_summarizer_aweebtaku-1.2.3/text_summarizer/__init__.py
@@ -0,0 +1,3 @@
+from .summarizer import TextSummarizer
+
+__version__ = "1.2.3"
