943a968a47fa0e3c5107a4ef990a18fc6a9f1158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:47 +0000

    Add package nishant-project/0.1.7/nishant_project-0.1.7-py3-none-any.whl

---
+++ packages/nishant-project/nishant_project-0.1.7-py3-none-any.whl/myproj/__init__.py
@@ -0,0 +1,5 @@
+from .core import add, division, multiply, subtract, upper
+from .utils import greet
+from .version import __version__
+
+__all__ = ["add", "multiply", "subtract", "division", "upper", "greet", "__version__"]
