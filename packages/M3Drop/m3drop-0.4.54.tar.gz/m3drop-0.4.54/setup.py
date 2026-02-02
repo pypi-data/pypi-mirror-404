132c7487e66f18cb3c341d870e04f64c02cf1a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:06 +0000

    Add package M3Drop/0.4.54/m3drop-0.4.54.tar.gz

---
+++ packages/M3Drop/m3drop-0.4.54.tar.gz/m3drop-0.4.54/setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="M3Drop",  # Name for pip (pip install M3Drop)
+    version="0.4.54",
+    author="Tallulah Andrews",
+    author_email="tandrew6@uwo.ca",
+    description="A Python implementation of the M3Drop single-cell RNA-seq analysis tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PragalvhaSharma/m3DropNew",
+    license="MIT",
+    
+    # 1. PACKAGE DISCOVERY
+    # Looks for folder 'm3Drop' (lowercase m)
+    packages=setuptools.find_packages(include=["m3Drop", "m3Drop.*"]),
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "anndata>=0.8.0",
+        "h5py>=3.8.0",
+        "matplotlib>=3.5.0",
+        "matplotlib-venn>=0.11",
+        "memory_profiler>=0.60.0",
+        "numpy>=1.21.0",
+        "pandas>=1.5.0",
+        "scanpy>=1.9.0",
+        "scikit-learn>=1.0.0",
+        "scipy>=1.8.0",
+        "seaborn>=0.11.0",
+        "statsmodels>=0.13.0",
+        "numba>=0.57.0",   
+        "psutil>=5.9.0",   
+        "py-cpuinfo",      
+    ],
+    extras_require={
+        "gpu": ["cupy-cuda12x"],
+    },
+    include_package_data=True,
+    
+    # 2. DATA MAPPING
+    # Key must match the folder name 'm3Drop'
+    package_data={
+        "m3Drop": ["*.md", "*.txt"],
+    },
+)
