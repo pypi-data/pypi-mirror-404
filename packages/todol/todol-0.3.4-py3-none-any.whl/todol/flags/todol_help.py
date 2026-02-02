f8b32e3bcff7fbf9ff196b69700e123c8f02f357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:06 +0000

    Add package todol/0.3.4/todol-0.3.4-py3-none-any.whl

---
+++ packages/todol/todol-0.3.4-py3-none-any.whl/todol/flags/todol_help.py
@@ -0,0 +1,10 @@
+class TodolHelp():
+    def help():
+        print("todol - A simple terminal todo list app")
+        print("Type 'h' inside the app to see available commands.")
+        print("\nAdditional flags:")
+        print("  todol-help      Show this help message")
+        print("  todol-version   Show current version")
+        print("  todol-path      Show where todo files are stored")
+        print("  todol-upgrade   Update todol (runs 'pipx upgrade todol')")
+        print("\nDocumentation: https://github.com/wattox00/todol")
