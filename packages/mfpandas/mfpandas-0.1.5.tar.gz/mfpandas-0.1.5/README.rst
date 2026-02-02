54cb081123b38fc2bc7058cd214588a905df48b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:01 +0000

    Add package mfpandas/0.1.5/mfpandas-0.1.5.tar.gz

---
+++ packages/mfpandas/mfpandas-0.1.5.tar.gz/mfpandas-0.1.5/README.rst
@@ -0,0 +1,13 @@
+mfpandas
+########
+
+The framework for working with various Mainframe (z/OS) datastructures via Pandas Dataframes.
+Read the docs at https://mfpandas.readthedocs.io 
+
+Installation::
+
+    pip install mfpandas 
+
+
+This used to be `pyracf` but that name is now being used by an awesome framework to work with RACF directly from python.
+Read more about it via: https://github.com/ambitus/pyracf 
---
+++ packages/mfpandas/mfpandas-0.1.5.tar.gz/mfpandas-0.1.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import glob 
+
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mfpandas",
+    version="0.1.5",
+    author="Wizard of z/OS",
+    author_email="wizard@zdevops.com",
+    description="Parsing various z/OS structures into Panda dataframes.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/wizardofzos/mfpandas",
+    project_urls={
+        "Bug Tracker": "https://github.com/wizardofzos/mfpandas/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={
+        "": "src"
+        },
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    install_requires=[
+        'wheel',
+        'pandas>=1.5.2',
+        'xlsxwriter>=3.1.0'
+    ],
+    python_requires=">=3.6",
+)
