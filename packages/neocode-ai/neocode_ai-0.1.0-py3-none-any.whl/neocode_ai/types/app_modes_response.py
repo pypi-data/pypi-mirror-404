29a5bb3b2dcf6c4ba3a3e8217d9465abf3b69e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:39 +0000

    Add package neocode-ai/0.1.0/neocode_ai-0.1.0-py3-none-any.whl

---
+++ packages/neocode-ai/neocode_ai-0.1.0-py3-none-any.whl/neocode_ai/types/app_modes_response.py
@@ -0,0 +1,10 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+from typing_extensions import TypeAlias
+
+from .mode import Mode
+
+__all__ = ["AppModesResponse"]
+
+AppModesResponse: TypeAlias = List[Mode]
