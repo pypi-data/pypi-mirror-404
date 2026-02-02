18b3919bb14b057fb7316dc188a03094502486e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 04:42:05 +0000

    Add package multiflexi-client/1.1.0/multiflexi_client-1.1.0-py3-none-any.whl

---
+++ packages/multiflexi-client/multiflexi_client-1.1.0-py3-none-any.whl/multiflexi_client/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from multiflexi_client.api.app_api import AppApi
+from multiflexi_client.api.company_api import CompanyApi
+from multiflexi_client.api.default_api import DefaultApi
+from multiflexi_client.api.gdpr_api import GdprApi
+from multiflexi_client.api.job_api import JobApi
+from multiflexi_client.api.runtemplate_api import RuntemplateApi
+from multiflexi_client.api.user_api import UserApi
+
