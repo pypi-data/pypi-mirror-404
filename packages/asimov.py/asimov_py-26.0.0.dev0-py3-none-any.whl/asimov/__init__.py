07e47f7a7596f25a12c88c4a8a661ff02cc90b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 04:41:50 +0000

    Add package asimov.py/26.0.0.dev0/asimov_py-26.0.0.dev0-py3-none-any.whl

---
+++ packages/asimov.py/asimov_py-26.0.0.dev0-py3-none-any.whl/asimov/__init__.py
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+"""ASIMOV Software Development Kit (SDK) for Python"""
+
+import sys
+
+assert sys.version_info >= (3, 9), "ASIMOV.py requires Python 3.9+"
+
+from ._version import __version__, __version_tuple__
