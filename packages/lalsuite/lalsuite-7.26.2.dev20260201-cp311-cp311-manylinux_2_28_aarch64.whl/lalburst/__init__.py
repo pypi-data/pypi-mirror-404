900f0c89d042753bc587f17ee868147abcbc2fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-02 03:56:18 +0000

    Add package lalsuite/7.26.2.dev20260201/lalsuite-7.26.2.dev20260201-cp311-cp311-manylinux_2_28_aarch64.whl

---
+++ packages/lalsuite/lalsuite-7.26.2.dev20260201-cp311-cp311-manylinux_2_28_aarch64.whl/lalburst/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from ._lalburst_swig import *
+
+__version__ = "2.0.7.1"
+
+## \addtogroup lalburst_python
+"""This package provides Python wrappings and extensions to LALBurst"""
